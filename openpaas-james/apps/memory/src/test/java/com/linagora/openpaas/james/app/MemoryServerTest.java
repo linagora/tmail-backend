493191b8001211d8ccc646191f00f735afad6ac1
Benoit Tellier <btellier@linagora.com>
2021-01-18 13:52:42 +0700

    ISSUE-16 Remove POP3, LMTP and ManageSieve support

--- openpaas-james/apps/memory/src/test/java/com/linagora/openpaas/james/app/MemoryServerTest.java
+++ openpaas-james/apps/memory/src/test/java/com/linagora/openpaas/james/app/MemoryServerTest.java
@@ -1,10 +1,13 @@
 package com.linagora.openpaas.james.app;
 
+import org.apache.james.GuiceJamesServer;
 import org.apache.james.JamesServerBuilder;
 import org.apache.james.JamesServerContract;
 import org.apache.james.JamesServerExtension;
 import org.apache.james.jmap.draft.JmapJamesServerContract;
 import org.apache.james.modules.TestJMAPServerModule;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 class MemoryServerTest implements JamesServerContract, JmapJamesServerContract {
@@ -13,4 +16,16 @@ class MemoryServerTest implements JamesServerContract, JmapJamesServerContract {
         .server(configuration -> MemoryServer.createServer(configuration)
             .overrideWith(new TestJMAPServerModule()))
         .build();
+
+    @Disabled("POP3 server is disabled")
+    @Test
+    public void connectPOP3ServerShouldSendShabangOnConnect(GuiceJamesServer jamesServer) throws Exception {
+        // POP3 server is disabled
+    }
+
+    @Disabled("LMTP server is disabled")
+    @Test
+    public void connectLMTPServerShouldSendShabangOnConnect(GuiceJamesServer jamesServer) throws Exception {
+        // LMTP server is disabled
+    }
 }
