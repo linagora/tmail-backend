quantranhong1999/openpaas-james

Linagora Filter/set memory tests

openpaas-james/integration-tests/jmap/memory-jmap-integration-tests/src/test/java/com/linagora/openpaas/james/MemoryLinagoraFilterSetMethodTest.java
@@ -1,0 +1,49 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package com.linagora.openpaas.james;
+
+import org.apache.james.JamesServerBuilder;
+import org.apache.james.JamesServerExtension;
+import org.apache.james.mailbox.inmemory.InMemoryId;
+import org.apache.james.modules.TestJMAPServerModule;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import com.linagora.openpaas.james.app.MemoryServer;
+import com.linagora.openpaas.james.common.LinagoraFilterSetMethodContract;
+import com.linagora.openpaas.james.common.module.JmapGuiceCustomModule;
+
+public class MemoryLinagoraFilterSetMethodTest implements LinagoraFilterSetMethodContract {
+    @RegisterExtension
+    static JamesServerExtension jamesServerExtension = new JamesServerBuilder<>(JamesServerBuilder.defaultConfigurationProvider())
+        .server(configuration -> MemoryServer.createServer(configuration)
+            .overrideWith(new TestJMAPServerModule())
+            .overrideWith(new JmapGuiceCustomModule()))
+        .build();
+
+    @Override
+    public String generateMailboxIdForUser() {
+        return InMemoryId.of(1).toString();
+    }
+
+    @Override
+    public String generateAccountIdAsString() {
+        return null;
+    }
+}

linagora/tmail-backend

openpaas-james/integration-tests/jmap/memory-jmap-integration-tests/src/test/java/com/linagora/openpaas/james/MemoryLinagoraFilterSetMethodTest.java
@@ -1,0 +1,47 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package com.linagora.openpaas.james;
+
+import org.apache.james.JamesServerBuilder;
+import org.apache.james.JamesServerExtension;
+import org.apache.james.mailbox.inmemory.InMemoryId;
+import org.apache.james.modules.TestJMAPServerModule;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import com.linagora.openpaas.james.app.MemoryServer;
+import com.linagora.openpaas.james.common.LinagoraFilterSetMethodContract;
+
+public class MemoryLinagoraFilterSetMethodTest implements LinagoraFilterSetMethodContract {
+    @RegisterExtension
+    static JamesServerExtension jamesServerExtension = new JamesServerBuilder<>(JamesServerBuilder.defaultConfigurationProvider())
+        .server(configuration -> MemoryServer.createServer(configuration)
+            .overrideWith(new TestJMAPServerModule()))
+        .build();
+
+    @Override
+    public String generateMailboxIdForUser() {
+        return InMemoryId.of(1).toString();
+    }
+
+    @Override
+    public String generateAccountIdAsString() {
+        return "29883977c13473ae7cb7678ef767cbfbaffc8a44a6e463d971d23a65c1dc4af6";
+    }
+}