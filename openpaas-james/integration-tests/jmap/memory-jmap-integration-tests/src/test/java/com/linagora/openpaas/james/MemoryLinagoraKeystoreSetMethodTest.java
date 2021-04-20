036ccf7d078a784c4f07bafe6ddb57a7eefb5c09
Rene Cordier <rcordier@linagora.com>
2021-04-20 15:24:08 +0700

    ISSUE-93 Add a probe to test that the key is correctly stored

--- openpaas-james/integration-tests/jmap/memory-jmap-integration-tests/src/test/java/com/linagora/openpaas/james/MemoryLinagoraKeystoreSetMethodTest.java
+++ openpaas-james/integration-tests/jmap/memory-jmap-integration-tests/src/test/java/com/linagora/openpaas/james/MemoryLinagoraKeystoreSetMethodTest.java
@@ -2,17 +2,18 @@ package com.linagora.openpaas.james;
 
 import org.apache.james.JamesServerBuilder;
 import org.apache.james.JamesServerExtension;
-import org.apache.james.mailbox.inmemory.InMemoryId;
 import org.apache.james.modules.TestJMAPServerModule;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 import com.linagora.openpaas.james.app.MemoryServer;
 import com.linagora.openpaas.james.common.LinagoraKeystoreSetMethodContract;
+import com.linagora.openpaas.james.common.module.JmapGuiceKeystoreManagerModule;
 
 class MemoryLinagoraKeystoreSetMethodTest implements LinagoraKeystoreSetMethodContract {
     @RegisterExtension
     static JamesServerExtension jamesServerExtension = new JamesServerBuilder<>(JamesServerBuilder.defaultConfigurationProvider())
         .server(configuration -> MemoryServer.createServer(configuration)
-            .overrideWith(new TestJMAPServerModule()))
+            .overrideWith(new TestJMAPServerModule())
+            .overrideWith(new JmapGuiceKeystoreManagerModule()))
         .build();
 }
