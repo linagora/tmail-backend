d36639ad06e93e1f651b4d968a11e1b294f3680e
quanth <hqtran@linagora.com>
2021-05-13 15:19:08 +0700

    ISSUE-74 FilterStateChange intergration tests

---
+++ tmail-backend/integration-tests/jmap/distributed-jmap-integration-tests/src/test/java/com/linagora/tmail/james/DistributedLinagoraFilterStateChangeTest.java
@@ -0,0 +1,51 @@
+package com.linagora.tmail.james;
+
+import org.apache.james.CassandraExtension;
+import org.apache.james.JamesServerBuilder;
+import org.apache.james.JamesServerExtension;
+import org.apache.james.SearchConfiguration;
+import org.apache.james.mailbox.cassandra.ids.CassandraId;
+import org.apache.james.modules.AwsS3BlobStoreExtension;
+import org.apache.james.modules.RabbitMQExtension;
+import org.apache.james.modules.TestJMAPServerModule;
+import org.apache.james.modules.blobstore.BlobStoreConfiguration;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import com.linagora.tmail.james.app.DistributedJamesConfiguration;
+import com.linagora.tmail.james.app.DistributedServer;
+import com.linagora.tmail.james.app.DockerElasticSearchExtension;
+import com.linagora.tmail.james.common.LinagoraFilterStateChangeTest;
+import com.linagora.tmail.james.common.module.JmapGuiceFilterStateChangeModule;
+
+public class DistributedLinagoraFilterStateChangeTest implements LinagoraFilterStateChangeTest {
+    @RegisterExtension
+    static JamesServerExtension testExtension = new JamesServerBuilder<DistributedJamesConfiguration>(tmpDir ->
+        DistributedJamesConfiguration.builder()
+            .workingDirectory(tmpDir)
+            .configurationFromClasspath()
+            .blobStore(BlobStoreConfiguration.builder()
+                .s3()
+                .disableCache()
+                .deduplication()
+                .noCryptoConfig())
+            .searchConfiguration(SearchConfiguration.elasticSearch())
+            .build())
+        .extension(new DockerElasticSearchExtension())
+        .extension(new CassandraExtension())
+        .extension(new RabbitMQExtension())
+        .extension(new AwsS3BlobStoreExtension())
+        .server(configuration -> DistributedServer.createServer(configuration)
+            .overrideWith(new TestJMAPServerModule())
+            .overrideWith(new JmapGuiceFilterStateChangeModule()))
+        .build();
+
+    @Override
+    public String generateMailboxIdForUser() {
+        return CassandraId.of("123e4567-e89b-12d3-a456-426614174000").asUuid().toString();
+    }
+
+    @Override
+    public String generateAccountIdAsString() {
+        return "29883977c13473ae7cb7678ef767cbfbaffc8a44a6e463d971d23a65c1dc4af6";
+    }
+}
