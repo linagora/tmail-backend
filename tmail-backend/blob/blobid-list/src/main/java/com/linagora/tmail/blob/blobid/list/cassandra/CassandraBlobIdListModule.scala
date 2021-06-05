19996af315be48b43853861bcb09b7fb5f705305
Benoit Tellier <btellier@linagora.com>
2021-06-05 19:20:13 +0700

    [PERFORMANCE] Reduce Cassandra chunk length for some read intensive tables
    See https://github.com/apache/james-project/pull/476

--- tmail-backend/blob/blobid-list/src/main/java/com/linagora/tmail/blob/blobid/list/cassandra/CassandraBlobIdListModule.scala
+++ tmail-backend/blob/blobid-list/src/main/java/com/linagora/tmail/blob/blobid/list/cassandra/CassandraBlobIdListModule.scala
@@ -1,11 +1,11 @@
 package com.linagora.tmail.blob.blobid.list.cassandra
 
-import com.datastax.driver.core.DataType.{blob, text}
-import com.datastax.driver.core.schemabuilder.Create
+import com.datastax.driver.core.DataType.text
 import com.datastax.driver.core.schemabuilder.Create.Options
 import com.datastax.driver.core.schemabuilder.SchemaBuilder.KeyCaching.ALL
 import com.datastax.driver.core.schemabuilder.SchemaBuilder.rows
-import BlobIdListTable.{BLOB_ID, TABLE_NAME}
+import com.datastax.driver.core.schemabuilder.{Create, SchemaBuilder}
+import com.linagora.tmail.blob.blobid.list.cassandra.BlobIdListTable.{BLOB_ID, TABLE_NAME}
 import org.apache.james.backends.cassandra.components.CassandraModule
 import org.apache.james.backends.cassandra.utils.CassandraConstants.DEFAULT_CACHED_ROW_PER_PARTITION
 
@@ -13,7 +13,8 @@ object CassandraBlobIdListModule {
   val MODULE: CassandraModule = CassandraModule.table(TABLE_NAME)
     .comment("Holds BlobId list definition")
     .options((options: Options) => options
-      .caching(ALL, rows(DEFAULT_CACHED_ROW_PER_PARTITION)))
+      .caching(ALL, rows(DEFAULT_CACHED_ROW_PER_PARTITION))
+      .compressionOptions(SchemaBuilder.lz4.withChunkLengthInKb(8)))
     .statement((statement: Create) => statement
       .addPartitionKey(BLOB_ID, text))
     .build
