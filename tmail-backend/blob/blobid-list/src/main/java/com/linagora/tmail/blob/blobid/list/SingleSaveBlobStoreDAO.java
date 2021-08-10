540b8ad8347486dca535cb3cd4185795f862a1f9
Rene Cordier <rcordier@linagora.com>
2021-08-10 15:52:47 +0700

    fixup! Fix SingleSaveBlobStoreDAO save blob and deleteBucket methods

--- tmail-backend/blob/blobid-list/src/main/java/com/linagora/tmail/blob/blobid/list/SingleSaveBlobStoreDAO.java
+++ tmail-backend/blob/blobid-list/src/main/java/com/linagora/tmail/blob/blobid/list/SingleSaveBlobStoreDAO.java
@@ -12,7 +12,6 @@ import org.apache.james.blob.api.ObjectStoreException;
 import org.apache.james.blob.api.ObjectStoreIOException;
 import org.reactivestreams.Publisher;
 
-import com.google.common.base.Preconditions;
 import com.google.common.io.ByteSource;
 
 import reactor.core.publisher.Mono;
@@ -41,54 +40,55 @@ public class SingleSaveBlobStoreDAO implements BlobStoreDAO {
         return blobStoreDAO.readBytes(bucketName, blobId);
     }
 
-    private boolean isBucketNameDefault(BucketName bucketName) {
-        Preconditions.checkNotNull(bucketName);
-        return defaultBucketName.equals(bucketName);
-    }
-
-    private Mono<Void> saveBlobId(BlobId blobId, BucketName bucketName) {
-        if (isBucketNameDefault(bucketName)) {
-            return Mono.from(blobIdList.store(blobId))
-                .then();
+    @Override
+    public Mono<Void> save(BucketName bucketName, BlobId blobId, byte[] data) {
+        if (defaultBucketName.equals(bucketName)) {
+            return Mono.from(blobIdList.isStored(blobId))
+                .flatMap(isStored -> {
+                    if (isStored) {
+                        return Mono.empty();
+                    }
+                    return Mono.from(blobStoreDAO.save(bucketName, blobId, data))
+                        .then(Mono.from(blobIdList.store(blobId)))
+                        .then();
+                });
         } else {
-            return Mono.empty();
+            return Mono.from(blobStoreDAO.save(bucketName, blobId, data));
         }
     }
 
     @Override
-    public Mono<Void> save(BucketName bucketName, BlobId blobId, byte[] data) {
-        return Mono.from(blobIdList.isStored(blobId))
-                .flatMap(isStored -> {
-                    if (isStored && isBucketNameDefault(bucketName)) {
-                        return Mono.empty();
-                    }
-                    return Mono.from(blobStoreDAO.save(bucketName, blobId, data))
-                        .then(saveBlobId(blobId, bucketName));
-                });
-    }
-
-    @Override
     public Publisher<Void> save(BucketName bucketName, BlobId blobId, InputStream inputStream) {
-        return Mono.from(blobIdList.isStored(blobId))
+        if (defaultBucketName.equals(bucketName)) {
+            return Mono.from(blobIdList.isStored(blobId))
                 .flatMap(isStored -> {
-                    if (isStored && isBucketNameDefault(bucketName)) {
+                    if (isStored) {
                         return Mono.empty();
                     }
                     return Mono.from(blobStoreDAO.save(bucketName, blobId, inputStream))
-                        .then(saveBlobId(blobId, bucketName));
+                        .then(Mono.from(blobIdList.store(blobId)))
+                        .then();
                 });
+        } else {
+            return Mono.from(blobStoreDAO.save(bucketName, blobId, inputStream));
+        }
     }
 
     @Override
     public Publisher<Void> save(BucketName bucketName, BlobId blobId, ByteSource content) {
-        return Mono.from(blobIdList.isStored(blobId))
+        if (defaultBucketName.equals(bucketName)) {
+            return Mono.from(blobIdList.isStored(blobId))
                 .flatMap(isStored -> {
-                    if (isStored && isBucketNameDefault(bucketName)) {
+                    if (isStored) {
                         return Mono.empty();
                     }
                     return Mono.from(blobStoreDAO.save(bucketName, blobId, content))
-                        .then(saveBlobId(blobId, bucketName));
+                        .then(Mono.from(blobIdList.store(blobId)))
+                        .then();
                 });
+        } else {
+            return Mono.from(blobStoreDAO.save(bucketName, blobId, content));
+        }
     }
 
     @Override
