05bc20edd9499f8ce0a0172bc18202d855a3a37f
Rene Cordier <rcordier@linagora.com>
2021-09-28 09:08:19 +0700

    ISSUE-215 Add missing LongLivedToken/get distributed integration test

---
+++ tmail-backend/integration-tests/jmap/distributed-jmap-integration-tests/src/test/java/com/linagora/tmail/james/DistributedLongLivedTokenGetTest.java
@@ -0,0 +1,40 @@
+package com.linagora.tmail.james;
+
+import org.apache.james.CassandraExtension;
+import org.apache.james.JamesServerBuilder;
+import org.apache.james.JamesServerExtension;
+import org.apache.james.mailbox.opendistro.DockerOpenDistroExtension;
+import org.apache.james.mailbox.opendistro.DockerOpenDistroSingleton;
+import org.apache.james.modules.AwsS3BlobStoreExtension;
+import org.apache.james.modules.RabbitMQExtension;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import com.linagora.tmail.blob.blobid.list.BlobStoreConfiguration;
+import com.linagora.tmail.james.app.DistributedJamesConfiguration;
+import com.linagora.tmail.james.app.DistributedServer;
+import com.linagora.tmail.james.common.LinagoraLongLivedTokenSetMethodContract;
+import com.linagora.tmail.james.common.module.JmapGuiceKeystoreManagerModule;
+import com.linagora.tmail.module.LinagoraTestJMAPServerModule;
+
+public class DistributedLongLivedTokenGetTest implements LinagoraLongLivedTokenSetMethodContract {
+    @RegisterExtension
+    static JamesServerExtension
+        testExtension = new JamesServerBuilder<DistributedJamesConfiguration>(tmpDir ->
+        DistributedJamesConfiguration.builder()
+            .workingDirectory(tmpDir)
+            .configurationFromClasspath()
+            .blobStore(BlobStoreConfiguration.builder()
+                .disableCache()
+                .deduplication()
+                .noCryptoConfig()
+                .disableSingleSave())
+            .build())
+        .extension(new DockerOpenDistroExtension(DockerOpenDistroSingleton.INSTANCE))
+        .extension(new CassandraExtension())
+        .extension(new RabbitMQExtension())
+        .extension(new AwsS3BlobStoreExtension())
+        .server(configuration -> DistributedServer.createServer(configuration)
+            .overrideWith(new LinagoraTestJMAPServerModule())
+            .overrideWith(new JmapGuiceKeystoreManagerModule()))
+        .build();
+}
