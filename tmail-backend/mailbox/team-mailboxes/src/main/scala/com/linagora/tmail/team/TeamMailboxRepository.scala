0ad61b7c01393e267fe5ae1fac969d038649dbb5
Tung Van TRAN <vttran@linagora.com>
2021-10-05 08:00:47 +0700

    ISSUE-254 Create the TeamMailboxRepository API and its implementation

---
+++ tmail-backend/mailbox/team-mailboxes/src/main/scala/com/linagora/tmail/team/TeamMailboxRepository.scala
@@ -0,0 +1,126 @@
+package com.linagora.tmail.team
+
+import com.linagora.tmail.team.TeamMailboxNameSpace.TEAM_MAILBOX_NAMESPACE
+import com.linagora.tmail.team.TeamMailboxRepositoryImpl.{TEAM_MAILBOX_QUERY, TEAM_MAILBOX_RIGHTS_DEFAULT}
+import org.apache.james.core.{Domain, Username}
+import org.apache.james.mailbox.model.MailboxACL
+import org.apache.james.mailbox.model.MailboxACL.{NameType, Right}
+import org.apache.james.mailbox.model.search.MailboxQuery
+import org.apache.james.mailbox.{MailboxManager, MailboxSession, SessionProvider}
+import org.reactivestreams.Publisher
+import reactor.core.scala.publisher.{SFlux, SMono}
+
+import javax.inject.Inject
+import scala.jdk.CollectionConverters._
+
+trait TeamMailboxRepository {
+
+  def createTeamMailbox(teamMailbox: TeamMailbox): Publisher[Void]
+
+  def deleteTeamMailbox(teamMailbox: TeamMailbox): Publisher[Void]
+
+  def listTeamMailboxes(domain: Domain): Publisher[TeamMailbox]
+
+  def listTeamMailboxes(username: Username): Publisher[TeamMailbox]
+
+  def addMember(teamMailbox: TeamMailbox, addUser: Username): Publisher[Void]
+
+  def removeMember(teamMailbox: TeamMailbox, removeUser: Username): Publisher[Void]
+
+  def listMembers(teamMailbox: TeamMailbox): Publisher[Username]
+
+}
+
+object TeamMailboxRepositoryImpl {
+  val TEAM_MAILBOX_QUERY: MailboxQuery = MailboxQuery.builder
+    .namespace(TEAM_MAILBOX_NAMESPACE)
+    .matchesAllMailboxNames
+    .build
+
+  val TEAM_MAILBOX_RIGHTS_DEFAULT: MailboxACL.Rfc4314Rights =
+    new MailboxACL.Rfc4314Rights(
+      Right.Lookup,
+      Right.Post,
+      Right.Read,
+      Right.WriteSeenFlag,
+      Right.DeleteMessages,
+      Right.Write
+    )
+}
+
+class TeamMailboxRepositoryImpl @Inject()(mailboxManager: MailboxManager,
+                                          sessionProvider: SessionProvider) extends TeamMailboxRepository {
+
+  override def createTeamMailbox(teamMailbox: TeamMailbox): Publisher[Void] = {
+    val session: MailboxSession = sessionProvider.createSystemSession(Username.of(teamMailbox.user.asString))
+    SMono.fromCallable(() => mailboxManager.createMailbox(teamMailbox.mailboxPath, session))
+      .`then`(SMono.fromCallable(() => mailboxManager.createMailbox(teamMailbox.inboxPath, session)))
+      .`then`(SMono.fromCallable(() => mailboxManager.createMailbox(teamMailbox.sentPath, session)))
+      .`then`()
+  }
+
+  override def deleteTeamMailbox(teamMailbox: TeamMailbox): Publisher[Void] = {
+    val session: MailboxSession = sessionProvider.createSystemSession(Username.of(teamMailbox.user.asString))
+    SMono.fromCallable(() => mailboxManager.deleteMailbox(teamMailbox.mailboxPath, session))
+      .`then`(SMono.fromCallable(() => mailboxManager.deleteMailbox(teamMailbox.inboxPath, session)))
+      .`then`(SMono.fromCallable(() => mailboxManager.deleteMailbox(teamMailbox.sentPath, session)))
+      .`then`()
+  }
+
+  override def listTeamMailboxes(domain: Domain): Publisher[TeamMailbox] = {
+    val session: MailboxSession = sessionProvider.createSystemSession(Username.of(domain.asString()))
+    SFlux.fromPublisher(mailboxManager.search(TEAM_MAILBOX_QUERY, session))
+      .filter(mailboxMetaData => domain.asString().equals(mailboxMetaData.getPath.getUser.getLocalPart))
+      .flatMapIterable(mailboxMetaData => TeamMailbox.from(mailboxMetaData.getPath))
+  }
+
+  override def listTeamMailboxes(username: Username): Publisher[TeamMailbox] =
+    SFlux.fromPublisher(mailboxManager.search(TEAM_MAILBOX_QUERY, sessionProvider.createSystemSession(username)))
+      .flatMapIterable(mailboxMetaData => TeamMailbox.from(mailboxMetaData.getPath))
+
+  override def addMember(teamMailbox: TeamMailbox, addUser: Username): Publisher[Void] =
+    SMono.fromPublisher(isExistTeamMailbox(teamMailbox))
+      .filter(teamMailboxExist => teamMailboxExist)
+      .doOnNext(_ => mailboxManager.applyRightsCommand(
+        teamMailbox.mailboxPath,
+        MailboxACL.command
+          .forUser(addUser)
+          .rights(TEAM_MAILBOX_RIGHTS_DEFAULT)
+          .asAddition(),
+        sessionProvider.createSystemSession(Username.of(teamMailbox.user.asString))))
+      .switchIfEmpty(SMono.error(TeamMailboxNotFoundException()))
+      .`then`()
+
+  override def removeMember(teamMailbox: TeamMailbox, removeUser: Username): Publisher[Void] =
+    SMono.fromPublisher(isUserInTeamMailbox(teamMailbox, removeUser))
+      .filter(teamMailboxExist => teamMailboxExist)
+      .doOnNext(_ => mailboxManager.applyRightsCommand(
+        teamMailbox.mailboxPath,
+        MailboxACL.command
+          .forUser(removeUser)
+          .rights(TEAM_MAILBOX_RIGHTS_DEFAULT)
+          .asRemoval(),
+        sessionProvider.createSystemSession(Username.of(teamMailbox.user.asString))))
+      .switchIfEmpty(SMono.error(TeamMailboxNotFoundException()))
+      .`then`()
+
+  override def listMembers(teamMailbox: TeamMailbox): Publisher[Username] = {
+    val session: MailboxSession = sessionProvider.createSystemSession(Username.of(teamMailbox.user.asString))
+    SMono.fromCallable(() => mailboxManager.listRights(teamMailbox.mailboxPath, session))
+      .flatMapIterable(mailboxACL => mailboxACL.getEntries.asScala)
+      .map(entryKeyAndRights => entryKeyAndRights._1)
+      .filter(entryKey => NameType.user.equals(entryKey.getNameType))
+      .map(entryKey => Username.of(entryKey.getName))
+      .distinct()
+  }
+
+  private def isUserInTeamMailbox(teamMailbox: TeamMailbox, checkUser: Username): SMono[Boolean] =
+    SFlux.fromPublisher(listTeamMailboxes(checkUser))
+      .filter(teamMailbox1 => teamMailbox1.equals(teamMailbox))
+      .hasElements
+
+  private def isExistTeamMailbox(teamMailbox: TeamMailbox): SMono[Boolean] =
+    SFlux.fromPublisher(listTeamMailboxes(teamMailbox.user))
+      .filter(teamMailbox1 => teamMailbox1.equals(teamMailbox))
+      .hasElements
+}
---
+++ tmail-backend/mailbox/team-mailboxes/src/test/scala/com/linagora/tmail/team/TeamMailboxTest.scala
@@ -0,0 +1,42 @@
+package com.linagora.tmail.team
+
+import com.linagora.tmail.team.TeamMailboxRepositoryContract.TEAM_MAILBOX_USERNAME
+import org.apache.james.mailbox.model.MailboxPath
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class TeamMailboxTest {
+
+  @Test
+  def fromMailboxPathShouldReturnNoneWhenNamespaceInvalid(): Unit = {
+    assertThat(TeamMailbox.from(new MailboxPath("namespace123", TEAM_MAILBOX_USERNAME, "sales"))
+      .isEmpty)
+      .isTrue
+  }
+
+  @Test
+  def fromMailboxPathShouldReturnNoneWhenNameInvalid(): Unit = {
+    assertThat(TeamMailbox.from(new MailboxPath("namespace123", TEAM_MAILBOX_USERNAME, "sales."))
+      .isEmpty)
+      .isTrue
+  }
+
+  @Test
+  def fromMailboxPathShouldReturnTeamMailboxWhenValid(): Unit = {
+    assertThat(TeamMailbox.from(new MailboxPath("#TeamMailbox", TEAM_MAILBOX_USERNAME, "sales"))
+      .isEmpty)
+      .isFalse
+  }
+
+  @Test
+  def mailboxPathTeamMailboxShouldRespectAsStringMethod(): Unit = {
+    val teamMailbox: TeamMailbox = TeamMailbox.from(new MailboxPath("#TeamMailbox", TEAM_MAILBOX_USERNAME, "sales")).get
+    assertThat(teamMailbox.mailboxPath.asString())
+      .isEqualTo("#TeamMailbox:linagora.com:sales")
+    assertThat(teamMailbox.inboxPath.asString())
+      .isEqualTo("#TeamMailbox:linagora.com:sales.INBOX")
+    assertThat(teamMailbox.sentPath.asString())
+      .isEqualTo("#TeamMailbox:linagora.com:sales.Sent")
+  }
+
+}
