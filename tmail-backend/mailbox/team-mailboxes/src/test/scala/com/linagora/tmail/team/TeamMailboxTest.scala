f0ace405b513ce5ddaf798d466c32a0d70629950
Benoit Tellier <btellier@linagora.com>
2021-10-06 09:30:21 +0700

    ISSUE-254 Exact assertions in TeamMailboxTest

--- tmail-backend/mailbox/team-mailboxes/src/test/scala/com/linagora/tmail/team/TeamMailboxTest.scala
+++ tmail-backend/mailbox/team-mailboxes/src/test/scala/com/linagora/tmail/team/TeamMailboxTest.scala
@@ -1,31 +1,30 @@
 package com.linagora.tmail.team
 
-import com.linagora.tmail.team.TeamMailboxRepositoryContract.TEAM_MAILBOX_USERNAME
+import com.linagora.tmail.team.TeamMailboxRepositoryContract.{TEAM_MAILBOX_USER, TEAM_MAILBOX_USERNAME}
 import org.apache.james.mailbox.model.MailboxPath
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+
+import scala.jdk.OptionConverters._
 
 class TeamMailboxTest {
 
   @Test
   def fromMailboxPathShouldReturnNoneWhenNamespaceInvalid(): Unit = {
-    assertThat(TeamMailbox.from(new MailboxPath("namespace123", TEAM_MAILBOX_USERNAME, "sales"))
-      .isEmpty)
-      .isTrue
+    assertThat(TeamMailbox.from(new MailboxPath("namespace123", TEAM_MAILBOX_USERNAME, "sales")).toJava)
+      .isEmpty
   }
 
   @Test
   def fromMailboxPathShouldReturnNoneWhenNameInvalid(): Unit = {
-    assertThat(TeamMailbox.from(new MailboxPath("namespace123", TEAM_MAILBOX_USERNAME, "sales."))
-      .isEmpty)
-      .isTrue
+    assertThat(TeamMailbox.from(new MailboxPath("namespace123", TEAM_MAILBOX_USERNAME, "sales.")).toJava)
+      .isEmpty
   }
 
   @Test
   def fromMailboxPathShouldReturnTeamMailboxWhenValid(): Unit = {
-    assertThat(TeamMailbox.from(new MailboxPath("#TeamMailbox", TEAM_MAILBOX_USERNAME, "sales"))
-      .isEmpty)
-      .isFalse
+    assertThat(TeamMailbox.from(new MailboxPath("#TeamMailbox", TEAM_MAILBOX_USERNAME, "sales")).toJava)
+      .contains(TeamMailbox.apply(TEAM_MAILBOX_USER, TeamMailboxName.apply(TeamMailboxName.validate("sales").toOption.get)))
   }
 
   @Test
