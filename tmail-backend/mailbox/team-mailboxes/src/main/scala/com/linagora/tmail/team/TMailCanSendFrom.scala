d9f72b9c8d805845449686ef81d8287be14dc364
Benoit Tellier <btellier@linagora.com>
2021-10-11 16:33:10 +0700

    ISSUE-248 Implement TMailCanSendFrom

---
+++ tmail-backend/mailbox/team-mailboxes/src/main/scala/com/linagora/tmail/team/TMailCanSendFrom.scala
@@ -0,0 +1,31 @@
+package com.linagora.tmail.team
+
+import java.util.stream
+
+import org.apache.james.core.{MailAddress, Username}
+import org.apache.james.rrt.api.{AliasReverseResolver, RecipientRewriteTable}
+import org.apache.james.rrt.lib.CanSendFromImpl
+import reactor.core.publisher.Flux
+import reactor.core.scala.publisher.SMono
+
+class TMailCanSendFrom(rrt: RecipientRewriteTable, aliasReverseResolver: AliasReverseResolver, teamMailboxRepository: TeamMailboxRepository) extends CanSendFromImpl(rrt, aliasReverseResolver) {
+  override def userCanSendFrom(connectedUser: Username, fromUser: Username): Boolean =
+    super.userCanSendFrom(connectedUser, fromUser) || validTeamMailbox(connectedUser, fromUser)
+
+  private def validTeamMailbox(connectedUser: Username, fromUser: Username): Boolean =
+    TeamMailbox.asTeamMailbox(fromUser.asMailAddress()) match {
+      case Some(teamMailbox) => SMono(teamMailboxRepository.listMembers(teamMailbox))
+        .filter(connectedUser.equals(_))
+        .hasElement
+        .onErrorResume {
+          case _: TeamMailboxNotFoundException => SMono.just(false)
+          case e => SMono.error(e)
+        }
+        .block()
+      case None => false
+    }
+
+  override def allValidFromAddressesForUser(user: Username): stream.Stream[MailAddress] = stream.Stream.concat(
+    super.allValidFromAddressesForUser(user),
+    Flux.from(teamMailboxRepository.listTeamMailboxes(user)).map(_.asMailAddress).toStream)
+}
---
+++ tmail-backend/mailbox/team-mailboxes/src/test/scala/com/linagora/tmail/team/TMailCanSendFromTest.scala
@@ -0,0 +1,88 @@
+package com.linagora.tmail.team
+
+import com.google.common.collect.ImmutableList
+import eu.timepit.refined.auto._
+import org.apache.james.core.{Domain, MailAddress, Username}
+import org.apache.james.dnsservice.api.DNSService
+import org.apache.james.domainlist.lib.DomainListConfiguration
+import org.apache.james.domainlist.memory.MemoryDomainList
+import org.apache.james.mailbox.inmemory.manager.InMemoryIntegrationResources
+import org.apache.james.rrt.api.{CanSendFrom, RecipientRewriteTableConfiguration}
+import org.apache.james.rrt.lib.{AliasReverseResolverImpl, CanSendFromContract, Mapping, MappingSource}
+import org.apache.james.rrt.memory.MemoryRecipientRewriteTable
+import org.apache.james.user.memory.MemoryUsersRepository
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.{BeforeEach, Test}
+import org.mockito.Mockito.mock
+import reactor.core.scala.publisher.SMono
+
+class TMailCanSendFromTest extends CanSendFromContract {
+  var testee: CanSendFrom = _
+  var rrt: MemoryRecipientRewriteTable = _
+  var teamMailboxRepository: TeamMailboxRepository = _
+
+  @BeforeEach
+  def setUp(): Unit = {
+    val integrationResources = InMemoryIntegrationResources.defaultResources
+    teamMailboxRepository = new TeamMailboxRepositoryImpl(integrationResources.getMailboxManager, integrationResources.getMailboxManager)
+
+    val domainList = new MemoryDomainList(mock(classOf[DNSService]))
+    domainList.configure(DomainListConfiguration.DEFAULT)
+    domainList.addDomain(CanSendFromContract.DOMAIN)
+    domainList.addDomain(CanSendFromContract.OTHER_DOMAIN)
+
+    val usersRepository = MemoryUsersRepository.withVirtualHosting(domainList)
+
+    rrt = new MemoryRecipientRewriteTable
+    rrt.setUsersRepository(usersRepository)
+    rrt.setDomainList(domainList)
+    rrt.setConfiguration(RecipientRewriteTableConfiguration.DEFAULT_ENABLED)
+
+    testee = new TMailCanSendFrom(rrt, new AliasReverseResolverImpl(rrt), teamMailboxRepository)
+  }
+
+  override def canSendFrom: CanSendFrom = testee
+
+  override def addAliasMapping(alias: Username, user: Username): Unit = rrt.addAliasMapping(MappingSource.fromUser(alias.getLocalPart, alias.getDomainPart.get), user.asString)
+
+  override def addDomainMapping(alias: Domain, domain: Domain, mappingType: Mapping.Type): Unit = {
+    mappingType match {
+      case Mapping.Type.Domain =>
+        rrt.addDomainMapping(MappingSource.fromDomain(alias), domain)
+
+      case Mapping.Type.DomainAlias =>
+        rrt.addDomainAliasMapping(MappingSource.fromDomain(alias), domain)
+    }
+  }
+
+  override def addGroupMapping(group: String, user: Username): Unit = rrt.addGroupMapping(MappingSource.fromUser(Username.of(group)), user.asString)
+
+  @Test
+  def teamMailboxMemberCanSend(): Unit = {
+    val teamMailbox = TeamMailbox(Domain.of("domain.tld"), TeamMailboxName("marketing"))
+    SMono(teamMailboxRepository.createTeamMailbox(teamMailbox)).block()
+    SMono(teamMailboxRepository.addMember(teamMailbox, Username.of("bob@domain.tld"))).block()
+
+    assertThat(testee.userCanSendFrom(Username.of("bob@domain.tld"), Username.of("marketing@domain.tld")))
+      .isTrue
+  }
+
+  @Test
+  def nonMembersCannotSend(): Unit = {
+    val teamMailbox = TeamMailbox(Domain.of("domain.tld"), TeamMailboxName("marketing"))
+    SMono(teamMailboxRepository.createTeamMailbox(teamMailbox)).block()
+
+    assertThat(testee.userCanSendFrom(Username.of("bob@domain.tld"), Username.of("marketing@domain.tld")))
+      .isFalse
+  }
+
+  @Test
+  def allValidFromAddressesForUserShouldListTeamMailboxAlias(): Unit = {
+    val teamMailbox = TeamMailbox(Domain.of("domain.tld"), TeamMailboxName("marketing"))
+    SMono(teamMailboxRepository.createTeamMailbox(teamMailbox)).block()
+    SMono(teamMailboxRepository.addMember(teamMailbox, Username.of("bob@domain.tld"))).block()
+
+    assertThat(testee.allValidFromAddressesForUser(Username.of("bob@domain.tld")).collect(ImmutableList.toImmutableList[MailAddress]))
+      .containsOnly(new MailAddress("marketing@domain.tld"), new MailAddress("bob@domain.tld"))
+  }
+}
