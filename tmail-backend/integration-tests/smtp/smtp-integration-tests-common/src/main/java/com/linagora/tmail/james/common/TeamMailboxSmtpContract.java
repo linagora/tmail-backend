bb05e72376e16aefa3223a1ca0dd4634926627a7
Benoit TELLIER <btellier@linagora.com>
2021-10-14 10:35:22 +0700

    ISSUE-248 Email sent from a Team Mailbox should be well received (#295)

--- tmail-backend/integration-tests/smtp/smtp-integration-tests-common/src/main/java/com/linagora/tmail/james/common/TeamMailboxSmtpContract.java
+++ tmail-backend/integration-tests/smtp/smtp-integration-tests-common/src/main/java/com/linagora/tmail/james/common/TeamMailboxSmtpContract.java
@@ -1,14 +1,21 @@
 package com.linagora.tmail.james.common;
 
+import static org.apache.james.utils.TestIMAPClient.INBOX;
 import static org.assertj.core.api.Assertions.assertThatCode;
+
+import java.time.Duration;
 
 import org.apache.james.GuiceJamesServer;
 import org.apache.james.core.Domain;
 import org.apache.james.core.Username;
+import org.apache.james.modules.protocols.ImapGuiceProbe;
 import org.apache.james.modules.protocols.SmtpGuiceProbe;
 import org.apache.james.util.Port;
 import org.apache.james.utils.DataProbeImpl;
 import org.apache.james.utils.SMTPMessageSender;
+import org.apache.james.utils.TestIMAPClient;
+import org.awaitility.Awaitility;
+import org.awaitility.core.ConditionFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -17,18 +24,22 @@ import com.linagora.tmail.team.TeamMailboxName;
 import com.linagora.tmail.team.TeamMailboxProbe;
 
 public abstract class TeamMailboxSmtpContract {
-
     public static final Domain DOMAIN = Domain.of("domain.tld");
     public static final TeamMailbox MARKETING_TEAM_MAILBOX = TeamMailbox.apply(DOMAIN, TeamMailboxName.fromString("marketing").toOption().get());
     public static final Username BOB = Username.fromLocalPartWithDomain("bob", DOMAIN);
     public static final String BOB_PASSWORD = "123456";
+    public static final ConditionFactory AWAIT_TEN_SECONDS = Awaitility.await().atMost(Duration.ofSeconds(10));
+
     private SMTPMessageSender messageSender;
+    private TestIMAPClient imapClient;
 
     @BeforeEach
     void setUp(GuiceJamesServer server) throws Exception {
-        Port port = server.getProbe(SmtpGuiceProbe.class).getSmtpPort();
-        messageSender = new SMTPMessageSender("domain.tld");
-        messageSender.connect("127.0.0.1", port);
+        Port smtpPort = server.getProbe(SmtpGuiceProbe.class).getSmtpPort();
+        int imapPort = server.getProbe(ImapGuiceProbe.class).getImapPort();
+        messageSender = new SMTPMessageSender("domain.tld")
+            .connect("127.0.0.1", smtpPort);
+        imapClient = new TestIMAPClient().connect("127.0.0.1", imapPort);
 
         server.getProbe(DataProbeImpl.class)
             .fluent()
@@ -41,10 +52,14 @@ public abstract class TeamMailboxSmtpContract {
     }
 
     @Test
-    void smtpShouldAcceptSubmissionFromATeamMailbox() {
+    void smtpShouldAcceptSubmissionFromATeamMailbox() throws Exception {
         assertThatCode(() -> messageSender.authenticate(BOB.asString(), BOB_PASSWORD)
             .sendMessage(MARKETING_TEAM_MAILBOX.asMailAddress().asString(), BOB.asString()))
             .doesNotThrowAnyException();
+
+        imapClient.login(BOB, BOB_PASSWORD)
+            .select(INBOX)
+            .awaitMessage(AWAIT_TEN_SECONDS);
     }
 }
 
