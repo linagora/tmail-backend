587db7850725bc7d0a836e065e1576f80c28b4e2
Rene Cordier <rcordier@linagora.com>
2021-10-21 16:05:24 +0700

    ISSUE-251 Route for team mailbox get quota

--- tmail-backend/webadmin/webadmin-team-mailboxes/src/main/java/com/linagora/tmail/webadmin/TeamMailboxQuotaService.java
+++ tmail-backend/webadmin/webadmin-team-mailboxes/src/main/java/com/linagora/tmail/webadmin/TeamMailboxQuotaService.java
@@ -1,24 +1,32 @@
 package com.linagora.tmail.webadmin;
 
+import java.util.Map;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 
 import org.apache.james.core.quota.QuotaCountLimit;
 import org.apache.james.core.quota.QuotaSizeLimit;
 import org.apache.james.mailbox.exception.MailboxException;
+import org.apache.james.mailbox.model.Quota;
 import org.apache.james.mailbox.model.QuotaRoot;
 import org.apache.james.mailbox.quota.MaxQuotaManager;
+import org.apache.james.mailbox.quota.QuotaManager;
 import org.apache.james.webadmin.dto.ValidatedQuotaDTO;
 
+import com.google.common.collect.Sets;
 import com.linagora.tmail.team.TeamMailbox;
 
 public class TeamMailboxQuotaService {
     private final MaxQuotaManager maxQuotaManager;
+    private final QuotaManager quotaManager;
 
     @Inject
-    public TeamMailboxQuotaService(MaxQuotaManager maxQuotaManager) {
+    public TeamMailboxQuotaService(MaxQuotaManager maxQuotaManager, QuotaManager quotaManager) {
         this.maxQuotaManager = maxQuotaManager;
+        this.quotaManager = quotaManager;
     }
 
     public Optional<QuotaCountLimit> getMaxCountQuota(TeamMailbox teamMailbox) throws MailboxException {
@@ -62,5 +70,40 @@ public class TeamMailboxQuotaService {
         } catch (MailboxException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public TeamMailboxQuotaDetailsDTO getQuota(TeamMailbox teamMailbox) throws MailboxException {
+        QuotaRoot quotaRoot = teamMailbox.quotaRoot();
+        QuotaManager.Quotas quotas = quotaManager.getQuotas(quotaRoot);
+        TeamMailboxQuotaDetailsDTO.Builder quotaDetails = TeamMailboxQuotaDetailsDTO.builder()
+            .occupation(quotas.getStorageQuota(),
+                quotas.getMessageQuota());
+
+        mergeMaps(
+            maxQuotaManager.listMaxMessagesDetails(quotaRoot),
+            maxQuotaManager.listMaxStorageDetails(quotaRoot))
+            .forEach(quotaDetails::valueForScope);
+
+        quotaDetails.computed(computedQuota(quotaRoot));
+        return quotaDetails.build();
+    }
+
+    private Map<Quota.Scope, ValidatedQuotaDTO> mergeMaps(Map<Quota.Scope, QuotaCountLimit> counts, Map<Quota.Scope, QuotaSizeLimit> sizes) {
+        return Sets.union(counts.keySet(), sizes.keySet())
+            .stream()
+            .collect(Collectors.toMap(Function.identity(),
+                scope -> ValidatedQuotaDTO
+                    .builder()
+                    .count(Optional.ofNullable(counts.get(scope)))
+                    .size(Optional.ofNullable(sizes.get(scope)))
+                    .build()));
+    }
+
+    private ValidatedQuotaDTO computedQuota(QuotaRoot quotaRoot) throws MailboxException {
+        return ValidatedQuotaDTO
+            .builder()
+            .count(maxQuotaManager.getMaxMessage(quotaRoot))
+            .size(maxQuotaManager.getMaxStorage(quotaRoot))
+            .build();
     }
 }
