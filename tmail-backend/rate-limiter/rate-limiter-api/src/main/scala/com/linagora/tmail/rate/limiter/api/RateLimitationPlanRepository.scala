76480252aee6c8628ca1fb38be29f3ec4d647854
vttran <vttran@linagora.com>
2022-02-17 11:35:11 +0700

    ISSUE-296 Implement RateLimitingPlanRepository API and InMemory + contract (#333)

---
+++ tmail-backend/rate-limiter/rate-limiter-api/src/main/scala/com/linagora/tmail/rate/limiter/api/RateLimitationPlanRepository.scala
@@ -0,0 +1,49 @@
+package com.linagora.tmail.rate.limiter.api
+
+import org.reactivestreams.Publisher
+import reactor.core.scala.publisher.{SFlux, SMono}
+
+import scala.collection.concurrent.Map
+
+trait RateLimitationPlanRepository {
+
+  def create(creationRequest: RateLimitingPlanCreateRequest): Publisher[RateLimitingPlan]
+
+  def update(resetRequest: RateLimitingPlanResetRequest): Publisher[Unit]
+
+  def get(id: RateLimitingPlanId): Publisher[RateLimitingPlan]
+
+  def planExists(id: RateLimitingPlanId): Publisher[Boolean]
+
+  def list(): Publisher[RateLimitingPlan]
+}
+
+case class RateLimitingPlanNotFoundException() extends RuntimeException
+
+class InMemoryRateLimitationPlanRepository() extends RateLimitationPlanRepository {
+
+  val rateLimitingPlanStore: Map[RateLimitingPlanId, RateLimitingPlan] = scala.collection.concurrent.TrieMap()
+
+  override def create(creationRequest: RateLimitingPlanCreateRequest): Publisher[RateLimitingPlan] =
+    SMono.fromCallable(() => {
+      val rateLimitingPlan: RateLimitingPlan = RateLimitingPlan.from(creationRequest)
+      rateLimitingPlanStore.put(rateLimitingPlan.id, rateLimitingPlan)
+      rateLimitingPlan
+    })
+
+  override def update(resetRequest: RateLimitingPlanResetRequest): Publisher[Unit] =
+    SMono.justOrEmpty(rateLimitingPlanStore.get(resetRequest.id))
+      .switchIfEmpty(SMono.error(new RateLimitingPlanNotFoundException))
+      .map(rateLimitingPlan => rateLimitingPlanStore.update(resetRequest.id, RateLimitingPlan.update(rateLimitingPlan, resetRequest)))
+
+  override def get(id: RateLimitingPlanId): Publisher[RateLimitingPlan] =
+    SMono.justOrEmpty(rateLimitingPlanStore.get(id))
+      .switchIfEmpty(SMono.error(new RateLimitingPlanNotFoundException))
+
+
+  override def list(): Publisher[RateLimitingPlan] =
+    SFlux.fromIterable(rateLimitingPlanStore)
+      .map(_._2)
+
+  override def planExists(id: RateLimitingPlanId): Publisher[Boolean] = SMono.justOrEmpty(rateLimitingPlanStore.get(id)).hasElement
+}
---
+++ tmail-backend/rate-limiter/rate-limiter-api/src/test/scala/com/linagora/tmail/rate/limiter/api/RateLimitationPlanRepositoryContract.scala
@@ -0,0 +1,206 @@
+package com.linagora.tmail.rate.limiter.api
+
+import com.linagora.tmail.rate.limiter.api.LimitTypes.LimitTypes
+import com.linagora.tmail.rate.limiter.api.RateLimitationPlanRepositoryContract.{CREATION_REQUEST, CREATION_REQUEST_WITH_MULTI_OPERATIONS, RESET_REQUEST}
+import eu.timepit.refined.auto._
+import org.apache.james.rate.limiter.api.AllowedQuantity
+import org.assertj.core.api.Assertions.{assertThat, assertThatCode, assertThatThrownBy}
+import org.assertj.core.api.SoftAssertions
+import org.junit.jupiter.api.{BeforeEach, Test}
+import reactor.core.scala.publisher.{SFlux, SMono}
+
+import java.time.Duration
+import scala.jdk.CollectionConverters._
+
+case class TestPJ(name: String, limits: LimitTypes)
+
+object RateLimitationPlanRepositoryContract {
+  val COUNT: Count = Count(AllowedQuantity.liftOrThrow(1))
+  val SIZE: Size = Size(AllowedQuantity.liftOrThrow(10000))
+  val LIMIT_TYPES: LimitTypes = LimitTypes.liftOrThrow(Set(COUNT, SIZE))
+  val RATE_LIMITATION: RateLimitation = RateLimitation("name1", Duration.ofMinutes(1), LIMIT_TYPES)
+  val TRANSIT_LIMITS: TransitLimitations = TransitLimitations(Seq(RATE_LIMITATION))
+  val RELAY_LIMITS: RelayLimitations = RelayLimitations(Seq(RATE_LIMITATION))
+
+  val CREATION_REQUEST: RateLimitingPlanCreateRequest = RateLimitingPlanCreateRequest(
+    name = "name1",
+    operationLimitations = OperationLimitationsType.liftOrThrow(Seq(TRANSIT_LIMITS)))
+  val RESET_REQUEST: RateLimitingPlanResetRequest = RateLimitingPlanResetRequest(
+    id = RateLimitingPlanId.generate,
+    name = "new name",
+    operationLimitations = OperationLimitationsType.liftOrThrow(Seq(TRANSIT_LIMITS)))
+
+  val CREATION_REQUEST_WITH_MULTI_OPERATIONS: RateLimitingPlanCreateRequest = RateLimitingPlanCreateRequest(
+    name = "complex_plan",
+    operationLimitations = OperationLimitationsType.liftOrThrow(Seq(
+      TransitLimitations(Seq(
+        RateLimitation(name = "limit1",
+          period = Duration.ofMinutes(1),
+          limits = LimitTypes.liftOrThrow(Set(Count(AllowedQuantity.liftOrThrow(1)), Size(AllowedQuantity.liftOrThrow(10000))))),
+        RateLimitation(name = "limit2",
+          period = Duration.ofMinutes(2),
+          limits = LimitTypes.liftOrThrow(Set(Count(AllowedQuantity.liftOrThrow(2)), Size(AllowedQuantity.liftOrThrow(20000))))))),
+      RelayLimitations(Seq(
+        RateLimitation(name = "limit3",
+          period = Duration.ofMinutes(3),
+          limits = LimitTypes.liftOrThrow(Set(Count(AllowedQuantity.liftOrThrow(3)), Size(AllowedQuantity.liftOrThrow(30000))))),
+        RateLimitation(name = "limit4",
+          period = Duration.ofMinutes(4),
+          limits = LimitTypes.liftOrThrow(Set(Count(AllowedQuantity.liftOrThrow(4)), Size(AllowedQuantity.liftOrThrow(40000))))))))))
+}
+
+trait RateLimitationPlanRepositoryContract {
+  def testee: RateLimitationPlanRepository
+
+  @Test
+  def createShouldReturnRateLimitingPlan(): Unit = {
+    val rateLimitingPlan: RateLimitingPlan = SMono.fromPublisher(testee.create(CREATION_REQUEST))
+      .block()
+
+    SoftAssertions.assertSoftly(softly => {
+      softly.assertThat(rateLimitingPlan.id).isNotNull
+      softly.assertThat(rateLimitingPlan.operationLimitations.asJava)
+        .containsExactlyInAnyOrderElementsOf(CREATION_REQUEST.operationLimitations.value.asJava)
+      softly.assertThat(rateLimitingPlan.name).isEqualTo(CREATION_REQUEST.name)
+    })
+  }
+
+  @Test
+  def createShouldStoreEntry(): Unit = {
+    val rateLimitingPlan: RateLimitingPlan = SMono.fromPublisher(testee.create(CREATION_REQUEST))
+      .block()
+
+    assertThat(SMono.fromPublisher(testee.get(rateLimitingPlan.id))
+      .block())
+      .isEqualTo(rateLimitingPlan)
+  }
+
+  @Test
+  def createShouldReturnDifferentEntry(): Unit = {
+    val rateLimitingPlan: RateLimitingPlan = SMono.fromPublisher(testee.create(CREATION_REQUEST))
+      .block()
+
+    assertThat(SMono.fromPublisher(testee.create(CREATION_REQUEST))
+      .block())
+      .isNotEqualTo(rateLimitingPlan)
+  }
+
+  @Test
+  def createShouldWorkWhenHaveSeveralOperationLimitations(): Unit = {
+    val rateLimitingPlan: RateLimitingPlan = SMono.fromPublisher(testee.create(CREATION_REQUEST_WITH_MULTI_OPERATIONS))
+      .block()
+
+    assertThat(SFlux.fromPublisher(testee.list()).collectSeq().block().asJava)
+      .containsExactlyInAnyOrder(rateLimitingPlan)
+  }
+
+  @Test
+  def updateShouldThrowWhenIdNotFound(): Unit = {
+    assertThatThrownBy(() => SMono.fromPublisher(testee.update(RESET_REQUEST)).block())
+      .isInstanceOf(classOf[RateLimitingPlanNotFoundException])
+  }
+
+  @Test
+  def updateShouldNotThrowWhenIdExists(): Unit = {
+    val rateLimitingPlan: RateLimitingPlan = SMono.fromPublisher(testee.create(CREATION_REQUEST))
+      .block()
+
+    assertThatCode(() => SMono.fromPublisher(testee.update(RESET_REQUEST.copy(id = rateLimitingPlan.id))).block())
+      .doesNotThrowAnyException()
+  }
+
+  @Test
+  def updateShouldModifyEntry(): Unit = {
+    val rateLimitingPlan: RateLimitingPlan = SMono.fromPublisher(testee.create(CREATION_REQUEST))
+      .block()
+
+    SMono.fromPublisher(testee.update(RESET_REQUEST.copy(id = rateLimitingPlan.id))).block()
+
+    assertThat(SMono.fromPublisher(testee.get(rateLimitingPlan.id)).block())
+      .isEqualTo(RateLimitingPlan(
+        id = rateLimitingPlan.id,
+        name = RESET_REQUEST.name,
+        operationLimitations = RESET_REQUEST.operationLimitations))
+  }
+
+  @Test
+  def updateShouldNotModifyAnotherEntry(): Unit = {
+    val rateLimitingPlan: RateLimitingPlan = SMono.fromPublisher(testee.create(CREATION_REQUEST))
+      .block()
+    val rateLimitingPlan2: RateLimitingPlan = SMono.fromPublisher(testee.create(CREATION_REQUEST))
+      .block()
+
+    SMono.fromPublisher(testee.update(RESET_REQUEST.copy(id = rateLimitingPlan.id))).block()
+
+    assertThat(SMono.fromPublisher(testee.get(rateLimitingPlan2.id))
+      .block())
+      .isEqualTo(rateLimitingPlan2)
+  }
+
+  @Test
+  def getShouldThrowWhenIdNotFound(): Unit = {
+    assertThatThrownBy(() => SMono.fromPublisher(testee.get(RateLimitingPlanId.generate)).block())
+      .isInstanceOf(classOf[RateLimitingPlanNotFoundException])
+  }
+
+  @Test
+  def getShouldReturnEntryWhenIdExists(): Unit = {
+    val rateLimitingPlan: RateLimitingPlan = SMono.fromPublisher(testee.create(CREATION_REQUEST))
+      .block()
+
+    assertThat(SMono.fromPublisher(testee.get(rateLimitingPlan.id)).block())
+      .isEqualTo(rateLimitingPlan)
+  }
+
+  @Test
+  def planExistsShouldReturnFalseByDefault(): Unit = {
+    assertThat(SMono.fromPublisher(testee.planExists(RateLimitingPlanId.generate)).block())
+      .isEqualTo(false)
+  }
+
+  @Test
+  def planExistsShouldReturnTrueWhenPlanExists(): Unit = {
+    val rateLimitingPlan: RateLimitingPlan = SMono.fromPublisher(testee.create(CREATION_REQUEST))
+      .block()
+
+    assertThat(SMono.fromPublisher(testee.planExists(rateLimitingPlan.id)).block())
+      .isEqualTo(true)
+  }
+
+  @Test
+  def listShouldReturnEmptyByDefault(): Unit = {
+    assertThat(SFlux.fromPublisher(testee.list()).collectSeq().block().asJava)
+      .isEmpty()
+  }
+
+  @Test
+  def listShouldReturnStoredEntriesWhenHasSingleElement(): Unit = {
+    val rateLimitingPlan: RateLimitingPlan = SMono.fromPublisher(testee.create(CREATION_REQUEST))
+      .block()
+
+    assertThat(SFlux.fromPublisher(testee.list()).collectSeq().block().asJava)
+      .containsExactlyInAnyOrder(rateLimitingPlan)
+  }
+
+  @Test
+  def listShouldReturnStoredEntriesWhenHasSeveralElement(): Unit = {
+    val rateLimitingPlan: RateLimitingPlan = SMono.fromPublisher(testee.create(CREATION_REQUEST))
+      .block()
+    val rateLimitingPlan2: RateLimitingPlan = SMono.fromPublisher(testee.create(CREATION_REQUEST_WITH_MULTI_OPERATIONS))
+      .block()
+
+    assertThat(SFlux.fromPublisher(testee.list()).collectSeq().block().asJava)
+      .containsExactlyInAnyOrder(rateLimitingPlan, rateLimitingPlan2)
+  }
+}
+
+class InMemoryRateLimitationPlanRepositoryTest extends RateLimitationPlanRepositoryContract {
+  var inMemoryRateLimitationPlanRepository: InMemoryRateLimitationPlanRepository = _
+
+  override def testee: RateLimitationPlanRepository = inMemoryRateLimitationPlanRepository
+
+  @BeforeEach
+  def beforeEach(): Unit = {
+    inMemoryRateLimitationPlanRepository = new InMemoryRateLimitationPlanRepository();
+  }
+}
