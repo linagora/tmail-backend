928905ba780ab7f5f287294cec9439a9ef8151b1
vttran <vttran@linagora.com>
2022-04-01 18:04:08 +0700

    ISSUE 371 - Mailet to collect contacts (#387)

---
+++ tmail-backend/mailets/src/main/scala/com/linagora/tmail/mailets/ContactsCollection.scala
@@ -0,0 +1,89 @@
+package com.linagora.tmail.mailets
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
+import com.google.common.collect.{ImmutableList, ImmutableSet}
+import com.linagora.tmail.james.jmap.contact.{ContactFields, TmailContactUserAddedEvent}
+import javax.inject.Inject
+import javax.mail.Message
+import javax.mail.internet.InternetAddress
+import org.apache.commons.collections.CollectionUtils
+import org.apache.james.core.{MailAddress, Username}
+import org.apache.james.events.Event.EventId
+import org.apache.james.events.{EventBus, RegistrationKey}
+import org.apache.james.mime4j.util.MimeUtil
+import org.apache.james.transport.mailets.ContactExtractor.ExtractedContacts
+import org.apache.mailet.base.GenericMailet
+import org.apache.mailet.{Attribute, AttributeName, AttributeValue, Mail, MailetException}
+import reactor.core.scala.publisher.{SFlux, SMono}
+
+import scala.jdk.CollectionConverters._
+import scala.jdk.OptionConverters._
+
+/**
+ * <p><b>ContactsCollection</b> allows extracting the recipient's contact of a message
+ *  and dispatch ContactUserAddedEvent to eventBus, in order to index contact asynchronously.
+ *  This mailet also stores them as JSON in a specified message attribute.</p>
+ *
+ * <p>Here is the JSON format:</p>
+ * <pre><code>
+ * {
+ *   "userEmail" : "sender@james.org",
+ *   "emails" : [ "to@james.org", "cc@james.org" ]
+ * }
+ * </code></pre>
+ *
+ * <p>Sample configuration:</p>
+ *
+ * <pre><code>
+ * &lt;mailet match="All" class="ContactsCollection"&gt;
+ *   &lt;attribute&gt;ExtractedContacts&lt;/attribute&gt;
+ * &lt;/mailet&gt;
+ * </code></pre>
+ */
+
+class ContactsCollection @Inject()(eventBus: EventBus) extends GenericMailet {
+
+  private val NO_REGISTRATION_KEYS: ImmutableSet[RegistrationKey] = ImmutableSet.of
+  private val OBJECT_MAPPER: ObjectMapper = new ObjectMapper().registerModule(new Jdk8Module)
+  private var attributeName: AttributeName = _
+
+  override def init(): Unit =
+    attributeName = Option(getInitParameter("attribute"))
+      .map(AttributeName.of)
+      .getOrElse(throw new MailetException("No value for `attribute` parameter was provided."))
+
+  override def service(mail: Mail): Unit =
+    if (CollectionUtils.isNotEmpty(mail.getRecipients)) {
+      val contacts: Seq[ContactFields] = extractRecipientsContacts(mail)
+      dispatchEvents(contacts)
+      appendAttributeToMail(mail, contacts)
+    }
+
+  private def extractRecipientsContacts(mail: Mail): Seq[ContactFields] =
+    Seq(Message.RecipientType.TO, Message.RecipientType.CC, Message.RecipientType.BCC)
+      .flatMap(recipientType => Option(mail.getMessage.getHeader(recipientType.toString, ",")))
+      .flatMap(header => InternetAddress.parseHeader(header, false).toSeq)
+      .map(extractContactField)
+
+  private def extractContactField(internetAddress: InternetAddress): ContactFields =
+    ContactFields(new MailAddress(internetAddress), firstname = Option(internetAddress.getPersonal).getOrElse(""))
+
+  private def dispatchEvents(contacts: Seq[ContactFields]): Unit =
+    SFlux.fromIterable(contacts)
+      .flatMap(contact => SMono.fromPublisher(eventBus.dispatch(TmailContactUserAddedEvent(
+        eventId = EventId.random(),
+        username = Username.fromMailAddress(contact.address),
+        contact = contact),
+        NO_REGISTRATION_KEYS)))
+      .collectSeq()
+      .block()
+
+  private def appendAttributeToMail(mail: Mail, contacts: Seq[ContactFields]): Unit =
+    mail.getMaybeSender.asOptional().toScala
+      .map(mailAddress => new ExtractedContacts(mailAddress.asString, ImmutableList.copyOf(contacts.map(_.address.asString()).asJava)))
+      .map(extractedContact => OBJECT_MAPPER.writeValueAsString(extractedContact))
+      .foreach(extractedContactJson => mail.setAttribute(new Attribute(attributeName, AttributeValue.of(extractedContactJson))))
+
+}
+
