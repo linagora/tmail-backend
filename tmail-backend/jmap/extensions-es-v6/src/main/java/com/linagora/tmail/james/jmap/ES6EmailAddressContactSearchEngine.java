a1bac42570a65b9f39c172599d6a3f7d4ca21f66
Quan Tran <hqtran@linagora.com>
2022-04-07 11:21:03 +0700

    ISSUE-328: ES 6 implementation of EmailAddress Search engine

---
+++ tmail-backend/jmap/extensions-es-v6/src/main/java/com/linagora/tmail/james/jmap/ES6EmailAddressContactSearchEngine.java
@@ -0,0 +1,110 @@
+package com.linagora.tmail.james.jmap;
+
+import static com.linagora.tmail.james.jmap.ContactMappingFactory.ACCOUNT_ID;
+import static com.linagora.tmail.james.jmap.ContactMappingFactory.CONTACT_ID;
+import static com.linagora.tmail.james.jmap.ContactMappingFactory.DOMAIN;
+import static com.linagora.tmail.james.jmap.ContactMappingFactory.EMAIL;
+import static com.linagora.tmail.james.jmap.ContactMappingFactory.FIRSTNAME;
+import static com.linagora.tmail.james.jmap.ContactMappingFactory.SURNAME;
+
+import java.util.UUID;
+
+import javax.inject.Inject;
+import javax.mail.internet.AddressException;
+
+import org.apache.james.backends.es.DocumentId;
+import org.apache.james.backends.es.ElasticSearchIndexer;
+import org.apache.james.backends.es.ReactorElasticSearchClient;
+import org.apache.james.backends.es.RoutingKey;
+import org.apache.james.core.Domain;
+import org.apache.james.core.MailAddress;
+import org.apache.james.core.Username;
+import org.apache.james.jmap.api.model.AccountId;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.reactivestreams.Publisher;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.github.fge.lambdas.Throwing;
+import com.google.common.collect.ImmutableList;
+import com.linagora.tmail.james.jmap.contact.ContactFields;
+import com.linagora.tmail.james.jmap.contact.EmailAddressContact;
+import com.linagora.tmail.james.jmap.contact.EmailAddressContactSearchEngine;
+import com.linagora.tmail.james.jmap.dto.DomainContactDocument;
+import com.linagora.tmail.james.jmap.dto.UserContactDocument;
+
+import reactor.core.publisher.Mono;
+
+public class ES6EmailAddressContactSearchEngine implements EmailAddressContactSearchEngine {
+    private static final String DELIMITER = ":";
+
+    private final ElasticSearchIndexer userContactIndexer;
+    private final ElasticSearchIndexer domainContactIndexer;
+    private final ReactorElasticSearchClient client;
+    private final ElasticSearchContactConfiguration configuration;
+    private final ObjectMapper mapper;
+
+    @Inject
+    public ES6EmailAddressContactSearchEngine(ReactorElasticSearchClient client, ElasticSearchContactConfiguration contactConfiguration) {
+        this.client = client;
+        this.userContactIndexer = new ElasticSearchIndexer(client, contactConfiguration.getUserContactWriteAliasName());
+        this.domainContactIndexer = new ElasticSearchIndexer(client, contactConfiguration.getDomainContactWriteAliasName());
+        this.configuration = contactConfiguration;
+        this.mapper = new ObjectMapper().registerModule(new GuavaModule()).registerModule(new Jdk8Module());
+    }
+
+    @Override
+    public Publisher<EmailAddressContact> index(AccountId accountId, ContactFields fields) {
+        EmailAddressContact emailAddressContact = EmailAddressContact.of(fields);
+        return Mono.fromCallable(() -> mapper.writeValueAsString(new UserContactDocument(accountId, emailAddressContact)))
+            .flatMap(content -> userContactIndexer.index(computeUserContactDocumentId(accountId, fields.address()), content,
+                RoutingKey.fromString(fields.address().asString())))
+            .thenReturn(emailAddressContact);
+    }
+
+    @Override
+    public Publisher<EmailAddressContact> index(Domain domain, ContactFields fields) {
+        EmailAddressContact emailAddressContact = EmailAddressContact.of(fields);
+        return Mono.fromCallable(() -> mapper.writeValueAsString(new DomainContactDocument(domain, emailAddressContact)))
+            .flatMap(content -> domainContactIndexer.index(computeDomainContactDocumentId(domain, fields.address()), content,
+                RoutingKey.fromString(fields.address().asString())))
+            .thenReturn(emailAddressContact);
+    }
+
+    @Override
+    public Publisher<EmailAddressContact> autoComplete(AccountId accountId, String part) {
+        SearchRequest request = new SearchRequest(configuration.getUserContactReadAliasName().getValue(), configuration.getDomainContactReadAliasName().getValue())
+            .source(new SearchSourceBuilder()
+                .query(QueryBuilders.boolQuery()
+                    .must(QueryBuilders.multiMatchQuery(part, EMAIL, FIRSTNAME, SURNAME))
+                    .should(QueryBuilders.termQuery(ACCOUNT_ID, accountId.getIdentifier()))
+                    .should(QueryBuilders.termQuery(DOMAIN, Username.of(accountId.getIdentifier()).getDomainPart()
+                        .map(Domain::asString)
+                        .orElse("")))
+                    .minimumShouldMatch(1)));
+
+        return client.search(request, RequestOptions.DEFAULT)
+            .flatMapIterable(searchResponse -> ImmutableList.copyOf(searchResponse.getHits().getHits()))
+            .map(Throwing.function(this::extractContentFromHit).sneakyThrow());
+    }
+
+    private DocumentId computeUserContactDocumentId(AccountId accountId, MailAddress mailAddress) {
+        return DocumentId.fromString(String.join(DELIMITER, accountId.getIdentifier(), mailAddress.asString()));
+    }
+
+    private DocumentId computeDomainContactDocumentId(Domain domain, MailAddress mailAddress) {
+        return DocumentId.fromString(String.join(DELIMITER, domain.asString(), mailAddress.asString()));
+    }
+
+    private EmailAddressContact extractContentFromHit(SearchHit hit) throws AddressException {
+        return new EmailAddressContact(UUID.fromString((String) hit.getSourceAsMap().get(CONTACT_ID)),
+            new ContactFields(new MailAddress(((String) hit.getSourceAsMap().get(EMAIL))),
+                (String) hit.getSourceAsMap().get(FIRSTNAME),
+                (String) hit.getSourceAsMap().get(SURNAME)));
+    }
+}
---
+++ tmail-backend/jmap/extensions-es-v6/src/main/java/com/linagora/tmail/james/jmap/ElasticSearchContactConfiguration.java
@@ -0,0 +1,238 @@
+package com.linagora.tmail.james.jmap;
+
+import java.util.Objects;
+import java.util.Optional;
+
+import org.apache.commons.configuration2.Configuration;
+import org.apache.james.backends.es.IndexName;
+import org.apache.james.backends.es.ReadAliasName;
+import org.apache.james.backends.es.WriteAliasName;
+
+public class ElasticSearchContactConfiguration {
+
+    public static class Builder {
+        private Optional<IndexName> userContactIndexName;
+        private Optional<IndexName> domainContactIndexName;
+        private Optional<ReadAliasName> userContactReadAliasName;
+        private Optional<WriteAliasName> userContactWriteAliasName;
+        private Optional<ReadAliasName> domainContactReadAliasName;
+        private Optional<WriteAliasName> domainContactWriteAliasName;
+        private Optional<Integer> maxNgram;
+        private Optional<Integer> minNgram;
+
+        Builder() {
+            userContactIndexName = Optional.empty();
+            domainContactIndexName = Optional.empty();
+            userContactReadAliasName = Optional.empty();
+            userContactWriteAliasName = Optional.empty();
+            domainContactReadAliasName = Optional.empty();
+            domainContactWriteAliasName = Optional.empty();
+            maxNgram = Optional.empty();
+            minNgram = Optional.empty();
+        }
+
+        Builder userContactIndexName(Optional<IndexName> userContactIndexName) {
+            this.userContactIndexName = userContactIndexName;
+            return this;
+        }
+
+        Builder domainContactIndexName(Optional<IndexName> domainContactIndexName) {
+            this.domainContactIndexName = domainContactIndexName;
+            return this;
+        }
+
+        Builder userContactReadAliasName(Optional<ReadAliasName> userContactReadAliasName) {
+            this.userContactReadAliasName = userContactReadAliasName;
+            return this;
+        }
+
+        Builder userContactWriteAliasName(Optional<WriteAliasName> userContactWriteAliasName) {
+            this.userContactWriteAliasName = userContactWriteAliasName;
+            return this;
+        }
+
+        Builder domainContactReadAliasName(Optional<ReadAliasName> domainContactReadAliasName) {
+            this.domainContactReadAliasName = domainContactReadAliasName;
+            return this;
+        }
+
+        Builder domainContactWriteAliasName(Optional<WriteAliasName> domainContactWriteAliasName) {
+            this.domainContactWriteAliasName = domainContactWriteAliasName;
+            return this;
+        }
+
+        Builder maxNgram(Optional<Integer> maxNgramDiff) {
+            this.maxNgram = maxNgramDiff;
+            return this;
+        }
+
+        Builder minNgram(Optional<Integer> minNgram) {
+            this.minNgram = minNgram;
+            return this;
+        }
+
+        public ElasticSearchContactConfiguration build() {
+            return new ElasticSearchContactConfiguration(
+                userContactIndexName.orElse(DEFAULT_INDEX_USER_CONTACT_NAME),
+                domainContactIndexName.orElse(DEFAULT_INDEX_DOMAIN_CONTACT_NAME),
+                userContactReadAliasName.orElse(DEFAULT_ALIAS_READ_USER_CONTACT_NAME),
+                userContactWriteAliasName.orElse(DEFAULT_ALIAS_WRITE_USER_CONTACT_NAME),
+                domainContactReadAliasName.orElse(DEFAULT_ALIAS_READ_DOMAIN_CONTACT_NAME),
+                domainContactWriteAliasName.orElse(DEFAULT_ALIAS_WRITE_DOMAIN_CONTACT_NAME),
+                maxNgram.orElse(DEFAULT_MAX_NGRAM),
+                minNgram.orElse(DEFAULT_MIN_NGRAM));
+        }
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    private static final String ELASTICSEARCH_INDEX_USER_CONTACT_NAME = "elasticsearch.index.contact.user.name";
+    private static final String ELASTICSEARCH_INDEX_DOMAIN_CONTACT_NAME = "elasticsearch.index.contact.domain.name";
+    private static final String ELASTICSEARCH_ALIAS_READ_USER_CONTACT_NAME = "elasticsearch.alias.read.contact.user.name";
+    private static final String ELASTICSEARCH_ALIAS_WRITE_USER_CONTACT_NAME = "elasticsearch.alias.write.contact.user.name";
+    private static final String ELASTICSEARCH_ALIAS_READ_DOMAIN_CONTACT_NAME = "elasticsearch.alias.read.contact.domain.name";
+    private static final String ELASTICSEARCH_ALIAS_WRITE_DOMAIN_CONTACT_NAME = "elasticsearch.alias.write.contact.domain.name";
+    private static final String ELASTICSEARCH_INDEX_CONTACT_MAX_NGRAM = "elasticsearch.index.contact.max.ngram";
+    private static final String ELASTICSEARCH_INDEX_CONTACT_MIN_NGRAM = "elasticsearch.index.contact.min.ngram";
+
+    public static final IndexName DEFAULT_INDEX_USER_CONTACT_NAME = new IndexName("user_contact");
+    public static final IndexName DEFAULT_INDEX_DOMAIN_CONTACT_NAME = new IndexName("domain_contact");
+    public static final WriteAliasName DEFAULT_ALIAS_WRITE_USER_CONTACT_NAME = new WriteAliasName("user_contact_write_alias");
+    public static final ReadAliasName DEFAULT_ALIAS_READ_USER_CONTACT_NAME = new ReadAliasName("user_contact_read_alias");
+    public static final WriteAliasName DEFAULT_ALIAS_WRITE_DOMAIN_CONTACT_NAME = new WriteAliasName("domain_contact_write_alias");
+    public static final ReadAliasName DEFAULT_ALIAS_READ_DOMAIN_CONTACT_NAME = new ReadAliasName("domain_contact_read_alias");
+    public static final Integer DEFAULT_MAX_NGRAM = 30;
+    public static final Integer DEFAULT_MIN_NGRAM = 3;
+
+    public static final ElasticSearchContactConfiguration DEFAULT_CONFIGURATION = builder().build();
+
+    public static ElasticSearchContactConfiguration fromProperties(Configuration configuration) {
+        return builder()
+            .userContactIndexName(computeUserContactIndexName(configuration))
+            .domainContactIndexName(computeDomainContactIndexName(configuration))
+            .userContactReadAliasName(computeUserContactReadAlias(configuration))
+            .userContactWriteAliasName(computeUserContactWriteAlias(configuration))
+            .domainContactReadAliasName(computeDomainContactReadAlias(configuration))
+            .domainContactWriteAliasName(computeDomainContactWriteAlias(configuration))
+            .maxNgram(computeMaxNgram(configuration))
+            .minNgram(computeMinNgram(configuration))
+            .build();
+    }
+
+    static Optional<IndexName> computeUserContactIndexName(Configuration configuration) {
+        return Optional.ofNullable(configuration.getString(ELASTICSEARCH_INDEX_USER_CONTACT_NAME))
+                .map(IndexName::new);
+    }
+
+    static Optional<IndexName> computeDomainContactIndexName(Configuration configuration) {
+        return Optional.ofNullable(configuration.getString(ELASTICSEARCH_INDEX_DOMAIN_CONTACT_NAME))
+            .map(IndexName::new);
+    }
+
+    static Optional<WriteAliasName> computeUserContactWriteAlias(Configuration configuration) {
+        return Optional.ofNullable(configuration.getString(ELASTICSEARCH_ALIAS_WRITE_USER_CONTACT_NAME))
+                .map(WriteAliasName::new);
+    }
+
+    static Optional<ReadAliasName> computeUserContactReadAlias(Configuration configuration) {
+        return Optional.ofNullable(configuration.getString(ELASTICSEARCH_ALIAS_READ_USER_CONTACT_NAME))
+                .map(ReadAliasName::new);
+    }
+
+    static Optional<WriteAliasName> computeDomainContactWriteAlias(Configuration configuration) {
+        return Optional.ofNullable(configuration.getString(ELASTICSEARCH_ALIAS_WRITE_DOMAIN_CONTACT_NAME))
+            .map(WriteAliasName::new);
+    }
+
+    static Optional<ReadAliasName> computeDomainContactReadAlias(Configuration configuration) {
+        return Optional.ofNullable(configuration.getString(ELASTICSEARCH_ALIAS_READ_DOMAIN_CONTACT_NAME))
+            .map(ReadAliasName::new);
+    }
+
+    static Optional<Integer> computeMaxNgram(Configuration configuration) {
+        return Optional.ofNullable(configuration.getInteger(ELASTICSEARCH_INDEX_CONTACT_MAX_NGRAM, null));
+    }
+
+    static Optional<Integer> computeMinNgram(Configuration configuration) {
+        return Optional.ofNullable(configuration.getInteger(ELASTICSEARCH_INDEX_CONTACT_MIN_NGRAM, null));
+    }
+
+    private final IndexName userContactIndexName;
+    private final IndexName domainContactIndexName;
+    private final ReadAliasName userContactReadAliasName;
+    private final WriteAliasName userContactWriteAliasName;
+    private final ReadAliasName domainContactReadAliasName;
+    private final WriteAliasName domainContactWriteAliasName;
+    private final int maxNgram;
+    private final int minNgram;
+
+    private ElasticSearchContactConfiguration(IndexName userContactIndexName, IndexName domainContactIndexName, ReadAliasName userContactReadAliasName,
+                                              WriteAliasName userContactWriteAliasName, ReadAliasName domainContactReadAliasName, WriteAliasName domainContactWriteAliasName,
+                                              int maxNgram, int minNgram) {
+        this.userContactIndexName = userContactIndexName;
+        this.domainContactIndexName = domainContactIndexName;
+        this.userContactReadAliasName = userContactReadAliasName;
+        this.userContactWriteAliasName = userContactWriteAliasName;
+        this.domainContactReadAliasName = domainContactReadAliasName;
+        this.domainContactWriteAliasName = domainContactWriteAliasName;
+        this.maxNgram = maxNgram;
+        this.minNgram = minNgram;
+    }
+
+    public IndexName getUserContactIndexName() {
+        return userContactIndexName;
+    }
+
+    public IndexName getDomainContactIndexName() {
+        return domainContactIndexName;
+    }
+
+    public ReadAliasName getUserContactReadAliasName() {
+        return userContactReadAliasName;
+    }
+
+    public WriteAliasName getUserContactWriteAliasName() {
+        return userContactWriteAliasName;
+    }
+
+    public ReadAliasName getDomainContactReadAliasName() {
+        return domainContactReadAliasName;
+    }
+
+    public WriteAliasName getDomainContactWriteAliasName() {
+        return domainContactWriteAliasName;
+    }
+
+    public int getMaxNgram() {
+        return maxNgram;
+    }
+
+    public int getMinNgram() {
+        return minNgram;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (o instanceof ElasticSearchContactConfiguration) {
+            ElasticSearchContactConfiguration that = (ElasticSearchContactConfiguration) o;
+
+            return Objects.equals(this.userContactIndexName, that.userContactIndexName)
+                && Objects.equals(this.domainContactIndexName, that.domainContactIndexName)
+                && Objects.equals(this.userContactReadAliasName, that.userContactReadAliasName)
+                && Objects.equals(this.userContactWriteAliasName, that.userContactWriteAliasName)
+                && Objects.equals(this.domainContactReadAliasName, that.domainContactReadAliasName)
+                && Objects.equals(this.domainContactWriteAliasName, that.domainContactWriteAliasName)
+                && Objects.equals(this.maxNgram, that.maxNgram)
+                && Objects.equals(this.minNgram, that.minNgram);
+        }
+        return false;
+    }
+
+    @Override
+    public final int hashCode() {
+        return Objects.hash(userContactIndexName, domainContactIndexName, userContactReadAliasName, userContactWriteAliasName,
+            domainContactReadAliasName, domainContactWriteAliasName, maxNgram, minNgram);
+    }
+}
