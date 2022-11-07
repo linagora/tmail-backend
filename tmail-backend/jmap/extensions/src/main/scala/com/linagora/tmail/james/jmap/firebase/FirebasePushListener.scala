17174db5142fe01218c10e345245ca74d6a71963
Quan Tran <hqtran@linagora.com>
2022-11-07 16:37:53 +0700

    ISSUE-501 FirebasePushListener

---
+++ tmail-backend/jmap/extensions/src/main/scala/com/linagora/tmail/james/jmap/firebase/FirebasePushListener.scala
@@ -0,0 +1,63 @@
+package com.linagora.tmail.james.jmap.firebase
+
+import com.google.firebase.messaging.{FirebaseMessagingException, MessagingErrorCode}
+import com.linagora.tmail.james.jmap.firebase.FirebasePushListener.{GROUP, LOGGER}
+import com.linagora.tmail.james.jmap.model.FirebaseSubscription
+import javax.inject.Inject
+import org.apache.james.events.EventListener.ReactiveGroupEventListener
+import org.apache.james.events.{Event, Group}
+import org.apache.james.jmap.api.model.TypeName
+import org.apache.james.jmap.change.StateChangeEvent
+import org.apache.james.jmap.core.{AccountId, StateChange}
+import org.apache.james.util.ReactorUtils
+import org.reactivestreams.Publisher
+import org.slf4j.LoggerFactory
+import reactor.core.scala.publisher.{SFlux, SMono}
+
+import scala.jdk.javaapi.CollectionConverters
+
+case class FirebasePushListenerGroup() extends Group {}
+
+object FirebasePushListener {
+  val GROUP: FirebasePushListenerGroup = FirebasePushListenerGroup()
+  private val LOGGER = LoggerFactory.getLogger(classOf[FirebasePushListener])
+}
+
+class FirebasePushListener @Inject()(subscriptionRepository: FirebaseSubscriptionRepository,
+                                     pushClient: FirebasePushClient) extends ReactiveGroupEventListener {
+  override def getDefaultGroup: Group = GROUP
+
+  override def isHandling(event: Event): Boolean = event.isInstanceOf[StateChangeEvent]
+
+  override def reactiveEvent(event: Event): Publisher[Void] =
+    event match {
+      case event: StateChangeEvent =>
+        SFlux(subscriptionRepository.list(event.username))
+          .flatMap(sendNotification(_, event), ReactorUtils.DEFAULT_CONCURRENCY)
+          .`then`()
+      case _ => SMono.empty
+    }
+
+  private def sendNotification(subscription: FirebaseSubscription, stateChangeEvent: StateChangeEvent): Publisher[Unit] =
+    stateChangeEvent
+      .asStateChange
+      .filter(subscription.types.toSet)
+      .fold(SMono.empty[Unit])(stateChange => SMono(pushClient.push(asPushRequest(stateChange, subscription)))
+        .onErrorResume {
+          case e: FirebaseMessagingException => e.getMessagingErrorCode match {
+            case MessagingErrorCode.INVALID_ARGUMENT | MessagingErrorCode.UNREGISTERED => SMono.fromPublisher(subscriptionRepository.revoke(stateChangeEvent.username, subscription.id))
+              .`then`(SMono.fromPublisher(ReactorUtils.logAsMono(() => LOGGER.warn("Subscription with invalid FCM token is removed", e))))
+            case _ => SMono.fromPublisher(ReactorUtils.logAsMono(() => LOGGER.warn("Unexpected error during push message to Firebase Cloud Messaging", e)))
+          }
+          case e => SMono.fromPublisher(ReactorUtils.logAsMono(() => LOGGER.warn("Unexpected error during push message to Firebase Cloud Messaging", e)))
+        }
+        .`then`())
+
+  private def asPushRequest(stateChange: StateChange, subscription: FirebaseSubscription): FirebasePushRequest =
+    new FirebasePushRequest(CollectionConverters.asJava(stateChange.changes
+      .flatMap(accountIdToTypeState => accountIdToTypeState._2.changes
+        .map(typeNameToState => evaluateKey(accountIdToTypeState._1, typeNameToState._1) -> typeNameToState._2.serialize))), subscription.token)
+
+  private def evaluateKey(accountId: AccountId, typeName: TypeName): String =
+    accountId.id.value + ":" + typeName.asString()
+}
