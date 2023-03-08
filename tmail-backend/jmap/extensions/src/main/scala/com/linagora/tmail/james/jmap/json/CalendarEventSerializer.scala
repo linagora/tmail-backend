2bc9da14f7c99060a2612cdd6fb5006554557f10
Tung Tran <vttran@linagora.com>
2023-03-08 12:29:17 +0700

    ISSUE-570 JMAP CalendarEvent/parse for start, end, duration, timeZone, location

--- tmail-backend/jmap/extensions/src/main/scala/com/linagora/tmail/james/jmap/json/CalendarEventSerializer.scala
+++ tmail-backend/jmap/extensions/src/main/scala/com/linagora/tmail/james/jmap/json/CalendarEventSerializer.scala
@@ -1,11 +1,12 @@
 package com.linagora.tmail.james.jmap.json
 
-import com.google.firestore.v1.Write
-import com.linagora.tmail.james.jmap.model.{CalendarDescriptionField, CalendarEventNotFound, CalendarEventNotParsable, CalendarEventParseRequest, CalendarEventParseResponse, CalendarEventParsed, CalendarTitleField}
-import org.apache.james.jmap.core.SetError
+import com.linagora.tmail.james.jmap.model._
+import org.apache.james.jmap.core.{SetError, UTCDate}
 import org.apache.james.jmap.json.mapWrites
 import org.apache.james.jmap.mail.{BlobId, BlobIds}
-import play.api.libs.json.{Format, JsResult, JsValue, Json, Reads, Writes}
+import play.api.libs.json._
+
+import java.time.ZonedDateTime
 
 object CalendarEventSerializer {
 
@@ -15,6 +16,14 @@ object CalendarEventSerializer {
   private implicit val calendarEventNotParsableWrites: Writes[CalendarEventNotParsable] = Json.valueWrites[CalendarEventNotParsable]
   private implicit val calendarTitleFieldFormat: Writes[CalendarTitleField] = Json.valueWrites[CalendarTitleField]
   private implicit val calendarDescriptionFieldFormat: Writes[CalendarDescriptionField] = Json.valueWrites[CalendarDescriptionField]
+  private implicit val calendarLocationFieldFormat: Writes[CalendarLocationField] = Json.valueWrites[CalendarLocationField]
+  private implicit val timeStampFieldWrites: Writes[ZonedDateTime] = time => JsString(time.format(dateTimeFormatter))
+  private implicit val calendarStartFieldWrites: Writes[CalendarStartField] = Json.valueWrites[CalendarStartField]
+  private implicit val calendarEndFieldWrites: Writes[CalendarEndField] = Json.valueWrites[CalendarEndField]
+  private implicit val utcDateWrites : Writes[UTCDate] = utcDate => JsString(utcDate.asUTC.format(dateTimeUTCFormatter))
+  private implicit val calendarDurationWrites : Writes[CalendarDurationField] = duration => JsString(duration.value.toString)
+  private implicit val calendarTimeZoneFieldWrites: Writes[CalendarTimeZoneField] = timeZone => JsString(timeZone.value.getID)
+
   private implicit val calendarEventParsedWrites: Writes[CalendarEventParsed] = Json.writes[CalendarEventParsed]
   private implicit val parsedMapWrites: Writes[Map[BlobId, CalendarEventParsed]] = mapWrites[BlobId, CalendarEventParsed](s => s.value.value, calendarEventParsedWrites)
 
--- tmail-backend/jmap/extensions/src/main/scala/com/linagora/tmail/james/jmap/model/CalendarEventParse.scala
+++ tmail-backend/jmap/extensions/src/main/scala/com/linagora/tmail/james/jmap/model/CalendarEventParse.scala
@@ -2,13 +2,19 @@ package com.linagora.tmail.james.jmap.model
 
 import com.google.common.base.Preconditions
 import com.linagora.tmail.james.jmap.model.CalendarEventParse.UnparsedBlobId
+import com.linagora.tmail.james.jmap.model.CalendarStartField.getTimeZoneAlternative
 import eu.timepit.refined.api.Refined
-import net.fortuna.ical4j.model.Calendar
+import net.fortuna.ical4j.data.{CalendarBuilder, CalendarParserFactory, ContentHandlerContext}
 import net.fortuna.ical4j.model.component.VEvent
-import org.apache.james.jmap.core.{AccountId, Id}
+import net.fortuna.ical4j.model.{Calendar, Property, TimeZoneRegistryFactory}
+import org.apache.james.jmap.core.{AccountId, Id, UTCDate}
 import org.apache.james.jmap.mail.MDNParseRequest.MAXIMUM_NUMBER_OF_BLOB_IDS
 import org.apache.james.jmap.mail.{BlobId, BlobIds, RequestTooLargeException}
 import org.apache.james.jmap.method.WithAccountId
+
+import java.io.InputStream
+import java.time.{Duration, ZoneId, ZonedDateTime}
+import java.util.TimeZone
 
 case class CalendarEventParseRequest(accountId: AccountId,
                                      blobIds: BlobIds) extends WithAccountId {
@@ -37,28 +43,127 @@ object CalendarEventNotParsable {
     CalendarEventNotParsable(notParsable1.value ++ notParsable2.value)
 }
 
+object CalendarTitleField {
+  def from(calendarEvent: VEvent): Option[CalendarTitleField] =
+    Option(calendarEvent.getSummary).map(_.getValue).map(CalendarTitleField(_))
+}
+
 case class CalendarTitleField(value: String) extends AnyVal
+
+object CalendarDescriptionField {
+  def from(calendarEvent: VEvent): Option[CalendarDescriptionField] =
+    Option(calendarEvent.getDescription).map(_.getValue).map(CalendarDescriptionField(_))
+}
+
 case class CalendarDescriptionField(value: String) extends AnyVal
+
+object CalendarLocationField {
+  def from(calendarEvent: VEvent): Option[CalendarLocationField] =
+    Option(calendarEvent.getLocation).map(_.getValue).map(CalendarLocationField(_))
+}
+
+case class CalendarLocationField(value: String) extends AnyVal
+
+object CalendarStartField {
+  def from(calendarEvent: VEvent): Option[CalendarStartField] =
+    Option(calendarEvent.getStartDate)
+      .flatMap(startDate => Option(startDate.getDate)
+        .map(date => CalendarStartField(ZonedDateTime.ofInstant(date.toInstant,
+          Option(startDate.getTimeZone).getOrElse(getTimeZoneAlternative(calendarEvent)).toZoneId))))
+
+  def getTimeZoneAlternative(calendarEvent: VEvent): TimeZone =
+    CalendarTimeZoneField.getTimeZoneFromTZID(calendarEvent)
+      .getOrElse(TimeZone.getTimeZone(ZoneId.of("UTC")))
+}
+
+case class CalendarStartField(value: ZonedDateTime) extends AnyVal {
+  def asUtcDate(): UTCDate = UTCDate(value)
+}
+
+object CalendarEndField {
+  def from(calendarEvent: VEvent): Option[CalendarEndField] =
+    Option(calendarEvent.getEndDate())
+      .flatMap(endDate => Option(endDate.getDate)
+        .map(date => CalendarEndField(ZonedDateTime.ofInstant(date.toInstant,
+          Option(endDate.getTimeZone).getOrElse(getTimeZoneAlternative(calendarEvent)).toZoneId))))
+}
+
+case class CalendarEndField(value: ZonedDateTime) extends AnyVal {
+  def asUtcDate(): UTCDate = UTCDate(value)
+}
+
+object CalendarDurationField {
+  def from(calendarEvent: VEvent): Option[CalendarDurationField] =
+    Option(calendarEvent.getDuration)
+      .map(_.getDuration)
+      .map(java.time.Duration.from)
+      .map(CalendarDurationField(_))
+}
+
+case class CalendarDurationField(value: Duration) extends AnyVal
+
+object CalendarTimeZoneField {
+  def from(calendarEvent: VEvent): Option[CalendarTimeZoneField] =
+    getTimeZoneFromTZID(calendarEvent)
+      .orElse(getTimeZoneFromStartDate(calendarEvent))
+      .map(CalendarTimeZoneField(_))
+
+  def getTimeZoneFromTZID(calendarEvent: VEvent): Option[TimeZone] =
+    Option(calendarEvent.getProperty(Property.TZID).asInstanceOf[Property])
+      .map(_.getValue)
+      .map(TimeZone.getTimeZone)
+
+  private def getTimeZoneFromStartDate(calendarEvent: VEvent): Option[TimeZone] =
+    Option(calendarEvent.getStartDate)
+      .flatMap(date => Option(date.getTimeZone))
+      .map(_.toZoneId)
+      .map(TimeZone.getTimeZone)
+}
+
+case class CalendarTimeZoneField(value: TimeZone) extends AnyVal
 
 case class InvalidCalendarFileException(blobId: BlobId) extends RuntimeException
 
 object CalendarEventParsed {
+  def from(calendarContent: InputStream): CalendarEventParsed =
+    from(new CalendarBuilder(CalendarParserFactory.getInstance.get,
+      new ContentHandlerContext().withSupressInvalidProperties(true),
+      TimeZoneRegistryFactory.getInstance.createRegistry)
+      .build(calendarContent))
+
   def from(calendar: Calendar): CalendarEventParsed =
     try {
       val vevent: VEvent = calendar.getComponent("VEVENT").asInstanceOf[VEvent]
       Preconditions.checkArgument(vevent != null, "Calendar is not contain VEVENT component".asInstanceOf[Object])
-      CalendarEventParsed(title = Option(vevent.getSummary).map(_.getValue).map(CalendarTitleField),
-        description = Option(vevent.getDescription).map(_.getValue).map(CalendarDescriptionField))
-    }
+      val start: Option[CalendarStartField] = CalendarStartField.from(vevent)
+      val end: Option[CalendarEndField] = CalendarEndField.from(vevent)
 
+      CalendarEventParsed(title = CalendarTitleField.from(vevent),
+        description = CalendarDescriptionField.from(vevent),
+        start = start,
+        end = end,
+        utcStart = start.map(_.asUtcDate()),
+        utcEnd = end.map(_.asUtcDate()),
+        duration = CalendarDurationField.from(vevent),
+        timeZone = CalendarTimeZoneField.from(vevent),
+        location = CalendarLocationField.from(vevent))
+    }
 }
-case class CalendarEventParsed(title: Option[CalendarTitleField],
-                               description: Option[CalendarDescriptionField])
+
+case class CalendarEventParsed(title: Option[CalendarTitleField] = None,
+                               description: Option[CalendarDescriptionField] = None,
+                               start: Option[CalendarStartField] = None,
+                               end: Option[CalendarEndField] = None,
+                               utcStart: Option[UTCDate] = None,
+                               utcEnd: Option[UTCDate] = None,
+                               timeZone: Option[CalendarTimeZoneField] = None,
+                               duration: Option[CalendarDurationField] = None,
+                               location: Option[CalendarLocationField] = None)
 
 case class CalendarEventParseResponse(accountId: AccountId,
-                            parsed: Option[Map[BlobId, CalendarEventParsed]],
-                            notFound: Option[CalendarEventNotFound],
-                            notParsable: Option[CalendarEventNotParsable])
+                                      parsed: Option[Map[BlobId, CalendarEventParsed]],
+                                      notFound: Option[CalendarEventNotFound],
+                                      notParsable: Option[CalendarEventNotParsable])
 
 object CalendarEventParseResults {
   def notFound(blobId: UnparsedBlobId): CalendarEventParseResults = CalendarEventParseResults(None, Some(CalendarEventNotFound(Set(blobId))), None)
