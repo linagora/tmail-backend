84e3414a479c95a96587d1c70d522782e9ec5eba
Tung Van TRAN <vttran@linagora.com>
2023-04-21 17:23:27 +0700

    ISSUE-640 Add OIDC jmap endpoints + SSO

--- demo/test.sh
+++ demo/test.sh
@@ -3,6 +3,8 @@
 set -eux
 
 ./dev.sh start
+
+REDIRECT_URI=http://test.sso.example.com:8080/login-callback.html
 # Get form token
 TOKEN=`curl http://sso.example.com/ 2>/dev/null|grep token|perl -pe 's/.*?value="(\w+)".*$/$1/'|head -n1`
 
@@ -12,13 +14,13 @@ COOKIE=`curl -X POST -d user='james-user@tmail.com' -d password=secret -d lmAuth
 echo "Identified by LLNG"
 
 # Get code
-CODE=`curl -s -D - -o /dev/null -b lemonldap=$COOKIE 'http://sso.example.com/oauth2/authorize?response_type=code&client_id=james&scope=openid+profile+email&redirect_uri=http://test.sso.example.com&confirm=1' 2>/dev/null|grep Location|perl -pe 's/.*?code=(\w+).*?$/$1/'`
+CODE=`curl -s -D - -o /dev/null -b lemonldap=$COOKIE 'http://sso.example.com/oauth2/authorize?response_type=code&client_id=james&scope=openid+profile+email&confirm=1&redirect_uri='$REDIRECT_URI 2>/dev/null|grep Location|perl -pe 's/.*?code=(\w+).*?$/$1/'`
 
 echo "Got a code in authorization flow ($CODE)"
 
 # Get ID Token
 
-ACCESS_TOKEN=`curl -X POST -d grant_type=authorization_code -d 'redirect_uri=http://test.sso.example.com' -d code=$CODE -u 'james:james' 'http://sso.example.com/oauth2/token' 2>/dev/null|perl -pe 's/^.*"access_token"\s*:\s*"(.*?)".*$/$1/'`
+ACCESS_TOKEN=`curl -X POST -d grant_type=authorization_code -d 'redirect_uri='$REDIRECT_URI -d code=$CODE -u 'james:james' 'http://sso.example.com/oauth2/token' 2>/dev/null|perl -pe 's/^.*"access_token"\s*:\s*"(.*?)".*$/$1/'`
 
 echo "Got an access_token"
 
@@ -30,7 +32,8 @@ fi
 
 echo -n "Trying James: "
 
-if curl -v -H 'Accept: application/json; jmapVersion=rfc-8621' -H "Authorization: Bearer $ACCESS_TOKEN" test.sso.example.com:8090/oidc/jmap 2>/dev/null | grep uploadUrl >/dev/null; then
+APISIX_JMAP_ENDPOINT=apisix.example.com:9080/oidc/jmap/session
+if curl -v -H 'Accept: application/json; jmapVersion=rfc-8621' -H "Authorization: Bearer $ACCESS_TOKEN" $APISIX_JMAP_ENDPOINT 2>/dev/null | grep uploadUrl >/dev/null; then
 	echo "OK"
 else
 	echo "Not OK"
@@ -40,7 +43,7 @@ fi
 
 curl -s -D - -o /dev/null -b lemonldap=$COOKIE http://sso.example.com/?logout=1 >/dev/null 2>&1
 
-if curl -v -H 'Accept: application/json; jmapVersion=rfc-8621' -H "Authorization: Bearer $ACCESS_TOKEN" test.sso.example.com:8090/oidc/jmap 2>/dev/null | grep uploadUrl >/dev/null; then
+if curl -v -H 'Accept: application/json; jmapVersion=rfc-8621' -H "Authorization: Bearer $ACCESS_TOKEN" $APISIX_JMAP_ENDPOINT 2>/dev/null | grep uploadUrl >/dev/null; then
 	echo "LOGOUT FAILED"
 else
 	echo "Logout OK"
