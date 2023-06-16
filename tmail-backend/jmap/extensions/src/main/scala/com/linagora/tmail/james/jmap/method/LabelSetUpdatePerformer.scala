2f6ad4acb862909ffb5b311fef6c56671aff1163
Quan Tran <hqtran@linagora.com>
2023-06-16 16:30:15 +0700

    ISSUE-709 Label/set update

---
+++ tmail-backend/jmap/extensions/src/main/scala/com/linagora/tmail/james/jmap/method/LabelSetUpdatePerformer.scala
@@ -0,0 +1,61 @@
+package com.linagora.tmail.james.jmap.method
+
+import com.linagora.tmail.james.jmap.label.LabelRepository
+import com.linagora.tmail.james.jmap.model.{LabelId, LabelNotFoundException, LabelPatchObject, LabelPatchUpdateValidationException, LabelSetRequest, LabelUpdateResponse, UnparsedLabelId, ValidatedLabelPatchObject}
+import javax.inject.Inject
+import org.apache.james.core.Username
+import org.apache.james.jmap.core.SetError
+import org.apache.james.jmap.core.SetError.SetErrorDescription
+import org.apache.james.util.ReactorUtils
+import reactor.core.scala.publisher.{SFlux, SMono}
+
+import scala.collection.Seq
+
+sealed trait LabelUpdateResult
+
+case class LabelUpdateFailure(id: UnparsedLabelId, exception: Throwable) extends LabelUpdateResult {
+  def asSetError: SetError = exception match {
+    case e: LabelPatchUpdateValidationException => SetError.invalidArguments(SetErrorDescription(e.error), e.asProperty)
+    case e: LabelNotFoundException => SetError.notFound(SetErrorDescription(e.getMessage))
+    case e: IllegalArgumentException => SetError.invalidArguments(SetErrorDescription(e.getMessage), None)
+    case _ => SetError.serverFail(SetErrorDescription(exception.getMessage))
+  }
+}
+
+case class LabelUpdateSuccess(id: LabelId) extends LabelUpdateResult
+
+case class LabelUpdateResults(results: Seq[LabelUpdateResult]) {
+  def updated: Map[LabelId, LabelUpdateResponse] =
+    results.flatMap(result => result match {
+      case success: LabelUpdateSuccess => Some((success.id, LabelUpdateResponse()))
+      case _ => None
+    }).toMap
+
+  def notUpdated: Map[UnparsedLabelId, SetError] =
+    results.flatMap(result => result match {
+      case failure: LabelUpdateFailure => Some(failure.id, failure.asSetError)
+      case _ => None
+    }).toMap
+}
+
+class LabelSetUpdatePerformer @Inject()(val labelRepository: LabelRepository) {
+  def update(request: LabelSetRequest, username: Username): SMono[LabelUpdateResults] =
+    SFlux.fromIterable(request.update.getOrElse(Map()))
+      .flatMap({
+        case (unparsedId: UnparsedLabelId, patch: LabelPatchObject) =>
+          val either = for {
+            validatedPatch <- patch.validate()
+          } yield {
+            updateLabel(username, LabelId(unparsedId.id), validatedPatch)
+          }
+          either.fold(e => SMono.just(LabelUpdateFailure(unparsedId, e)),
+            sMono => sMono
+              .onErrorResume(e => SMono.just(LabelUpdateFailure(unparsedId, e))))
+      }, maxConcurrency = ReactorUtils.DEFAULT_CONCURRENCY)
+      .collectSeq()
+      .map(LabelUpdateResults)
+
+  private def updateLabel(username: Username, labelId: LabelId, validatedPatch: ValidatedLabelPatchObject): SMono[LabelUpdateResult] =
+    SMono.fromPublisher(labelRepository.updateLabel(username, labelId, validatedPatch.displayNameUpdate, validatedPatch.colorUpdate))
+      .`then`(SMono.just(LabelUpdateSuccess(labelId)))
+}
--- tmail-backend/jmap/extensions/src/main/scala/com/linagora/tmail/james/jmap/model/LabelSet.scala
+++ tmail-backend/jmap/extensions/src/main/scala/com/linagora/tmail/james/jmap/model/LabelSet.scala
@@ -1,15 +1,17 @@
 package com.linagora.tmail.james.jmap.model
 
+import com.linagora.tmail.james.jmap.model.LabelPatchObject.{colorProperty, displayNameProperty, updateProperties}
 import org.apache.james.jmap.core.Id.Id
-import org.apache.james.jmap.core.{AccountId, SetError, UuidState}
+import org.apache.james.jmap.core.{AccountId, Properties, SetError, UuidState}
 import org.apache.james.jmap.mail.Keyword
 import org.apache.james.jmap.method.WithAccountId
-import play.api.libs.json.JsObject
+import play.api.libs.json.{JsObject, JsString, JsValue, Json}
 
 import scala.collection.Seq
 
 case class LabelSetRequest(accountId: AccountId,
                            create: Option[Map[LabelCreationId, JsObject]],
+                           update: Option[Map[UnparsedLabelId, LabelPatchObject]],
                            destroy: Option[Seq[UnparsedLabelId]]) extends WithAccountId
 
 case class LabelCreationId(id: Id)
@@ -22,5 +24,59 @@ case class LabelSetResponse(accountId: AccountId,
                             newState: UuidState,
                             created: Option[Map[LabelCreationId, LabelCreationResponse]],
                             notCreated: Option[Map[LabelCreationId, SetError]],
+                            updated: Option[Map[LabelId, LabelUpdateResponse]],
+                            notUpdated: Option[Map[UnparsedLabelId, SetError]],
                             destroyed: Option[Seq[LabelId]],
                             notDestroyed: Option[Map[UnparsedLabelId, SetError]])
+
+object LabelPatchObject {
+  private val displayNameProperty: String = "displayName"
+  private val colorProperty: String = "color"
+  private val updateProperties: Set[String] = Set(displayNameProperty, colorProperty)
+}
+
+case class LabelPatchObject(value: Map[String, JsValue]) {
+  def validate(): Either[LabelPatchUpdateValidationException, ValidatedLabelPatchObject] =
+    for {
+      _ <- validateProperties
+      displayName <- validateDisplayName
+      color <- validateColor
+    } yield {
+      ValidatedLabelPatchObject(displayName, color)
+    }
+
+  def validateProperties: Either[LabelPatchUpdateValidationException, LabelPatchObject] =
+    value.find(mapEntry => !updateProperties.contains(mapEntry._1))
+      .map(e => Left(LabelPatchUpdateValidationException("Some unknown properties were specified", Some(e._1))))
+      .getOrElse(Right(this))
+
+  private def validateColor: Either[LabelPatchUpdateValidationException, Option[Color]] =
+    value.get(colorProperty) match {
+      case Some(jsValue) => jsValue match {
+        case JsString(aString) => Color.validate(aString) match {
+          case Right(color: Color) => Right(Some(color))
+          case Left(e: IllegalArgumentException) => Left(LabelPatchUpdateValidationException(e.getMessage, Some(colorProperty)))
+        }
+        case _ => Left(LabelPatchUpdateValidationException("Expecting a JSON string as an argument", Some(colorProperty)))
+      }
+      case None => Right(None)
+    }
+
+  private def validateDisplayName: Either[LabelPatchUpdateValidationException, Option[DisplayName]] =
+    value.get(displayNameProperty) match {
+      case Some(jsValue) => jsValue match {
+        case JsString(aString) => Right(Some(DisplayName(aString)))
+        case _ => Left(LabelPatchUpdateValidationException("Expecting a JSON string as an argument", Some(displayNameProperty)))
+      }
+      case None => Right(None)
+    }
+}
+
+case class ValidatedLabelPatchObject(displayNameUpdate: Option[DisplayName] = None,
+                                     colorUpdate: Option[Color] = None)
+
+case class LabelUpdateResponse(json: JsObject = Json.obj())
+
+case class LabelPatchUpdateValidationException(error: String, property: Option[String] = None) extends IllegalArgumentException {
+  def asProperty: Option[Properties] = property.map(value => Properties.toProperties(Set(value)))
+}
