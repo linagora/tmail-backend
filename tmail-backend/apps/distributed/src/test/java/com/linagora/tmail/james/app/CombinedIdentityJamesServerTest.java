85f1627c01bb1186cb878df1a03871953c23aad9
Tung Van TRAN <vttran@linagora.com>
2023-06-16 11:36:46 +0700

    Fix error in the test related to Ldap container - java.lang.IllegalStateException: Mapped port can only be obtained after the container is started
    - Reason: Tmail-backend server extension tries to get LdapHost before Ldap already start

--- tmail-backend/apps/distributed/src/test/java/com/linagora/tmail/james/app/CombinedIdentityJamesServerTest.java
+++ tmail-backend/apps/distributed/src/test/java/com/linagora/tmail/james/app/CombinedIdentityJamesServerTest.java
@@ -1,36 +1,25 @@
 package com.linagora.tmail.james.app;
 
-import static org.apache.james.user.ldap.DockerLdapSingleton.ADMIN;
-import static org.apache.james.user.ldap.DockerLdapSingleton.ADMIN_PASSWORD;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
-import org.apache.commons.configuration2.BaseHierarchicalConfiguration;
-import org.apache.commons.configuration2.HierarchicalConfiguration;
-import org.apache.commons.configuration2.plist.PropertyListConfiguration;
-import org.apache.commons.configuration2.tree.ImmutableNode;
 import org.apache.james.GuiceJamesServer;
 import org.apache.james.JamesServerBuilder;
 import org.apache.james.JamesServerExtension;
 import org.apache.james.SearchConfiguration;
-import org.apache.james.server.core.configuration.ConfigurationProvider;
-import org.apache.james.user.ldap.DockerLdapSingleton;
-import org.apache.james.user.ldap.LdapGenericContainer;
 import org.apache.james.utils.DataProbeImpl;
 import org.apache.james.utils.GuiceProbe;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 import com.google.inject.multibindings.Multibinder;
 import com.linagora.tmail.combined.identity.CombinedUsersRepository;
+import com.linagora.tmail.combined.identity.LdapExtension;
 import com.linagora.tmail.combined.identity.UsersRepositoryClassProbe;
 import com.linagora.tmail.combined.identity.UsersRepositoryModuleChooser;
 import com.linagora.tmail.module.LinagoraTestJMAPServerModule;
 
 public class CombinedIdentityJamesServerTest {
-    static LdapGenericContainer ldapContainer = DockerLdapSingleton.ldapContainer;
 
     @RegisterExtension
     static JamesServerExtension testExtension = new JamesServerBuilder<DistributedJamesConfiguration>(tmpDir ->
@@ -42,44 +31,13 @@ public class CombinedIdentityJamesServerTest {
             .build())
         .server(configuration -> DistributedServer.createServer(configuration)
             .overrideWith(new LinagoraTestJMAPServerModule())
-            .overrideWith(binder -> binder.bind(ConfigurationProvider.class).toInstance((s, l) -> new BaseHierarchicalConfiguration(baseConfiguration())))
             .overrideWith(binder -> Multibinder.newSetBinder(binder, GuiceProbe.class).addBinding().to(UsersRepositoryClassProbe.class)))
         .extension(new DockerOpenSearchExtension())
         .extension(new CassandraExtension())
         .extension(new RabbitMQExtension())
+        .extension(new LdapExtension())
         .lifeCycle(JamesServerExtension.Lifecycle.PER_CLASS)
         .build();
-
-
-    @BeforeAll
-    static void setUpAll() {
-        ldapContainer.start();
-    }
-
-    @AfterAll
-    static void afterAll() {
-        ldapContainer.stop();
-    }
-
-    private static HierarchicalConfiguration<ImmutableNode> baseConfiguration() {
-        PropertyListConfiguration configuration = new PropertyListConfiguration();
-        configuration.addProperty("[@ldapHost]", DockerLdapSingleton.ldapContainer.getLdapHost());
-        configuration.addProperty("[@principal]", "cn=admin,dc=james,dc=org");
-        configuration.addProperty("[@credentials]", ADMIN_PASSWORD);
-        configuration.addProperty("[@userBase]", "ou=People,dc=james,dc=org");
-        configuration.addProperty("[@userObjectClass]", "inetOrgPerson");
-        configuration.addProperty("[@maxRetries]", "1");
-        configuration.addProperty("[@retryStartInterval]", "0");
-        configuration.addProperty("[@retryMaxInterval]", "2");
-        configuration.addProperty("[@retryIntervalScale]", "1000");
-        configuration.addProperty("[@connectionTimeout]", "1000");
-        configuration.addProperty("[@readTimeout]", "1000");
-
-        configuration.addProperty("[@userIdAttribute]", "mail");
-        configuration.addProperty("[@administratorId]", ADMIN.asString());
-        configuration.addProperty("enableVirtualHosting", true);
-        return configuration;
-    }
 
     @Test
     void shouldUseCombinedUsersRepositoryWhenSpecified(GuiceJamesServer server) {
