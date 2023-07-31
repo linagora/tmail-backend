70374884805b635a3471c74d54e3c22fda03d051
Quan Tran <hqtran@linagora.com>
2023-07-31 17:53:23 +0700

    fixup! ISSUE-762 Integrate Rspamd Healthcheck

--- tmail-backend/integration-tests/healthcheck/healthcheck-integration-tests-common/src/main/java/com/linagora/tmail/integration/TMailHealthCheckIntegrationTests.java
+++ tmail-backend/integration-tests/healthcheck/healthcheck-integration-tests-common/src/main/java/com/linagora/tmail/integration/TMailHealthCheckIntegrationTests.java
@@ -29,7 +29,7 @@ public abstract class TMailHealthCheckIntegrationTests {
                 .jsonPath()
                 .getList("checks.componentName", String.class);
 
-        assertThat(listComponentNames).contains("Tasks execution");
+        assertThat(listComponentNames).contains("Tasks execution", "Rspamd");
     }
 
     @Test
