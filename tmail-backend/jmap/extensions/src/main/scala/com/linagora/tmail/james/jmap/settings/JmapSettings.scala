98f73f62b9c156f203021579c0e473bcada3aa58
Tung Tran <vttran@linagora.com>
2023-08-29 08:57:23 +0700

    fixup! ISSUE-785 drop MatchesRegex

--- tmail-backend/jmap/extensions/src/main/scala/com/linagora/tmail/james/jmap/settings/JmapSettings.scala
+++ tmail-backend/jmap/extensions/src/main/scala/com/linagora/tmail/james/jmap/settings/JmapSettings.scala
@@ -1,19 +1,33 @@
 package com.linagora.tmail.james.jmap.settings
 
+import com.google.common.base.CharMatcher
 import com.linagora.tmail.james.jmap.settings.JmapSettings.{JmapSettingsKey, JmapSettingsValue}
-import eu.timepit.refined.api.Refined
-import eu.timepit.refined.refineV
-import eu.timepit.refined.string.MatchesRegex
+import eu.timepit.refined
+import eu.timepit.refined.api.{Refined, Validate}
 import org.apache.james.jmap.core.UuidState
 
 object JmapSettings {
+  case class JmapSettingKeyConstraint()
 
-  private type SettingKeyRegex = MatchesRegex["^[a-zA-Z0-9-_#.]{1,255}$"]
-  private type SettingKeyType = String Refined SettingKeyRegex
+  type SettingKeyType = String Refined JmapSettingKeyConstraint
+
+  private val charMatcher: CharMatcher = CharMatcher.inRange('a', 'z')
+    .or(CharMatcher.inRange('0', '9'))
+    .or(CharMatcher.inRange('A', 'Z'))
+    .or(CharMatcher.is('.'))
+    .or(CharMatcher.is('_'))
+    .or(CharMatcher.is('-'))
+    .or(CharMatcher.is('#'))
+  def validateSettingKey(string: String): Either[IllegalArgumentException, SettingKeyType] =
+    refined.refineV[JmapSettingKeyConstraint](string)(
+        Validate.fromPredicate(s => s.nonEmpty && s.length < 256 && charMatcher.matchesAllOf(s),
+          s => s"'$s' contains some invalid characters. Should be [#a-zA-Z0-9-_#.] and no longer than 255 chars",
+          JmapSettingKeyConstraint()))
+      .left.map(new IllegalArgumentException(_))
 
   object JmapSettingsKey {
     def liftOrThrow(value: String): JmapSettingsKey =
-      refineV[SettingKeyRegex](value) match {
+      validateSettingKey(value) match {
         case Left(error) => throw new IllegalArgumentException(error)
         case Right(settingKey) => JmapSettingsKey(settingKey)
       }
