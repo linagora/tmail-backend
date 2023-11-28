1789b84bb77189b1491ac17b76786dd2d6053476
Benoit TELLIER <btellier@linagora.com>
2023-11-28 08:46:17 +0100

    fixup! fixup! fixup! [BACKPORT] JAMES-3963 Full text search: AND for combining phrase terms

--- tmail-backend/mailets/src/test/scala/com/linagora/tmail/mailets/EnforceRateLimitingPlanTest.scala
+++ tmail-backend/mailets/src/test/scala/com/linagora/tmail/mailets/EnforceRateLimitingPlanTest.scala
@@ -3,17 +3,16 @@ package com.linagora.tmail.mailets
 import java.time.Duration
 import java.util
 import java.util.stream.IntStream
-
 import com.codahale.metrics.MetricRegistry
 import com.linagora.tmail.mailets.EnforceRateLimitingPlanTest.{USER1, USER2}
 import com.linagora.tmail.rate.limiter.api.memory.MemoryRateLimitingPlanUserRepository
 import com.linagora.tmail.rate.limiter.api.{DeliveryLimitations, InMemoryRateLimitingPlanRepository, LimitTypes, OperationLimitationsType, RateLimitation, RateLimitingPlan, RateLimitingPlanCreateRequest, RateLimitingPlanRepository, RateLimitingPlanUserRepository, RelayLimitations, TransitLimitations}
 import eu.timepit.refined.auto._
+import org.apache.james.backends.redis.{DockerRedis, RedisConfiguration, RedisExtension}
 import org.apache.james.core.Username
 import org.apache.james.metrics.api.NoopGaugeRegistry
 import org.apache.james.metrics.dropwizard.DropWizardGaugeRegistry
-import org.apache.james.rate.limiter.redis.{RedisRateLimiterConfiguration, RedisRateLimiterFactory}
-import org.apache.james.rate.limiter.{DockerRedis, RedisExtension}
+import org.apache.james.rate.limiter.redis.RedisRateLimiterFactory
 import org.apache.james.util.Size.parse
 import org.apache.mailet.base.test.{FakeMail, FakeMailContext, FakeMailetConfig}
 import org.apache.mailet.{Mail, MailetConfig}
@@ -46,7 +45,7 @@ class EnforceRateLimitingPlanTest {
   def setup(redis: DockerRedis): Unit = {
     rateLimitationPlanRepository = new InMemoryRateLimitingPlanRepository
     rateLimitingPlanUserRepository = new MemoryRateLimitingPlanUserRepository
-    redisRateLimiterFactory = new RedisRateLimiterFactory(RedisRateLimiterConfiguration.from(redis.redisURI().toString, isCluster = false))
+    redisRateLimiterFactory = new RedisRateLimiterFactory(RedisConfiguration.from(redis.redisURI().toString, isCluster = false))
 
     val rateLimitingPlan: RateLimitingPlan = SMono.fromPublisher(rateLimitationPlanRepository.create(RateLimitingPlanCreateRequest(
       name = "PaidPlan",
