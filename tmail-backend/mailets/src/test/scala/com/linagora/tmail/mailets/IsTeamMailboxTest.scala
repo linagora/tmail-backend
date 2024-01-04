f8fe62fc919fdd32ebb5228a03a2c0dee1fba850
Benoit TELLIER <btellier@linagora.com>
2024-01-04 10:14:25 +0100

    ISSUE-884 Add a IsTeamMailbox matcher (#885)

---
+++ tmail-backend/mailets/src/test/scala/com/linagora/tmail/mailets/IsTeamMailboxTest.scala
@@ -0,0 +1,56 @@
+package com.linagora.tmail.mailets
+
+import com.google.common.collect.ImmutableList
+import com.linagora.tmail.team.{TeamMailbox, TeamMailboxCallbackNoop, TeamMailboxRepository, TeamMailboxRepositoryImpl}
+import org.apache.james.core.MailAddress
+import org.apache.james.mailbox.MailboxManager
+import org.apache.james.mailbox.inmemory.manager.InMemoryIntegrationResources
+import org.apache.james.mailbox.store.StoreSubscriptionManager
+import org.apache.mailet.base.test.FakeMail
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.{BeforeEach, Test}
+import reactor.core.scala.publisher.SMono
+
+class IsTeamMailboxTest {
+  private var testee: IsTeamMailbox = null
+  private var mailboxManager: MailboxManager = null
+  private var teamMailboxRepository: TeamMailboxRepository = null
+
+  @BeforeEach
+  def setup(): Unit = {
+
+    val resources = InMemoryIntegrationResources.defaultResources()
+    mailboxManager = resources.getMailboxManager
+    val subscriptionManager = new StoreSubscriptionManager(resources.getMailboxManager.getMapperFactory,
+      resources.getMailboxManager.getMapperFactory, resources.getMailboxManager.getEventBus)
+
+    teamMailboxRepository = new TeamMailboxRepositoryImpl(mailboxManager, subscriptionManager, TeamMailboxCallbackNoop.asSet)
+
+    testee = new IsTeamMailbox(teamMailboxRepository)
+  }
+
+  @Test
+  def shouldNotMatchRegularAddresses(): Unit = {
+    val address = new MailAddress("user1@domain.tld")
+    val mail = FakeMail.defaultFakeMail()
+    mail.setRecipients(ImmutableList.of(address))
+
+    val expected = testee.`match`(mail)
+
+    assertThat(expected).isEmpty()
+  }
+
+  @Test
+  def shouldMatchTeamMailboxes(): Unit = {
+    val address = new MailAddress("user1@domain.tld")
+
+    SMono(teamMailboxRepository.createTeamMailbox(TeamMailbox.asTeamMailbox(address).get)).block()
+
+    val mail = FakeMail.defaultFakeMail()
+    mail.setRecipients(ImmutableList.of(address))
+
+    val expected = testee.`match`(mail)
+
+    assertThat(expected).containsExactly(address)
+  }
+}
