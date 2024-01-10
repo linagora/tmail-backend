50dc57390f6dcc8b47af855413d86dd85e5db8e9
Tung Tran <vttran@linagora.com>
2024-01-10 17:45:00 +0700

    ISSUE-883 Implement postgres-app

---
+++ tmail-backend/apps/postgres/src/test/java/com/linagora/tmail/james/app/PostgresTmailServerTest.java
@@ -0,0 +1,42 @@
+package com.linagora.tmail.james.app;
+
+import static org.apache.james.PostgresJamesConfiguration.EventBusImpl.IN_MEMORY;
+
+import org.apache.james.JamesServerBuilder;
+import org.apache.james.JamesServerConcreteContract;
+import org.apache.james.JamesServerExtension;
+import org.apache.james.SearchConfiguration;
+import org.apache.james.backends.postgres.PostgresExtension;
+import org.apache.james.modules.blobstore.BlobStoreConfiguration;
+import org.apache.james.utils.GuiceProbe;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import com.google.inject.multibindings.Multibinder;
+import com.linagora.tmail.combined.identity.UsersRepositoryClassProbe;
+import com.linagora.tmail.encrypted.MailboxConfiguration;
+import com.linagora.tmail.encrypted.MailboxManagerClassProbe;
+
+class PostgresTmailServerTest implements JamesServerConcreteContract {
+    static PostgresExtension postgresExtension = PostgresExtension.empty();
+
+    @RegisterExtension
+    static JamesServerExtension testExtension = new JamesServerBuilder<PostgresTmailConfiguration>(tmpDir ->
+        PostgresTmailConfiguration.builder()
+            .workingDirectory(tmpDir)
+            .configurationFromClasspath()
+            .blobStore(BlobStoreConfiguration.builder()
+                .postgres()
+                .disableCache()
+                .deduplication()
+                .noCryptoConfig())
+            .searchConfiguration(SearchConfiguration.scanning())
+            .mailbox(new MailboxConfiguration(false))
+            .eventBusImpl(IN_MEMORY)
+            .build())
+        .server(configuration -> PostgresTmailServer.createServer(configuration)
+            .overrideWith(binder -> Multibinder.newSetBinder(binder, GuiceProbe.class).addBinding().to(MailboxManagerClassProbe.class))
+            .overrideWith(binder -> Multibinder.newSetBinder(binder, GuiceProbe.class).addBinding().to(UsersRepositoryClassProbe.class)))
+        .extension(postgresExtension)
+        .lifeCycle(JamesServerExtension.Lifecycle.PER_CLASS)
+        .build();
+}
