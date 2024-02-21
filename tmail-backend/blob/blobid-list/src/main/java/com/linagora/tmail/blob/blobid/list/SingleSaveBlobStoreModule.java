bfe37f7b1a3f30c984f87155cf9f81648341a70d
Tung Tran <vttran@linagora.com>
2024-02-21 18:16:02 +0700

    ISSUE-929 Fix blob store module chooser

--- tmail-backend/blob/blobid-list/src/main/java/com/linagora/tmail/blob/blobid/list/SingleSaveBlobStoreModule.java
+++ tmail-backend/blob/blobid-list/src/main/java/com/linagora/tmail/blob/blobid/list/SingleSaveBlobStoreModule.java
@@ -1,11 +1,8 @@
 package com.linagora.tmail.blob.blobid.list;
 
 import org.apache.james.backends.cassandra.components.CassandraModule;
-import org.apache.james.blob.api.BucketName;
-import org.apache.james.blob.objectstorage.aws.S3BlobStoreDAO;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.Provides;
 import com.google.inject.multibindings.Multibinder;
 import com.linagora.tmail.blob.blobid.list.cassandra.BlobIdListCassandraModule;
 import com.linagora.tmail.blob.blobid.list.cassandra.CassandraBlobIdListModule;
@@ -19,12 +16,5 @@ public class SingleSaveBlobStoreModule extends AbstractModule {
         Multibinder.newSetBinder(binder(), CassandraModule.class)
             .addBinding()
             .toInstance(CassandraBlobIdListModule.MODULE());
-    }
-
-    @Provides
-    private SingleSaveBlobStoreDAO provideSingleSaveBlobStoreDAO(S3BlobStoreDAO s3BlobStoreDAO,
-                                                              BlobIdList blobIdList,
-                                                              BucketName defaultBucketName) {
-        return new SingleSaveBlobStoreDAO(s3BlobStoreDAO, blobIdList, defaultBucketName);
     }
 }
