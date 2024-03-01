ed751cc6bd9e817545d8d14bd6f70b4ebd150f2b
Rene Cordier <rcordier@linagora.com>
2024-03-01 16:40:45 +0700

    [WIP] Tmail quorum queues

--- tmail-backend/apps/distributed/src/test/java/com/linagora/tmail/james/app/TestRabbitMQModule.java
+++ tmail-backend/apps/distributed/src/test/java/com/linagora/tmail/james/app/TestRabbitMQModule.java
@@ -73,7 +73,10 @@ public class TestRabbitMQModule extends AbstractModule {
     public RabbitMQEmailAddressContactConfiguration rabbitMQEmailAddressContactConfiguration(RabbitMQConfiguration rabbitMQConfiguration) {
         return new RabbitMQEmailAddressContactConfiguration(ADDRESS_CONTACT_QUEUE,
             rabbitMQConfiguration.getUri(),
-            rabbitMQConfiguration.getManagementCredentials());
+            rabbitMQConfiguration.getManagementCredentials(),
+            rabbitMQConfiguration.getVhost(),
+            false,
+            0);
     }
 
     @Provides
