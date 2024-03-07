acd11e24dd626b4dd9521f6fc6937c830c25ccfa
hung phan <hphan@linagora.com>
2024-03-07 14:57:22 +0700

    ISSUE-918 JMAP memory binding for Postgres TMail app

--- tmail-backend/apps/postgres/src/main/java/com/linagora/tmail/james/app/PostgresTmailServer.java
+++ tmail-backend/apps/postgres/src/main/java/com/linagora/tmail/james/app/PostgresTmailServer.java
@@ -1,10 +1,15 @@
 package com.linagora.tmail.james.app;
 
+import static org.apache.james.PostgresJamesServerMain.JMAP;
+
+import java.util.List;
+
 import org.apache.james.ExtraProperties;
 import org.apache.james.GuiceJamesServer;
 import org.apache.james.JamesServerMain;
 import org.apache.james.SearchConfiguration;
 import org.apache.james.SearchModuleChooser;
+import org.apache.james.jmap.draft.JMAPListenerModule;
 import org.apache.james.modules.BlobExportMechanismModule;
 import org.apache.james.modules.MailboxModule;
 import org.apache.james.modules.MailetProcessingModule;
@@ -13,11 +18,12 @@ import org.apache.james.modules.blobstore.BlobStoreCacheModulesChooser;
 import org.apache.james.modules.blobstore.BlobStoreModulesChooser;
 import org.apache.james.modules.data.PostgresDataModule;
 import org.apache.james.modules.data.PostgresDelegationStoreModule;
+import org.apache.james.modules.data.PostgresEventStoreModule;
 import org.apache.james.modules.data.PostgresUsersRepositoryModule;
+import org.apache.james.modules.data.PostgresVacationModule;
 import org.apache.james.modules.data.SievePostgresRepositoryModules;
 import org.apache.james.modules.event.RabbitMQEventBusModule;
 import org.apache.james.modules.events.PostgresDeadLetterModule;
-import org.apache.james.modules.eventstore.MemoryEventStoreModule;
 import org.apache.james.modules.mailbox.DefaultEventModule;
 import org.apache.james.modules.mailbox.PostgresDeletedMessageVaultModule;
 import org.apache.james.modules.mailbox.PostgresMailboxModule;
@@ -31,14 +37,11 @@ import org.apache.james.modules.protocols.SMTPServerModule;
 import org.apache.james.modules.queue.activemq.ActiveMQQueueModule;
 import org.apache.james.modules.queue.rabbitmq.RabbitMQModule;
 import org.apache.james.modules.server.DataRoutesModules;
-import org.apache.james.modules.server.DefaultProcessorsConfigurationProviderModule;
 import org.apache.james.modules.server.InconsistencyQuotasSolvingRoutesModule;
 import org.apache.james.modules.server.JMXServerModule;
 import org.apache.james.modules.server.MailQueueRoutesModule;
 import org.apache.james.modules.server.MailRepositoriesRoutesModule;
 import org.apache.james.modules.server.MailboxRoutesModule;
-import org.apache.james.modules.server.NoJwtModule;
-import org.apache.james.modules.server.RawPostDequeueDecoratorModule;
 import org.apache.james.modules.server.ReIndexingModule;
 import org.apache.james.modules.server.SieveRoutesModule;
 import org.apache.james.modules.server.TaskManagerModule;
@@ -54,7 +57,39 @@ import com.google.inject.Module;
 import com.google.inject.util.Modules;
 import com.linagora.tmail.DatabaseCombinedUserRequireModule;
 import com.linagora.tmail.UsersRepositoryModuleChooser;
+import com.linagora.tmail.encrypted.InMemoryEncryptedEmailContentStoreModule;
+import com.linagora.tmail.encrypted.KeystoreMemoryModule;
+import com.linagora.tmail.james.jmap.contact.MemoryEmailAddressContactModule;
+import com.linagora.tmail.james.jmap.firebase.FirebaseCommonModule;
+import com.linagora.tmail.james.jmap.firebase.FirebaseModuleChooserConfiguration;
+import com.linagora.tmail.james.jmap.firebase.MemoryFirebaseSubscriptionRepository;
+import com.linagora.tmail.james.jmap.label.MemoryLabelRepositoryModule;
+import com.linagora.tmail.james.jmap.method.CalendarEventMethodModule;
+import com.linagora.tmail.james.jmap.method.ContactAutocompleteMethodModule;
+import com.linagora.tmail.james.jmap.method.CustomMethodModule;
+import com.linagora.tmail.james.jmap.method.EmailRecoveryActionMethodModule;
+import com.linagora.tmail.james.jmap.method.EmailSendMethodModule;
+import com.linagora.tmail.james.jmap.method.EncryptedEmailDetailedViewGetMethodModule;
+import com.linagora.tmail.james.jmap.method.EncryptedEmailFastViewGetMethodModule;
+import com.linagora.tmail.james.jmap.method.FilterGetMethodModule;
+import com.linagora.tmail.james.jmap.method.FilterSetMethodModule;
+import com.linagora.tmail.james.jmap.method.ForwardGetMethodModule;
+import com.linagora.tmail.james.jmap.method.ForwardSetMethodModule;
+import com.linagora.tmail.james.jmap.method.JmapSettingsMethodModule;
+import com.linagora.tmail.james.jmap.method.KeystoreGetMethodModule;
+import com.linagora.tmail.james.jmap.method.KeystoreSetMethodModule;
+import com.linagora.tmail.james.jmap.method.LabelMethodModule;
+import com.linagora.tmail.james.jmap.oidc.WebFingerModule;
+import com.linagora.tmail.james.jmap.settings.MemoryJmapSettingsRepositoryModule;
+import com.linagora.tmail.james.jmap.team.mailboxes.TeamMailboxJmapModule;
+import com.linagora.tmail.james.jmap.ticket.TicketRoutesModule;
+import com.linagora.tmail.rate.limiter.api.memory.MemoryRateLimitingModule;
 import com.linagora.tmail.rspamd.RspamdModule;
+import com.linagora.tmail.team.TeamMailboxModule;
+import com.linagora.tmail.webadmin.EmailAddressContactRoutesModule;
+import com.linagora.tmail.webadmin.RateLimitPlanRoutesModule;
+import com.linagora.tmail.webadmin.archival.InboxArchivalTaskModule;
+import com.linagora.tmail.webadmin.cleanup.MailboxesCleanupModule;
 
 public class PostgresTmailServer {
     static Logger LOGGER = LoggerFactory.getLogger("org.apache.james.CONFIGURATION");
@@ -78,6 +113,7 @@ public class PostgresTmailServer {
         SearchConfiguration searchConfiguration = configuration.searchConfiguration();
 
         return GuiceJamesServer.forConfiguration(configuration)
+            .combineWith(POSTGRES_MODULE_AGGREGATE)
             .combineWith(SearchModuleChooser.chooseModules(searchConfiguration))
             .combineWith(chooseUserRepositoryModule(configuration))
             .combineWith(chooseBlobStoreModules(configuration))
@@ -85,9 +121,9 @@ public class PostgresTmailServer {
             .combineWith(chooseDeletedMessageVaultModules(configuration))
             .combineWith(chooseRedisRateLimiterModule(configuration))
             .combineWith(chooseRspamdModule(configuration))
-            .combineWith(POSTGRES_MODULE_AGGREGATE);
+            .combineWith(chooseFirebase(configuration.firebaseModuleChooserConfiguration()))
+            .overrideWith(chooseJmapModule(configuration));
     }
-
 
     private static final Module WEBADMIN = Modules.combine(
         new WebAdminServerModule(),
@@ -109,23 +145,54 @@ public class PostgresTmailServer {
         new SMTPServerModule(),
         WEBADMIN);
 
+    public static final Module JMAP_LINAGORA = Modules.override(
+        JMAP,
+        new CalendarEventMethodModule(),
+        new ContactAutocompleteMethodModule(),
+        new CustomMethodModule(),
+        new EncryptedEmailDetailedViewGetMethodModule(),
+        new EncryptedEmailFastViewGetMethodModule(),
+        new EmailSendMethodModule(),
+        new FilterGetMethodModule(),
+        new FilterSetMethodModule(),
+        new ForwardGetMethodModule(),
+        new InMemoryEncryptedEmailContentStoreModule(),
+        new KeystoreMemoryModule(),
+        new ForwardSetMethodModule(),
+        new KeystoreSetMethodModule(),
+        new KeystoreGetMethodModule(),
+        new TicketRoutesModule(),
+        new WebFingerModule(),
+        new EmailRecoveryActionMethodModule(),
+        new LabelMethodModule(),
+        new JmapSettingsMethodModule(),
+        new MailboxesCleanupModule(),
+        new InboxArchivalTaskModule())
+        .with(new TeamMailboxJmapModule());
+
     private static final Module POSTGRES_SERVER_MODULE = Modules.combine(
         new ActiveMQQueueModule(),
         new BlobExportMechanismModule(),
         new PostgresDelegationStoreModule(),
-        new DefaultProcessorsConfigurationProviderModule(),
         new PostgresMailboxModule(),
         new PostgresDeadLetterModule(),
         new PostgresDataModule(),
         new MailboxModule(),
-        new NoJwtModule(),
-        new RawPostDequeueDecoratorModule(),
         new SievePostgresRepositoryModules(),
         new TaskManagerModule(),
-        new MemoryEventStoreModule(),
-        new TikaMailboxModule());
-    private static final Module POSTGRES_MODULE_AGGREGATE = Modules.combine(
-        new MailetProcessingModule(), POSTGRES_SERVER_MODULE, PROTOCOLS);
+        new PostgresEventStoreModule(),
+        new TikaMailboxModule(),
+        new PostgresVacationModule());
+
+    private static final Module POSTGRES_MODULE_AGGREGATE = Modules.override(Modules.combine(
+        new MailetProcessingModule(), POSTGRES_SERVER_MODULE, PROTOCOLS, JMAP_LINAGORA))
+            .with(new TeamMailboxModule(),
+                new MemoryRateLimitingModule(),
+                new RateLimitPlanRoutesModule(),
+                new MemoryEmailAddressContactModule(),
+                new EmailAddressContactRoutesModule(),
+                new MemoryLabelRepositoryModule(),
+                new MemoryJmapSettingsRepositoryModule());
 
     private static Module chooseBlobStoreModules(PostgresTmailConfiguration configuration) {
         return Modules.combine(Modules.combine(BlobStoreModulesChooser.chooseModules(configuration.blobStoreConfiguration())),
@@ -150,10 +217,7 @@ public class PostgresTmailServer {
     }
 
     private static Module chooseDeletedMessageVaultModules(PostgresTmailConfiguration configuration) {
-        if (configuration.deletedMessageVaultConfiguration().isEnabled()) {
-            return Modules.combine(new PostgresDeletedMessageVaultModule(), new DeletedMessageVaultRoutesModule());
-        }
-        return Modules.EMPTY_MODULE;
+        return Modules.combine(new PostgresDeletedMessageVaultModule(), new DeletedMessageVaultRoutesModule());
     }
 
     private static Module chooseRedisRateLimiterModule(PostgresTmailConfiguration configuration) {
@@ -168,5 +232,20 @@ public class PostgresTmailServer {
             return new RspamdModule();
         }
         return Modules.EMPTY_MODULE;
+    }
+
+    private static Module chooseJmapModule(PostgresTmailConfiguration configuration) {
+        if (configuration.jmapEnabled()) {
+            return new JMAPListenerModule();
+        }
+        return binder -> {
+        };
+    }
+
+    private static List<Module> chooseFirebase(FirebaseModuleChooserConfiguration moduleChooserConfiguration) {
+        if (moduleChooserConfiguration.enable()) {
+            return List.of(new MemoryFirebaseSubscriptionRepository.Module(), new FirebaseCommonModule());
+        }
+        return List.of();
     }
 }
