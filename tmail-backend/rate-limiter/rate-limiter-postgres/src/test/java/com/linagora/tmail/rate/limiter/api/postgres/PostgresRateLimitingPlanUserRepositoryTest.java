5abd483c10d238698c20826094f340ad75313ed0
Quan Tran <hqtran@linagora.com>
2024-03-12 16:05:12 +0700

    ISSUE-915 Implement PostgresRateLimitingPlanUserRepository

---
+++ tmail-backend/rate-limiter/rate-limiter-postgres/src/test/java/com/linagora/tmail/rate/limiter/api/postgres/PostgresRateLimitingPlanUserRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.linagora.tmail.rate.limiter.api.postgres;
+
+import org.apache.james.backends.postgres.PostgresExtension;
+import org.apache.james.backends.postgres.PostgresModule;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import com.linagora.tmail.rate.limiter.api.RateLimitingPlanUserRepository;
+import com.linagora.tmail.rate.limiter.api.RateLimitingPlanUserRepositoryContract;
+import com.linagora.tmail.rate.limiter.api.postgres.repository.PostgresRateLimitingPlanUserRepository;
+import com.linagora.tmail.rate.limiter.api.postgres.table.PostgresRateLimitPlanUserTable;
+
+class PostgresRateLimitingPlanUserRepositoryTest implements RateLimitingPlanUserRepositoryContract {
+    @RegisterExtension
+    static PostgresExtension postgresExtension = PostgresExtension.withRowLevelSecurity(
+        PostgresModule.aggregateModules(PostgresRateLimitPlanUserTable.MODULE()));
+
+    private PostgresRateLimitingPlanUserRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        repository = new PostgresRateLimitingPlanUserRepository(postgresExtension.getExecutorFactory());
+    }
+
+    @Override
+    public RateLimitingPlanUserRepository testee() {
+        return repository;
+    }
+}
