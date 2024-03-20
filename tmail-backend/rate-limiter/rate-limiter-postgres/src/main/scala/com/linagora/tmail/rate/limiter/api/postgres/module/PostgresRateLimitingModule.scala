08b42a937c8fa0da73677d4980b8d1f3a7b17eb1
hungphan227 <45198168+hungphan227@users.noreply.github.com>
2024-03-20 09:09:04 +0700

    ISSUE-914 Implement PostgresRateLimitingPlanRepository (#974)

--- tmail-backend/rate-limiter/rate-limiter-postgres/src/main/scala/com/linagora/tmail/rate/limiter/api/postgres/module/PostgresRateLimitingModule.scala
+++ tmail-backend/rate-limiter/rate-limiter-postgres/src/main/scala/com/linagora/tmail/rate/limiter/api/postgres/module/PostgresRateLimitingModule.scala
@@ -2,22 +2,24 @@ package com.linagora.tmail.rate.limiter.api.postgres.module
 
 import com.google.inject.multibindings.Multibinder
 import com.google.inject.{AbstractModule, Scopes}
+import com.linagora.tmail.rate.limiter.api.postgres.PostgresRateLimitingPlanRepository
 import com.linagora.tmail.rate.limiter.api.postgres.repository.PostgresRateLimitingPlanUserRepository
-import com.linagora.tmail.rate.limiter.api.postgres.table.PostgresRateLimitPlanUserTable
-import com.linagora.tmail.rate.limiter.api.{InMemoryRateLimitingPlanRepository, RateLimitingPlanRepository, RateLimitingPlanUserRepository, RateLimitingPlanUsernameChangeTaskStep}
+import com.linagora.tmail.rate.limiter.api.postgres.table.{PostgresRateLimitPlanModule, PostgresRateLimitPlanUserTable}
+import com.linagora.tmail.rate.limiter.api.{RateLimitingPlanRepository, RateLimitingPlanUserRepository, RateLimitingPlanUsernameChangeTaskStep}
 import org.apache.james.backends.postgres.PostgresModule
 import org.apache.james.user.api.UsernameChangeTaskStep
 
 class PostgresRateLimitingModule() extends AbstractModule {
   override def configure(): Unit = {
     bind(classOf[PostgresRateLimitingPlanUserRepository]).in(Scopes.SINGLETON)
-    bind(classOf[InMemoryRateLimitingPlanRepository]).in(Scopes.SINGLETON)
+    bind(classOf[PostgresRateLimitingPlanRepository]).in(Scopes.SINGLETON)
 
     bind(classOf[RateLimitingPlanUserRepository]).to(classOf[PostgresRateLimitingPlanUserRepository])
-    bind(classOf[RateLimitingPlanRepository]).to(classOf[InMemoryRateLimitingPlanRepository])
+    bind(classOf[RateLimitingPlanRepository]).to(classOf[PostgresRateLimitingPlanRepository])
 
     val postgresRateLimitingDataDefinitions = Multibinder.newSetBinder(binder, classOf[PostgresModule])
     postgresRateLimitingDataDefinitions.addBinding().toInstance(PostgresRateLimitPlanUserTable.MODULE)
+    postgresRateLimitingDataDefinitions.addBinding().toInstance(PostgresRateLimitPlanModule.MODULE)
 
     Multibinder.newSetBinder(binder(), classOf[UsernameChangeTaskStep])
       .addBinding()
