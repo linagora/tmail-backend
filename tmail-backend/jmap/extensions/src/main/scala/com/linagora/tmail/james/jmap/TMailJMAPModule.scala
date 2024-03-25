4861e8e1ae4f2b9697b8c3329416f1c79b8ed2d5
Tung Tran <vttran@linagora.com>
2024-03-25 09:13:04 +0700

    ISSUE-952 [ICS Reply] Binding to apps - Fix guice instance for JMAPConfiguration

--- tmail-backend/jmap/extensions/src/main/scala/com/linagora/tmail/james/jmap/TMailJMAPModule.scala
+++ tmail-backend/jmap/extensions/src/main/scala/com/linagora/tmail/james/jmap/TMailJMAPModule.scala
@@ -1,16 +1,25 @@
 package com.linagora.tmail.james.jmap
 
+import java.io.FileNotFoundException
+
 import com.google.inject.name.Named
 import com.google.inject.{AbstractModule, Provides, Singleton}
-import org.apache.commons.configuration2.Configuration
+import org.apache.commons.configuration2.{Configuration, PropertiesConfiguration}
 import org.apache.james.utils.PropertiesProvider
 
+import scala.util.Try
+
 class TMailJMAPModule extends AbstractModule {
+
+  val JMAP_CONFIGURATION_EMPTY: Configuration = new PropertiesConfiguration()
 
   @Provides
   @Singleton
   @Named("jmap")
   def provideJMAPConfiguration(propertiesProvider: PropertiesProvider): Configuration =
-    propertiesProvider.getConfiguration("jmap")
+    Try(propertiesProvider.getConfiguration("jmap"))
+      .fold({
+        case _: FileNotFoundException => JMAP_CONFIGURATION_EMPTY
+      }, identity)
 
 }
