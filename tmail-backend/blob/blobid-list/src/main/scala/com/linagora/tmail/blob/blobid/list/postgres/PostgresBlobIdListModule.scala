26db33a0846e2f29aa89570bd9e0f1c4413265a9
Quan Tran <hqtran@linagora.com>
2024-03-25 15:19:55 +0700

    ISSUE-922 Implement PostgresBlobIdList

---
+++ tmail-backend/blob/blobid-list/src/main/scala/com/linagora/tmail/blob/blobid/list/postgres/PostgresBlobIdListModule.scala
@@ -0,0 +1,23 @@
+package com.linagora.tmail.blob.blobid.list.postgres
+
+import org.apache.james.backends.postgres.{PostgresModule, PostgresTable}
+import org.jooq.impl.{DSL, SQLDataType}
+import org.jooq.{Field, Record, Table}
+
+object PostgresBlobIdListModule {
+  val TABLE_NAME: Table[Record] = DSL.table("blob_id_list")
+
+  val BLOB_ID: Field[String] = DSL.field("blob_id", SQLDataType.VARCHAR.notNull)
+
+  val TABLE: PostgresTable = PostgresTable.name(TABLE_NAME.getName)
+    .createTableStep((dsl, tableName) => dsl.createTableIfNotExists(tableName)
+      .column(BLOB_ID)
+      .constraint(DSL.primaryKey(BLOB_ID)))
+    .supportsRowLevelSecurity
+    .build
+
+  val MODULE: PostgresModule = PostgresModule
+    .builder
+    .addTable(TABLE)
+    .build
+}
