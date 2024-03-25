1140e0bba8d5e0b07eddc5d4c85334b58547bc2c
Quan Tran <hqtran@linagora.com>
2024-03-25 15:19:55 +0700

    ISSUE-922 Implement PostgresBlobIdList

---
+++ tmail-backend/blob/blobid-list/src/main/scala/com/linagora/tmail/blob/blobid/list/postgres/PostgresBlobIdListDAO.scala
@@ -0,0 +1,25 @@
+package com.linagora.tmail.blob.blobid.list.postgres
+
+import com.linagora.tmail.blob.blobid.list.postgres.PostgresBlobIdListModule.{BLOB_ID, TABLE_NAME}
+import javax.inject.Inject
+import org.apache.james.backends.postgres.utils.PostgresExecutor
+import org.apache.james.blob.api.BlobId
+import reactor.core.publisher.Mono
+import reactor.core.scala.publisher.SMono
+
+class PostgresBlobIdListDAO @Inject()(postgresExecutor: PostgresExecutor) {
+  def insert(blobId: BlobId): SMono[Unit] =
+    SMono.fromPublisher(postgresExecutor.executeVoid(dsl => Mono.from(dsl.insertInto(TABLE_NAME)
+        .set(BLOB_ID, blobId.asString()))))
+      .`then`
+
+  def isStored(blobId: BlobId): SMono[java.lang.Boolean] =
+    SMono.fromPublisher(postgresExecutor.executeExists(dsl => dsl.selectOne()
+      .from(TABLE_NAME)
+      .where(BLOB_ID.eq(blobId.asString()))))
+
+  def remove(blobId: BlobId): SMono[Unit] =
+    SMono.fromPublisher(postgresExecutor.executeVoid(dsl => Mono.from(dsl.deleteFrom(TABLE_NAME)
+        .where(BLOB_ID.eq(blobId.asString())))))
+      .`then`
+}
