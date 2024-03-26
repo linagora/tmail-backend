13a932e1c6447fc44bf0030a57dbc314959a4cea
Quan Tran <hqtran@linagora.com>
2024-03-26 12:09:59 +0700

    ISSUE-922 Modularize BlobStoreModulesChooser for postgres-app
    Because of some bean annotation manipulation (up coming single save binding for example), we would not be able to stick with James's BlobStoreModulesChooser anymore.
    
    This allows postgres-app to reuse the TMail's BlobStoreModulesChooser (shared with Distributed app) to avoid duplicate a bunch of code.
    
    Retro compatibility for Distributed app is assured: default to S3 implementation

--- tmail-backend/guice/blob-guice/src/main/java/com/linagora/tmail/blob/guice/BlobStoreConfiguration.java
+++ tmail-backend/guice/blob-guice/src/main/java/com/linagora/tmail/blob/guice/BlobStoreConfiguration.java
@@ -7,6 +7,7 @@ import java.util.stream.Stream;
 
 import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.james.blob.aes.CryptoConfig;
 import org.apache.james.modules.mailbox.ConfigurationComponent;
 import org.apache.james.server.blob.deduplication.StorageStrategy;
@@ -36,6 +37,11 @@ public record BlobStoreConfiguration(BlobStoreImplName implementation,
 
         default RequireSecondaryS3BlobStoreConfig s3() {
             return implementation(BlobStoreImplName.S3);
+        }
+
+        default RequireCache postgres() {
+            return implementation(BlobStoreImplName.POSTGRES)
+                .noSecondaryS3BlobStore();
         }
     }
 
@@ -111,7 +117,8 @@ public record BlobStoreConfiguration(BlobStoreImplName implementation,
 
     public enum BlobStoreImplName {
         FILE("file"),
-        S3("s3");
+        S3("s3"),
+        POSTGRES("postgres");
 
         static String supportedImplNames() {
             return Stream.of(BlobStoreImplName.values())
@@ -172,6 +179,12 @@ public record BlobStoreConfiguration(BlobStoreImplName implementation,
     }
 
     static BlobStoreConfiguration from(Configuration configuration) throws ConfigurationException {
+        BlobStoreImplName blobStoreImplName = Optional.ofNullable(configuration.getString(BLOBSTORE_IMPLEMENTATION_PROPERTY))
+            .filter(StringUtils::isNotBlank)
+            .map(StringUtils::trim)
+            .map(BlobStoreImplName::from)
+            .orElse(BlobStoreImplName.S3);
+
         boolean cacheEnabled = configuration.getBoolean(CACHE_ENABLE_PROPERTY, false);
         boolean deduplicationEnabled = Try.ofCallable(() -> configuration.getBoolean(DEDUPLICATION_ENABLE_PROPERTY))
                 .getOrElseThrow(() -> new IllegalStateException("""
