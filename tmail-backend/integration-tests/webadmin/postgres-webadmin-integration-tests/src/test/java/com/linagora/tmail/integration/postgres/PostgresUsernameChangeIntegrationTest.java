aab364238597deb99886d0ee3fb4120dc52e940a
Tung Tran <vttran@linagora.com>
2024-03-27 15:34:00 +0700

    ISSUE-994 Adapt opensearch for Postgres distributed

--- tmail-backend/integration-tests/webadmin/postgres-webadmin-integration-tests/src/test/java/com/linagora/tmail/integration/postgres/PostgresUsernameChangeIntegrationTest.java
+++ tmail-backend/integration-tests/webadmin/postgres-webadmin-integration-tests/src/test/java/com/linagora/tmail/integration/postgres/PostgresUsernameChangeIntegrationTest.java
@@ -1,26 +1,44 @@
 package com.linagora.tmail.integration.postgres;
 
+import static com.linagora.tmail.blob.blobid.list.BlobStoreConfiguration.BlobStoreImplName.POSTGRES;
 import static com.linagora.tmail.james.jmap.OpenSearchContactConfiguration.DEFAULT_CONFIGURATION;
+import static org.apache.james.PostgresJamesConfiguration.EventBusImpl.IN_MEMORY;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Durations.ONE_HUNDRED_MILLISECONDS;
 
 import java.io.IOException;
 
-import org.apache.james.GuiceJamesServer;
+import org.apache.james.ClockExtension;
+import org.apache.james.JamesServerBuilder;
 import org.apache.james.JamesServerExtension;
+import org.apache.james.SearchConfiguration;
 import org.apache.james.backends.opensearch.ReactorOpenSearchClient;
+import org.apache.james.backends.postgres.PostgresExtension;
+import org.apache.james.utils.GuiceProbe;
 import org.awaitility.Awaitility;
 import org.awaitility.Durations;
 import org.awaitility.core.ConditionFactory;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.opensearch.client.opensearch._types.query_dsl.QueryBuilders;
 import org.opensearch.client.opensearch.core.SearchRequest;
 
+import com.google.inject.multibindings.Multibinder;
+import com.linagora.tmail.blob.blobid.list.BlobStoreConfiguration;
+import com.linagora.tmail.combined.identity.UsersRepositoryClassProbe;
+import com.linagora.tmail.encrypted.MailboxConfiguration;
+import com.linagora.tmail.encrypted.MailboxManagerClassProbe;
 import com.linagora.tmail.integration.UsernameChangeIntegrationContract;
+import com.linagora.tmail.integration.probe.RateLimitingProbe;
 import com.linagora.tmail.james.app.DockerOpenSearchExtension;
+import com.linagora.tmail.james.app.PostgresTmailConfiguration;
+import com.linagora.tmail.james.app.PostgresTmailServer;
+import com.linagora.tmail.james.common.probe.JmapGuiceContactAutocompleteProbe;
+import com.linagora.tmail.james.common.probe.JmapGuiceKeystoreManagerProbe;
+import com.linagora.tmail.james.common.probe.JmapGuiceLabelProbe;
+import com.linagora.tmail.james.common.probe.JmapSettingsProbe;
+import com.linagora.tmail.module.LinagoraTestJMAPServerModule;
+import com.linagora.tmail.team.TeamMailboxProbe;
 
 public class PostgresUsernameChangeIntegrationTest extends UsernameChangeIntegrationContract {
     private static final ConditionFactory CALMLY_AWAIT = Awaitility
@@ -32,7 +50,32 @@ public class PostgresUsernameChangeIntegrationTest extends UsernameChangeIntegra
     static DockerOpenSearchExtension opensearchExtension = new DockerOpenSearchExtension();
 
     @RegisterExtension
-    static JamesServerExtension testExtension = PostgresWebAdminBase.JAMES_SERVER_EXTENSION_SUPPLIER.get()
+    static JamesServerExtension testExtension = new JamesServerBuilder<PostgresTmailConfiguration>(tmpDir ->
+        PostgresTmailConfiguration.builder()
+            .workingDirectory(tmpDir)
+            .configurationFromClasspath()
+            .blobStore(BlobStoreConfiguration.builder()
+                .implementation(POSTGRES)
+                .disableCache()
+                .deduplication()
+                .noCryptoConfig()
+                .disableSingleSave())
+            .searchConfiguration(SearchConfiguration.openSearch())
+            .mailbox(new MailboxConfiguration(false))
+            .eventBusImpl(IN_MEMORY)
+            .build())
+        .server(configuration -> PostgresTmailServer.createServer(configuration)
+            .overrideWith(new LinagoraTestJMAPServerModule())
+            .overrideWith(binder -> Multibinder.newSetBinder(binder, GuiceProbe.class).addBinding().to(MailboxManagerClassProbe.class))
+            .overrideWith(binder -> Multibinder.newSetBinder(binder, GuiceProbe.class).addBinding().to(UsersRepositoryClassProbe.class))
+            .overrideWith(binder -> Multibinder.newSetBinder(binder, GuiceProbe.class).addBinding().to(TeamMailboxProbe.class))
+            .overrideWith(binder -> Multibinder.newSetBinder(binder, GuiceProbe.class).addBinding().to(JmapGuiceContactAutocompleteProbe.class))
+            .overrideWith(binder -> Multibinder.newSetBinder(binder, GuiceProbe.class).addBinding().to(JmapSettingsProbe.class))
+            .overrideWith(binder -> Multibinder.newSetBinder(binder, GuiceProbe.class).addBinding().to(JmapGuiceLabelProbe.class))
+            .overrideWith(binder -> Multibinder.newSetBinder(binder, GuiceProbe.class).addBinding().to(RateLimitingProbe.class))
+            .overrideWith(binder -> Multibinder.newSetBinder(binder, GuiceProbe.class).addBinding().to(JmapGuiceKeystoreManagerProbe.class)))
+        .extension(PostgresExtension.empty())
+        .extension(new ClockExtension())
         .extensions(opensearchExtension)
         .build();
 
@@ -53,11 +96,5 @@ public class PostgresUsernameChangeIntegrationTest extends UsernameChangeIntegra
                         .build())
                 .block()
                 .hits().total().value()).isEqualTo(documentCount));
-    }
-
-    @Test
-    @Disabled("TODO https://github.com/linagora/tmail-backend/issues/994")
-    void shouldAdaptContacts(GuiceJamesServer server) throws Exception {
-
     }
 }
