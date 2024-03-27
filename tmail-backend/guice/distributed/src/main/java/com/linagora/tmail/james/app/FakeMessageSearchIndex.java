14cee5432cc23c23902e7ad306593fb350e132a7
Tung Tran <vttran@linagora.com>
2024-03-27 14:49:46 +0700

    ISSUE-994 Duplicated the DisabledEmailAddressContactSearchEngine, FakeMessageSearchIndex from distributed (cassandra) to guice-distributed module

---
+++ tmail-backend/guice/distributed/src/main/java/com/linagora/tmail/james/app/FakeMessageSearchIndex.java
@@ -0,0 +1,83 @@
+package com.linagora.tmail.james.app;
+
+import java.util.Collection;
+import java.util.EnumSet;
+import java.util.List;
+
+import org.apache.commons.lang3.NotImplementedException;
+import org.apache.james.events.Group;
+import org.apache.james.mailbox.MailboxManager;
+import org.apache.james.mailbox.MailboxSession;
+import org.apache.james.mailbox.MessageUid;
+import org.apache.james.mailbox.exception.MailboxException;
+import org.apache.james.mailbox.model.Mailbox;
+import org.apache.james.mailbox.model.MailboxId;
+import org.apache.james.mailbox.model.MessageId;
+import org.apache.james.mailbox.model.SearchQuery;
+import org.apache.james.mailbox.model.UpdatedFlags;
+import org.apache.james.mailbox.store.mail.model.MailboxMessage;
+import org.apache.james.mailbox.store.search.ListeningMessageSearchIndex;
+
+import com.google.common.collect.ImmutableSet;
+
+import jakarta.mail.Flags;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+// Required for CLI
+public class FakeMessageSearchIndex extends ListeningMessageSearchIndex {
+
+    public FakeMessageSearchIndex() {
+        super(null, ImmutableSet.of(), null);
+    }
+
+    @Override
+    public Mono<Void> add(MailboxSession session, Mailbox mailbox, MailboxMessage message) {
+        throw new NotImplementedException("not implemented");
+    }
+
+    @Override
+    public Mono<Void> delete(MailboxSession session, MailboxId mailboxId, Collection<MessageUid> expungedUids) {
+        throw new NotImplementedException("not implemented");
+    }
+
+    @Override
+    public Mono<Void> deleteAll(MailboxSession session, MailboxId mailboxId) {
+        throw new NotImplementedException("not implemented");
+    }
+
+    @Override
+    public Mono<Void> update(MailboxSession session, MailboxId mailboxId, List<UpdatedFlags> updatedFlagsList) {
+        throw new NotImplementedException("not implemented");
+    }
+
+    @Override
+    public Mono<Flags> retrieveIndexedFlags(Mailbox mailbox, MessageUid uid) {
+        throw new NotImplementedException("not implemented");
+    }
+
+    @Override
+    public Group getDefaultGroup() {
+        throw new NotImplementedException("not implemented");
+    }
+
+    @Override
+    public Flux<MessageUid> doSearch(MailboxSession session, Mailbox mailbox, SearchQuery searchQuery) throws MailboxException {
+        throw new NotImplementedException("not implemented");
+    }
+
+    @Override
+    public Flux<MessageId> search(MailboxSession session, Collection<MailboxId> mailboxIds, SearchQuery searchQuery, long limit) {
+        throw new NotImplementedException("not implemented");
+    }
+
+    @Override
+    public EnumSet<MailboxManager.SearchCapabilities> getSupportedCapabilities(EnumSet<MailboxManager.MessageCapabilities> messageCapabilities) {
+        throw new NotImplementedException("not implemented");
+    }
+
+    @Override
+    public ExecutionMode getExecutionMode() {
+        throw new NotImplementedException("not implemented");
+    }
+}
