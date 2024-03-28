be1accb88bc25f2b844fbff29722bad5dd33aab9
vttran <vttran@linagora.com>
2024-03-28 18:12:21 +0700

    Guice binding Encrypted mailbox module to Postgres app (#995)
    * Guice binding Encrypted mailbox module to Postgres app

--- tmail-backend/mailbox/encrypted/mailbox-encrypted-postgres/src/main/java/com/linagora/tmail/encrypted/postgres/PostgresEncryptedEmailContentStoreModule.java
+++ tmail-backend/mailbox/encrypted/mailbox-encrypted-postgres/src/main/java/com/linagora/tmail/encrypted/postgres/PostgresEncryptedEmailContentStoreModule.java
@@ -10,8 +10,9 @@ import com.linagora.tmail.encrypted.EncryptedEmailContentStore;
 public class PostgresEncryptedEmailContentStoreModule extends AbstractModule {
     @Override
     protected void configure() {
-        bind(EncryptedEmailContentStore.class).to(PostgresEncryptedEmailContentStore.class);
         bind(PostgresEncryptedEmailStoreDAO.class).in(Scopes.SINGLETON);
+        bind(PostgresEncryptedEmailContentStore.class).in(Scopes.SINGLETON);
+        bind(EncryptedEmailContentStore.class).to(PostgresEncryptedEmailContentStore.class);
 
         Multibinder<PostgresModule> postgresDataDefinitions = Multibinder.newSetBinder(binder(), PostgresModule.class);
         postgresDataDefinitions.addBinding().toInstance(com.linagora.tmail.encrypted.postgres.table.PostgresEncryptedEmailStoreModule.MODULE);
