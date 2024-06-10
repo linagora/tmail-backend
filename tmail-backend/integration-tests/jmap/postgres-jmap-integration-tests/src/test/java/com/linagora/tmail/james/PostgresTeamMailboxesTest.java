617a21cfa12424951d0b550a913f41d2b0841b4c
Tung Tran <vttran@linagora.com>
2024-06-10 10:17:26 +0700

    Update PostgresTeamMailboxesTest: using rabbitmq eventbus for testing
    - More stable

--- tmail-backend/integration-tests/jmap/postgres-jmap-integration-tests/src/test/java/com/linagora/tmail/james/PostgresTeamMailboxesTest.java
+++ tmail-backend/integration-tests/jmap/postgres-jmap-integration-tests/src/test/java/com/linagora/tmail/james/PostgresTeamMailboxesTest.java
@@ -1,28 +1,69 @@
 package com.linagora.tmail.james;
 
-import static com.linagora.tmail.james.TmailJmapBase.JAMES_SERVER_EXTENSION_FUNCTION;
+import static org.apache.james.PostgresJamesConfiguration.EventBusImpl.RABBITMQ;
 
+import org.apache.james.ClockExtension;
+import org.apache.james.GuiceJamesServer;
+import org.apache.james.JamesServerBuilder;
 import org.apache.james.JamesServerExtension;
+import org.apache.james.SearchConfiguration;
+import org.apache.james.backends.postgres.PostgresExtension;
+import org.apache.james.jmap.rfc8621.contract.probe.DelegationProbeModule;
 import org.apache.james.utils.GuiceProbe;
 import org.apache.james.webadmin.RandomPortSupplier;
 import org.apache.james.webadmin.WebAdminConfiguration;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 import com.google.inject.multibindings.Multibinder;
 import com.google.inject.util.Modules;
+import com.linagora.tmail.blob.blobid.list.BlobStoreConfiguration;
+import com.linagora.tmail.combined.identity.UsersRepositoryClassProbe;
+import com.linagora.tmail.encrypted.MailboxManagerClassProbe;
+import com.linagora.tmail.james.app.DockerOpenSearchExtension;
+import com.linagora.tmail.james.app.PostgresTmailConfiguration;
+import com.linagora.tmail.james.app.PostgresTmailServer;
+import com.linagora.tmail.james.app.RabbitMQExtension;
 import com.linagora.tmail.james.common.TeamMailboxesContract;
+import com.linagora.tmail.module.LinagoraTestJMAPServerModule;
 import com.linagora.tmail.team.TeamMailboxProbe;
 
 public class PostgresTeamMailboxesTest implements TeamMailboxesContract {
 
     @RegisterExtension
-    static JamesServerExtension testExtension = JAMES_SERVER_EXTENSION_FUNCTION
-        .apply(Modules.combine(binder -> binder.bind(WebAdminConfiguration.class).toInstance(WebAdminConfiguration.builder()
-                .port(new RandomPortSupplier())
-                .enabled()
-                .host("127.0.0.1")
-                .build()),
-            binder -> Multibinder.newSetBinder(binder, GuiceProbe.class)
-                .addBinding().to(TeamMailboxProbe.class)))
+    static JamesServerExtension testExtension = new JamesServerBuilder<PostgresTmailConfiguration>(tmpDir ->
+        PostgresTmailConfiguration.builder()
+            .workingDirectory(tmpDir)
+            .configurationFromClasspath()
+            .blobStore(BlobStoreConfiguration.builder()
+                .postgres()
+                .disableCache()
+                .deduplication()
+                .noCryptoConfig()
+                .disableSingleSave())
+            .searchConfiguration(SearchConfiguration.openSearch())
+            .eventBusImpl(RABBITMQ)
+            .build())
+        .server(configuration -> PostgresTmailServer.createServer(configuration)
+            .overrideWith(new LinagoraTestJMAPServerModule())
+            .overrideWith(binder -> Multibinder.newSetBinder(binder, GuiceProbe.class).addBinding().to(MailboxManagerClassProbe.class))
+            .overrideWith(binder -> Multibinder.newSetBinder(binder, GuiceProbe.class).addBinding().to(UsersRepositoryClassProbe.class))
+            .overrideWith(binder -> Multibinder.newSetBinder(binder, GuiceProbe.class).addBinding().to(TeamMailboxProbe.class))
+            .overrideWith(new DelegationProbeModule())
+            .overrideWith(Modules.combine(binder -> binder.bind(WebAdminConfiguration.class).toInstance(WebAdminConfiguration.builder()
+                    .port(new RandomPortSupplier())
+                    .enabled()
+                    .host("127.0.0.1")
+                    .build()),
+                binder -> Multibinder.newSetBinder(binder, GuiceProbe.class).addBinding().to(TeamMailboxProbe.class))))
+        .extension(PostgresExtension.empty())
+        .extension(new RabbitMQExtension())
+        .extension(new DockerOpenSearchExtension())
+        .extension(new ClockExtension())
         .build();
+
+    @Override
+    @Disabled("unstable")
+    public void webSocketShouldPushTeamMailboxStateChanges(GuiceJamesServer server) {
+    }
 }
