cabc7309a895dfd6880c60bbc2a6398b532ccab8
HoussemNasri <houssemnasri2001@gmail.com>
2024-10-08 11:28:38 +0100

    Implement OpenPaas mock server Junit extension

--- tmail-backend/jmap/extensions/src/main/java/com/linagora/tmail/james/jmap/contact/OpenPaasUserResponse.java
+++ tmail-backend/jmap/extensions/src/main/java/com/linagora/tmail/james/jmap/contact/OpenPaasUserResponse.java
@@ -2,8 +2,10 @@ package com.linagora.tmail.james.jmap.contact;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record OpenPaasUserResponse(@JsonProperty("id") String id,
                                    @JsonProperty("firstname") String firstname,
                                    @JsonProperty("lastname") String lastname,
@@ -11,39 +13,4 @@ public record OpenPaasUserResponse(@JsonProperty("id") String id,
                                    @JsonProperty("emails") List<String> emails,
                                    @JsonProperty("main_phone") String mainPhone,
                                    @JsonProperty("displayName") String displayName) {
-
-    @Override
-    public String firstname() {
-        return firstname;
-    }
-
-    @Override
-    public String lastname() {
-        return lastname;
-    }
-
-    @Override
-    public String preferredEmail() {
-        return preferredEmail;
-    }
-
-    @Override
-    public List<String> emails() {
-        return emails;
-    }
-
-    @Override
-    public String mainPhone() {
-        return mainPhone;
-    }
-
-    @Override
-    public String id() {
-        return id;
-    }
-
-    @Override
-    public String displayName() {
-        return displayName;
-    }
 }
