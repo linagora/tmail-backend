4a1c7aedd964a3760116a19afd69ad3107fdba44
Houssem Nasri <housi.housi2015@gmail.com>
2024-10-22 16:12:03 +0100

    Update tmail-backend/tmail-third-party/openpaas/src/main/java/com/linagora/tmail/contact/ContactAddedRabbitMqMessage.java
    Co-authored-by: Benoit TELLIER <btellier@linagora.com>

--- tmail-backend/tmail-third-party/openpaas/src/main/java/com/linagora/tmail/contact/ContactAddedRabbitMqMessage.java
+++ tmail-backend/tmail-third-party/openpaas/src/main/java/com/linagora/tmail/contact/ContactAddedRabbitMqMessage.java
@@ -11,9 +11,8 @@ public record ContactAddedRabbitMqMessage(String bookId, String bookName, String
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
     static ContactAddedRabbitMqMessage fromJSON(byte[] jsonBytes) {
-        String message = new String(jsonBytes, StandardCharsets.UTF_8);
         try {
-            return objectMapper.readValue(message, ContactAddedRabbitMqMessage.class);
+            return objectMapper.readValue(jsonBytes, ContactAddedRabbitMqMessage.class);
         } catch (JsonProcessingException e) {
             throw new RuntimeException("Failed to parse ContactAddedRabbitMqMessage", e);
         }
