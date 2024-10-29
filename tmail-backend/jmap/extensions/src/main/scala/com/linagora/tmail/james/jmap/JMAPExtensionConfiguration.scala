a42d6ae8b72450eab07dacb7111876545862012f
Quan Tran <hqtran@linagora.com>
2024-10-29 11:01:42 +0700

    ISSUE-1240 Use JMAPExtensionConfiguration to handle ticket IP validation config

--- tmail-backend/jmap/extensions/src/main/scala/com/linagora/tmail/james/jmap/JMAPExtensionConfiguration.scala
+++ tmail-backend/jmap/extensions/src/main/scala/com/linagora/tmail/james/jmap/JMAPExtensionConfiguration.scala
@@ -1,6 +1,6 @@
 package com.linagora.tmail.james.jmap
 
-import com.linagora.tmail.james.jmap.JMAPExtensionConfiguration.PUBLIC_ASSET_TOTAL_SIZE_LIMIT_DEFAULT
+import com.linagora.tmail.james.jmap.JMAPExtensionConfiguration.{PUBLIC_ASSET_TOTAL_SIZE_LIMIT_DEFAULT, TICKET_IP_VALIDATION_ENABLED}
 import eu.timepit.refined
 import org.apache.commons.configuration2.Configuration
 import org.apache.james.core.MailAddress
@@ -12,21 +12,26 @@ import scala.util.{Failure, Success, Try}
 object JMAPExtensionConfiguration {
   val PUBLIC_ASSET_TOTAL_SIZE_LIMIT_PROPERTY: String = "public.asset.total.size"
   val PUBLIC_ASSET_TOTAL_SIZE_LIMIT_DEFAULT: PublicAssetTotalSizeLimit = PublicAssetTotalSizeLimit.of(Size.of(20L, Size.Unit.M)).get
+  val TICKET_IP_VALIDATION_PROPERTY: String = "authentication.strategy.rfc8621.tickets.ip.validation.enabled"
+  val TICKET_IP_VALIDATION_ENABLED: TicketIpValidationEnable = TicketIpValidationEnable(true)
 
   val SUPPORT_MAIL_ADDRESS_PROPERTY: String = "support.mail.address"
 
   def from(configuration: Configuration): JMAPExtensionConfiguration = {
+    val publicAssetTotalSizeLimit: PublicAssetTotalSizeLimit = Option(configuration.getString(PUBLIC_ASSET_TOTAL_SIZE_LIMIT_PROPERTY, null))
+      .map(Size.parse)
+      .map(PublicAssetTotalSizeLimit.of(_).get)
+      .getOrElse(PUBLIC_ASSET_TOTAL_SIZE_LIMIT_DEFAULT)
+
     val supportMailAddressOpt: Option[MailAddress] =
       Option(configuration.getString(SUPPORT_MAIL_ADDRESS_PROPERTY, null))
         .map(s => new MailAddress(s))
 
-    JMAPExtensionConfiguration(
-      publicAssetTotalSizeLimit = Option(configuration.getString(PUBLIC_ASSET_TOTAL_SIZE_LIMIT_PROPERTY, null))
-        .map(Size.parse)
-        .map(PublicAssetTotalSizeLimit.of(_).get)
-        .getOrElse(PUBLIC_ASSET_TOTAL_SIZE_LIMIT_DEFAULT),
-      supportMailAddressOpt
-    )
+    val ticketIpValidationEnable: TicketIpValidationEnable = Option(configuration.getBoolean(TICKET_IP_VALIDATION_PROPERTY, null))
+      .map(TicketIpValidationEnable(_))
+      .getOrElse(TICKET_IP_VALIDATION_ENABLED)
+
+    JMAPExtensionConfiguration(publicAssetTotalSizeLimit, supportMailAddressOpt, ticketIpValidationEnable)
   }
 }
 
@@ -37,8 +42,9 @@ object PublicAssetTotalSizeLimit {
   }
 }
 
-case class JMAPExtensionConfiguration(publicAssetTotalSizeLimit: PublicAssetTotalSizeLimit = JMAPExtensionConfiguration.PUBLIC_ASSET_TOTAL_SIZE_LIMIT_DEFAULT,
-                                      supportMailAddress: Option[MailAddress] = Option.empty) {
+case class JMAPExtensionConfiguration(publicAssetTotalSizeLimit: PublicAssetTotalSizeLimit = PUBLIC_ASSET_TOTAL_SIZE_LIMIT_DEFAULT,
+                                      supportMailAddress: Option[MailAddress] = Option.empty,
+                                      ticketIpValidationEnable: TicketIpValidationEnable = TICKET_IP_VALIDATION_ENABLED) {
   def this(publicAssetTotalSizeLimit: PublicAssetTotalSizeLimit) = {
     this(publicAssetTotalSizeLimit, Option.empty)
   }
@@ -55,3 +61,5 @@ case class JMAPExtensionConfiguration(publicAssetTotalSizeLimit: PublicAssetTota
 case class PublicAssetTotalSizeLimit(value: UnsignedInt) {
   def asLong(): Long = value.value
 }
+
+case class TicketIpValidationEnable(value: Boolean)
