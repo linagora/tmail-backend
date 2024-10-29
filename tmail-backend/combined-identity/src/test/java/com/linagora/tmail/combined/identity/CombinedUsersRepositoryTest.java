72d7d290e8598a67bef52b58ff7fc5be6d2f4b51
TungTV <vttran@linagora.com>
2024-10-29 13:16:45 +0700

    Adapt from new master code - Fix compile code

--- tmail-backend/combined-identity/src/test/java/com/linagora/tmail/combined/identity/CombinedUsersRepositoryTest.java
+++ tmail-backend/combined-identity/src/test/java/com/linagora/tmail/combined/identity/CombinedUsersRepositoryTest.java
@@ -13,12 +13,15 @@ import org.apache.commons.configuration2.tree.ImmutableNode;
 import org.apache.james.backends.cassandra.CassandraClusterExtension;
 import org.apache.james.core.Username;
 import org.apache.james.domainlist.api.DomainList;
+import org.apache.james.metrics.api.NoopGaugeRegistry;
 import org.apache.james.user.api.UsersRepository;
 import org.apache.james.user.cassandra.CassandraRepositoryConfiguration;
 import org.apache.james.user.cassandra.CassandraUsersDAO;
 import org.apache.james.user.cassandra.CassandraUsersRepositoryModule;
 import org.apache.james.user.ldap.DockerLdapSingleton;
+import org.apache.james.user.ldap.LDAPConnectionFactory;
 import org.apache.james.user.ldap.LdapGenericContainer;
+import org.apache.james.user.ldap.LdapRepositoryConfiguration;
 import org.apache.james.user.ldap.ReadOnlyLDAPUsersDAO;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -60,8 +63,11 @@ public class CombinedUsersRepositoryTest {
         @BeforeEach
         void setUp(CombinedTestSystem testSystem) throws Exception {
             this.testSystem = testSystem;
-            readOnlyLDAPUsersDAO = new ReadOnlyLDAPUsersDAO();
-            readOnlyLDAPUsersDAO.configure(ldapRepositoryConfiguration(ldapContainer, true));
+            HierarchicalConfiguration<ImmutableNode> config = ldapRepositoryConfiguration(ldapContainer, true);
+            LdapRepositoryConfiguration ldapConfiguration = LdapRepositoryConfiguration.from(config);
+            readOnlyLDAPUsersDAO = new ReadOnlyLDAPUsersDAO(new NoopGaugeRegistry(), new LDAPConnectionFactory(ldapConfiguration).getLdapConnectionPool(),
+                ldapConfiguration);
+            readOnlyLDAPUsersDAO.configure(config);
             readOnlyLDAPUsersDAO.init();
 
             cassandraUsersDAO = new CassandraUsersDAO(cassandraCluster.getCassandraCluster().getConf(), CassandraRepositoryConfiguration.DEFAULT);
@@ -99,8 +105,11 @@ public class CombinedUsersRepositoryTest {
         @BeforeEach
         void setUp(CombinedTestSystem testSystem) throws Exception {
             this.testSystem = testSystem;
-            readOnlyLDAPUsersDAO = new ReadOnlyLDAPUsersDAO();
-            readOnlyLDAPUsersDAO.configure(ldapRepositoryConfiguration(ldapContainer, false));
+            HierarchicalConfiguration<ImmutableNode> config = ldapRepositoryConfiguration(ldapContainer, false);
+            LdapRepositoryConfiguration ldapConfiguration = LdapRepositoryConfiguration.from(config);
+            readOnlyLDAPUsersDAO = new ReadOnlyLDAPUsersDAO(new NoopGaugeRegistry(), new LDAPConnectionFactory(ldapConfiguration).getLdapConnectionPool(),
+                ldapConfiguration);
+            readOnlyLDAPUsersDAO.configure(config);
             readOnlyLDAPUsersDAO.init();
 
             cassandraUsersDAO = new CassandraUsersDAO(cassandraCluster.getCassandraCluster().getConf(), CassandraRepositoryConfiguration.DEFAULT);
