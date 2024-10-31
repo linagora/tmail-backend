2d13cd49b846971522f52bcbf9e83a79ae965d31
TungTV <vttran@linagora.com>
2024-10-31 13:12:56 +0700

    Fixup LinagoraTestJMAPServerModule - should respect jmap configuration file when present

--- tmail-backend/guice/protocols/jmap/src/test/java/com/linagora/tmail/module/LinagoraTestJMAPServerModule.java
+++ tmail-backend/guice/protocols/jmap/src/test/java/com/linagora/tmail/module/LinagoraTestJMAPServerModule.java
@@ -1,10 +1,14 @@
 package com.linagora.tmail.module;
 
+import java.io.FileNotFoundException;
+
 import jakarta.inject.Singleton;
 
 import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.apache.james.modules.TestJMAPServerModule;
+import org.apache.james.utils.PropertiesProvider;
 
 import com.google.inject.Provides;
 import com.google.inject.name.Named;
@@ -13,10 +17,15 @@ public class LinagoraTestJMAPServerModule extends TestJMAPServerModule {
     @Provides
     @Singleton
     @Named("jmap")
-    Configuration provideConfiguration() {
-        Configuration configuration = new PropertiesConfiguration();
-        configuration.addProperty("calendarEvent.reply.mailTemplateLocation", "classpath://eml/");
-        configuration.addProperty("url.prefix", "http://localhost:8000");
-        return configuration;
+    Configuration provideConfiguration(PropertiesProvider propertiesProvider) throws ConfigurationException {
+        try {
+            return propertiesProvider.getConfiguration("jmap");
+        } catch (FileNotFoundException e) {
+            // return a default configuration for the test
+            Configuration configuration = new PropertiesConfiguration();
+            configuration.addProperty("calendarEvent.reply.mailTemplateLocation", "classpath://eml/");
+            configuration.addProperty("url.prefix", "http://localhost:8000");
+            return configuration;
+        }
     }
 }
