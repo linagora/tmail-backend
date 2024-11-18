e068afe702c6cb29606417d7fc019bdde10b11e8
HoussemNasri <houssemnasri2001@gmail.com>
2024-11-18 18:03:50 +0100

    [ISSUE-1257] Use OpenPaasAmqpForwardAttribute in MemoryCalendarEventReplyWithAMQPWorkflowTest

--- tmail-backend/integration-tests/jmap/memory-jmap-integration-tests/src/test/java/com/linagora/tmail/james/MemoryCalendarEventReplyWithAMQPWorkflowTest.java
+++ tmail-backend/integration-tests/jmap/memory-jmap-integration-tests/src/test/java/com/linagora/tmail/james/MemoryCalendarEventReplyWithAMQPWorkflowTest.java
@@ -3,10 +3,13 @@ package com.linagora.tmail.james;
 import static org.apache.james.data.UsersRepositoryModuleChooser.Implementation.DEFAULT;
 
 import java.io.File;
-import java.nio.charset.StandardCharsets;
+import java.io.FileInputStream;
+import java.net.URI;
 import java.nio.file.Path;
 import java.util.Optional;
 import java.util.UUID;
+
+import jakarta.inject.Singleton;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.james.JamesServerBuilder;
@@ -16,6 +19,12 @@ import org.apache.james.transport.mailets.amqp.AmqpExtension;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 import com.github.fge.lambdas.Throwing;
+import com.google.inject.AbstractModule;
+import com.google.inject.Module;
+import com.google.inject.Provides;
+import com.linagora.tmail.AmqpUri;
+import com.linagora.tmail.OpenPaasModule;
+import com.linagora.tmail.configuration.OpenPaasConfiguration;
 import com.linagora.tmail.james.app.MemoryConfiguration;
 import com.linagora.tmail.james.app.MemoryServer;
 import com.linagora.tmail.james.common.LinagoraCalendarEventReplyWithAMQPWorkflowContract;
@@ -38,12 +47,12 @@ public class MemoryCalendarEventReplyWithAMQPWorkflowTest implements LinagoraCal
             Throwing.runnable(() -> FileUtils.copyDirectory(new File(ClassLoader.getSystemClassLoader().getResource(".").getFile()),
                 confPath.toFile())).run();
 
-            // Replace amqp uri in the mailetcontainer_with_amqpforward_openpass.xml and write it to mailetcontainer.xml
-            Throwing.runnable(() -> {
-                String sourceContent = FileUtils.readFileToString(confPath.resolve("mailetcontainer_with_amqpforward_openpass.xml").toFile(), StandardCharsets.UTF_8);
-                String newContent = sourceContent.replace("{{AmqpForwardAttribute_uri}}", amqpExtension.getAmqpUri());
-                FileUtils.writeStringToFile(confPath.resolve("mailetcontainer.xml").toFile(), newContent, StandardCharsets.UTF_8);
-            }).run();
+            // Write content of mailetcontainer_with_amqpforward_openpass.xml to mailetcontainer.xml.
+            Throwing.runnable(() ->
+                FileUtils.copyToFile(
+                    new FileInputStream(confPath.resolve("mailetcontainer_with_amqpforward_openpass.xml").toFile()),
+                    confPath.resolve("mailetcontainer.xml").toFile()
+                )).run();
 
             System.out.println("confPath: " + confPath);
             return MemoryConfiguration.builder()
@@ -53,7 +62,12 @@ public class MemoryCalendarEventReplyWithAMQPWorkflowTest implements LinagoraCal
                 .build();
         })
         .server(configuration -> MemoryServer.createServer(configuration)
-            .overrideWith(new LinagoraTestJMAPServerModule(), new DelegationProbeModule()))
+            .combineWith(new OpenPaasModule())
+            .overrideWith(
+                new LinagoraTestJMAPServerModule(),
+                new DelegationProbeModule(),
+                provideOpenPaasConfigurationModule(amqpExtension.getAmqpUri()))
+        )
         .build();
 
     @Override
@@ -64,5 +78,21 @@ public class MemoryCalendarEventReplyWithAMQPWorkflowTest implements LinagoraCal
     @Override
     public Optional<String> readAMQPContent() {
         return Throwing.supplier(() -> amqpExtension.readContent()).get();
+    }
+
+    private static Module provideOpenPaasConfigurationModule(String amqpUri) {
+        return new AbstractModule() {
+            @Provides
+            @Singleton
+            public OpenPaasConfiguration provideOpenPaasConfiguration() {
+                return new OpenPaasConfiguration(
+                    AmqpUri.from(amqpUri),
+                    URI.create("http://localhost:8081"),
+                    "user",
+                    "password"
+                );
+            }
+        };
+
     }
 }
