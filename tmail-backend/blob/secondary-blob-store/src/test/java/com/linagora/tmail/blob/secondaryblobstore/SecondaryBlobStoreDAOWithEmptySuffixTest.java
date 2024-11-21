888d69e67de80ef711e04fe12c33c38f56708f00
TungTV <vttran@linagora.com>
2024-11-21 15:11:36 +0700

    Adapt SecondaryBlobStoreDAOTest after migrate James

--- tmail-backend/blob/secondary-blob-store/src/test/java/com/linagora/tmail/blob/secondaryblobstore/SecondaryBlobStoreDAOWithEmptySuffixTest.java
+++ tmail-backend/blob/secondary-blob-store/src/test/java/com/linagora/tmail/blob/secondaryblobstore/SecondaryBlobStoreDAOWithEmptySuffixTest.java
@@ -380,15 +380,6 @@ public class SecondaryBlobStoreDAOWithEmptySuffixTest implements BlobStoreDAOCon
 
     @Test
     @Override
-    public void saveShouldThrowWhenNullData() {
-        BlobStoreDAO store = testee();
-
-        assertThatThrownBy(() -> Mono.from(store.save(TEST_BUCKET_NAME, TEST_BLOB_ID, (byte[]) null)).block())
-            .isInstanceOf(ObjectStoreException.class);
-    }
-
-    @Test
-    @Override
     public void saveByteSourceShouldThrowOnIOException() {
         BlobStoreDAO store = testee();
 
