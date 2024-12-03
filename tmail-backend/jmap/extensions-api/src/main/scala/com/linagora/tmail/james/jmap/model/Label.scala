1ddc17b47a57ebc7f44d0633f057e6a4c288a995
HoussemNasri <hnasri@linagora.com>
2024-12-03 21:49:45 +0100

    Move back some classes to jmap-extensions
    - Keeping the API module as small as possible

--- tmail-backend/jmap/extensions-api/src/main/scala/com/linagora/tmail/james/jmap/model/Label.scala
+++ tmail-backend/jmap/extensions-api/src/main/scala/com/linagora/tmail/james/jmap/model/Label.scala
@@ -84,35 +84,3 @@ case class UnparsedLabelId(id: Id) {
 }
 
 case class LabelIds(list: List[UnparsedLabelId])
-
-case class LabelGetRequest(accountId: AccountId,
-                           ids: Option[LabelIds],
-                           properties: Option[Properties]) extends WithAccountId {
-  def validateProperties: Either[IllegalArgumentException, Properties] =
-    properties match {
-      case None => Right(Label.allProperties)
-      case Some(value) =>
-        value -- Label.allProperties match {
-          case invalidProperties if invalidProperties.isEmpty() => Right(value ++ Label.idProperty)
-          case invalidProperties: Properties => Left(new IllegalArgumentException(s"The following properties [${invalidProperties.format()}] do not exist."))
-        }
-    }
-}
-
-object LabelGetResponse {
-  def from(accountId: AccountId, state: UuidState, list: Seq[Label], requestIds: Option[LabelIds]): LabelGetResponse =
-    requestIds match {
-      case None => LabelGetResponse(accountId, state, list)
-      case Some(value) =>
-        LabelGetResponse(
-          accountId = accountId,
-          state = state,
-          list = list.filter(label => value.list.contains(label.id.asUnparsedLabelId)),
-          notFound = value.list.diff(list.map(_.id.asUnparsedLabelId)))
-    }
-}
-
-case class LabelGetResponse(accountId: AccountId,
-                            state: UuidState,
-                            list: Seq[Label],
-                            notFound: Seq[UnparsedLabelId] = Seq())
