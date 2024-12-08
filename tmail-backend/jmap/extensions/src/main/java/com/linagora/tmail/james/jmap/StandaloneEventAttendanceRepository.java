e4015fa88ac8de3d5f05daadacfb8ffc81b9815c
HoussemNasri <hnasri@linagora.com>
2024-12-08 23:47:40 +0100

    Move flags conversion logic to AttendanceStatus

--- tmail-backend/jmap/extensions/src/main/java/com/linagora/tmail/james/jmap/StandaloneEventAttendanceRepository.java
+++ tmail-backend/jmap/extensions/src/main/java/com/linagora/tmail/james/jmap/StandaloneEventAttendanceRepository.java
@@ -1,8 +1,6 @@
 package com.linagora.tmail.james.jmap;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 import jakarta.inject.Inject;
 import jakarta.mail.Flags;
@@ -23,7 +21,6 @@ import reactor.core.publisher.Mono;
 
 public class StandaloneEventAttendanceRepository implements EventAttendanceRepository {
     private static final Logger LOGGER = LoggerFactory.getLogger(StandaloneEventAttendanceRepository.class);
-    private static final List<String> EVENT_ATTENDANCE_FLAGS = List.of("$accepted", "$rejected", "$tentativelyaccepted", "$needs-action");
 
     private final MessageIdManager messageIdManager;
     private final SessionProvider sessionProvider;
@@ -39,7 +36,7 @@ public class StandaloneEventAttendanceRepository implements EventAttendanceRepos
         MailboxSession systemMailboxSession = sessionProvider.createSystemSession(username);
 
         return getFlags(messageId, systemMailboxSession)
-            .flatMap(this::getAttendanceStatusFromFlags)
+            .flatMap(userFlags -> Mono.justOrEmpty(AttendanceStatus.fromMessageFlags(userFlags)))
             .switchIfEmpty(handleMissingEventAttendanceFlag(messageId, systemMailboxSession));
     }
 
@@ -59,48 +56,10 @@ public class StandaloneEventAttendanceRepository implements EventAttendanceRepos
 
         return getFlags(messageId, systemMailboxSession)
             .flatMap(flags -> {
-                EVENT_ATTENDANCE_FLAGS.forEach(flags::remove);
-                String attendanceFlag =
-                    getFlagFromAttendanceStatus(attendanceStatus).orElseGet(() -> {
-                        LOGGER.warn("No flag found for attendance status {}", attendanceStatus);
-                        LOGGER.warn("Falling back to $needs-action");
-                        return "$needs-action";
-                    });
-                flags.add(attendanceFlag);
+                AttendanceStatus.getEventAttendanceFlags().forEach(flags::remove);
+                flags.add(attendanceStatus.getUserFlag());
                 return doSetFlags(messageId, flags, systemMailboxSession);
             }).then();
-    }
-
-    private Optional<String> getFlagFromAttendanceStatus(AttendanceStatus attendanceStatus) {
-        return switch (attendanceStatus) {
-            case Accepted -> Optional.of("$accepted");
-            case Declined -> Optional.of("$rejected");
-            case Tentative -> Optional.of("$tentativelyaccepted");
-            case NeedsAction -> Optional.of("$needs-action");
-            case Delegated -> Optional.empty();
-        };
-    }
-
-    private Mono<AttendanceStatus> getAttendanceStatusFromFlags(Flags flags) {
-        long eventAttendanceFlagsCount = Arrays.stream(flags.getUserFlags())
-            .filter(EVENT_ATTENDANCE_FLAGS::contains)
-            .count();
-
-        if (eventAttendanceFlagsCount > 1) {
-            LOGGER.warn("A message should not have more than one event attendance flag");
-        }
-
-        if (flags.contains("$accepted")) {
-            return Mono.just(AttendanceStatus.Accepted);
-        } else if (flags.contains("$rejected")) {
-            return Mono.just(AttendanceStatus.Declined);
-        } else if (flags.contains("$tentativelyaccepted")) {
-            return Mono.just(AttendanceStatus.Tentative);
-        } else if (flags.contains("$needs-action")) {
-            return Mono.just(AttendanceStatus.NeedsAction);
-        } else {
-            return Mono.empty();
-        }
     }
 
     private Mono<Void> doSetFlags(MessageId messageId, Flags flagsToSet, MailboxSession session) {
