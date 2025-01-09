10357fbc00db9c7b962a488a370b084241c7dd9a
HoussemNasri <hnasri@linagora.com>
2025-01-09 14:03:46 +0100

    Add more integration tests

--- tmail-backend/integration-tests/jmap/jmap-integration-tests-common/src/main/scala/com/linagora/tmail/james/common/LinagoraCalendarEventAttendanceGetMethodContract.scala
+++ tmail-backend/integration-tests/jmap/jmap-integration-tests-common/src/main/scala/com/linagora/tmail/james/common/LinagoraCalendarEventAttendanceGetMethodContract.scala
@@ -4,8 +4,8 @@ import com.linagora.tmail.james.common.LinagoraCalendarEventMethodContractUtilit
 import io.netty.handler.codec.http.HttpHeaderNames.ACCEPT
 import io.restassured.RestAssured.{`given`, requestSpecification}
 import io.restassured.http.ContentType.JSON
+import io.restassured.specification.RequestSpecification
 import net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson
-import net.javacrumbs.jsonunit.core.Option.IGNORING_ARRAY_ORDER
 import org.apache.http.HttpStatus.SC_OK
 import org.apache.james.GuiceJamesServer
 import org.apache.james.jmap.http.UserCredential
@@ -13,7 +13,8 @@ import org.apache.james.jmap.rfc8621.contract.Fixture._
 import org.apache.james.mailbox.model.MailboxPath
 import org.apache.james.modules.MailboxProbeImpl
 import org.apache.james.utils.DataProbeImpl
-import org.junit.jupiter.api.{BeforeEach, Test}
+import org.junit.jupiter.api.{BeforeEach, Disabled, Test}
+import play.api.libs.json.Json
 
 trait LinagoraCalendarEventAttendanceGetMethodContract {
 
@@ -25,6 +26,7 @@ trait LinagoraCalendarEventAttendanceGetMethodContract {
       .addDomain(DOMAIN.asString)
       .addUser(BOB.asString, BOB_PASSWORD)
       .addUser(ALICE.asString, ALICE_PASSWORD)
+      .addUser(ANDRE.asString, ANDRE_PASSWORD)
 
     requestSpecification = baseRequestSpecBuilder(server)
       .setAuth(authScheme(UserCredential(BOB, BOB_PASSWORD)))
@@ -180,8 +182,7 @@ trait LinagoraCalendarEventAttendanceGetMethodContract {
             |    "needsAction": []
             |  },
             |  "c1"
-            |]""".stripMargin
-      )
+            |]""".stripMargin)
   }
 
   @Test
@@ -228,8 +229,272 @@ trait LinagoraCalendarEventAttendanceGetMethodContract {
             |    "needsAction": ["$blobId"]
             |  },
             |  "c1"
-            |]""".stripMargin
-      )
+            |]""".stripMargin)
+  }
+
+  @Test
+  def shouldFailWhenNumberOfBlobIdsTooLarge(): Unit = {
+    val blobIds: Array[String] = Range.inclusive(1, 999)
+      .map(_ + "")
+      .toArray
+    val blobIdsJson: String = Json.stringify(Json.arr(blobIds)).replace("[[", "[").replace("]]", "]")
+    val request: String =
+      s"""{
+         |  "using": [
+         |    "urn:ietf:params:jmap:core",
+         |    "com:linagora:params:calendar:event"],
+         |  "methodCalls": [[
+         |    "CalendarEventAttendance/get",
+         |    {
+         |      "accountId": "$ACCOUNT_ID",
+         |      "blobIds": $blobIdsJson
+         |    },
+         |    "c1"]]
+         |}""".stripMargin
+
+    val response: String =
+      `given`
+        .body(request)
+      .when
+        .post
+      .`then`
+        .statusCode(SC_OK)
+        .contentType(JSON)
+        .extract
+        .body
+        .asString
+
+    assertThatJson(response)
+      .inPath("methodResponses[0]")
+      .isEqualTo(
+        s"""[
+           |    "error",
+           |    {
+           |        "type": "requestTooLarge",
+           |        "description": "The number of ids requested by the client exceeds the maximum number the server is willing to process in a single method call"
+           |    },
+           |    "c1"
+           |]""".stripMargin)
+  }
+
+  @Test
+  def shouldFailWhenWrongAccountId(): Unit = {
+    val request: String =
+      s"""{
+         |  "using": [
+         |    "urn:ietf:params:jmap:core",
+         |    "com:linagora:params:calendar:event"],
+         |  "methodCalls": [[
+         |    "CalendarEventAttendance/get",
+         |    {
+         |      "accountId": "unknownAccountId",
+         |      "blobIds": [ "0f9f65ab-dc7b-4146-850f-6e4881093965" ]
+         |    },
+         |    "c1"]]
+         |}""".stripMargin
+
+    val response =
+      `given`
+        .body(request)
+      .when
+        .post
+      .`then`
+        .statusCode(SC_OK)
+        .contentType(JSON)
+        .extract
+        .body
+        .asString
+
+    assertThatJson(response)
+      .inPath("methodResponses[0]")
+      .isEqualTo(
+        s"""[
+           |    "error",
+           |    {
+           |        "type": "accountNotFound"
+           |    },
+           |    "c1"
+           |]""".stripMargin)
+  }
+
+  @Disabled("A system session is used to fetch message flags from user inbox which does not seem to handle user mailbox isolation.")
+  @Test
+  def shouldNotFoundWhenDoesNotHavePermission(server: GuiceJamesServer): Unit = {
+    val blobId: String =
+      sendInvitationEmailToBobAndGetIcsBlobIds(server, "emailWithAliceInviteBobIcsAttachment.eml", icsPartId = "3")
+
+    val request: String =
+      s"""{
+         |  "using": [
+         |    "urn:ietf:params:jmap:core",
+         |    "com:linagora:params:calendar:event"],
+         |  "methodCalls": [[
+         |    "CalendarEventAttendance/get",
+         |    {
+         |      "accountId": "$ANDRE_ACCOUNT_ID",
+         |      "blobIds": [ "$blobId" ]
+         |    },
+         |    "c1"]]
+         |}""".stripMargin
+
+    val response =
+      `given`(buildAndreRequestSpecification(server))
+        .body(request)
+      .when
+        .post
+      .`then`
+        .statusCode(SC_OK)
+        .contentType(JSON)
+        .extract
+        .body
+        .asString
+
+    assertThatJson(response)
+      .inPath("methodResponses[0]")
+      .isEqualTo(
+        s"""|[
+            |  "CalendarEventAttendance/get",
+            |  {
+            |    "accountId": "$ANDRE_ACCOUNT_ID",
+            |    "accepted": [],
+            |    "rejected": [],
+            |    "tentativelyAccepted": [],
+            |    "needsAction": [],
+            |    "notFound": ["$blobId"]
+            |  },
+            |  "c1"
+            |]""".stripMargin)
+  }
+
+  @Test
+  def shouldReturnUnknownMethodWhenMissingOneCapability(): Unit = {
+    val request: String =
+      s"""{
+         |  "using": ["urn:ietf:params:jmap:core"],
+         |  "methodCalls": [[
+         |    "CalendarEventAttendance/get",
+         |    {
+         |      "accountId": "$ACCOUNT_ID",
+         |      "blobIds": [ "123" ]
+         |    },
+         |    "c1"]]
+         |}""".stripMargin
+
+    val response =
+      `given`
+        .body(request)
+      .when
+        .post
+      .`then`
+        .statusCode(SC_OK)
+        .contentType(JSON)
+        .extract
+        .body
+        .asString
+
+    assertThatJson(response)
+      .inPath("methodResponses[0]")
+      .isEqualTo(
+        s"""[
+           |    "error",
+           |    {
+           |      "type": "unknownMethod",
+           |      "description": "Missing capability(ies): com:linagora:params:calendar:event"
+           |    },
+           |    "c1"]""".stripMargin)
+  }
+
+  @Test
+  def shouldReturnUnknownMethodWhenMissingAllCapabilities(): Unit = {
+    val request: String =
+      s"""{
+         |  "using": [],
+         |  "methodCalls": [[
+         |    "CalendarEventAttendance/get",
+         |    {
+         |      "accountId": "$ACCOUNT_ID",
+         |      "blobIds": [ "123" ]
+         |    },
+         |    "c1"]]
+         |}""".stripMargin
+
+    val response =
+      `given`
+        .body(request)
+      .when
+        .post
+      .`then`
+        .statusCode(SC_OK)
+        .contentType(JSON)
+        .extract
+        .body
+        .asString
+
+    assertThatJson(response)
+      .inPath("methodResponses[0]")
+      .isEqualTo(
+        s"""[
+           |    "error",
+           |    {
+           |      "type": "unknownMethod",
+           |      "description": "Missing capability(ies): urn:ietf:params:jmap:core, com:linagora:params:calendar:event"
+           |    },
+           |    "c1"]""".stripMargin)
+  }
+
+  @Test
+  def shouldSucceedWhenMixSeveralCases(server: GuiceJamesServer): Unit = {
+    val acceptedEventBlobId: String =
+      sendInvitationEmailToBobAndGetIcsBlobIds(server, "emailWithAliceInviteBobIcsAttachment.eml", icsPartId = "3")
+    acceptInvitation(acceptedEventBlobId)
+
+    val rejectedEventBlobId: String =
+      sendInvitationEmailToBobAndGetIcsBlobIds(server, "emailWithAliceInviteBobIcsAttachment.eml", icsPartId = "3")
+    rejectInvitation(rejectedEventBlobId)
+
+    val needsActionEventBlobId: String =
+    sendInvitationEmailToBobAndGetIcsBlobIds(server, "emailWithAliceInviteBobIcsAttachment.eml", icsPartId = "3")
+
+    val request: String =
+      s"""{
+         |  "using": [
+         |    "urn:ietf:params:jmap:core",
+         |    "com:linagora:params:calendar:event"],
+         |  "methodCalls": [[
+         |    "CalendarEventAttendance/get",
+         |    {
+         |      "accountId": "$ACCOUNT_ID",
+         |      "blobIds": [ "$acceptedEventBlobId", "$rejectedEventBlobId", "$needsActionEventBlobId" ]
+         |    },
+         |    "c1"]]
+         |}""".stripMargin
+
+    val response =
+      `given`
+        .body(request)
+      .when
+        .post
+      .`then`
+        .statusCode(SC_OK)
+        .contentType(JSON)
+        .extract
+        .body
+        .asString
+    println(response)
+    assertThatJson(response)
+      .inPath("methodResponses[0]")
+      .isEqualTo(
+        s"""|[
+            |  "CalendarEventAttendance/get",
+            |  {
+            |    "accountId": "29883977c13473ae7cb7678ef767cbfbaffc8a44a6e463d971d23a65c1dc4af6",
+            |    "accepted": ["$acceptedEventBlobId"],
+            |    "rejected": ["$rejectedEventBlobId"],
+            |    "tentativelyAccepted": [],
+            |    "needsAction": ["$needsActionEventBlobId"]
+            |  },
+            |  "c1"
+            |]""".stripMargin)
   }
 
   private def acceptInvitation(blobId: String) = {
@@ -280,7 +545,6 @@ trait LinagoraCalendarEventAttendanceGetMethodContract {
       .contentType(JSON)
   }
 
-
   private def maybeInvitation(blobId: String) = {
     val request: String =
       s"""{
@@ -304,5 +568,11 @@ trait LinagoraCalendarEventAttendanceGetMethodContract {
       .statusCode(SC_OK)
       .contentType(JSON)
   }
+
+  private def buildAndreRequestSpecification(server: GuiceJamesServer): RequestSpecification =
+    baseRequestSpecBuilder(server)
+      .setAuth(authScheme(UserCredential(ANDRE, ANDRE_PASSWORD)))
+      .addHeader(ACCEPT.toString, ACCEPT_RFC8621_VERSION_HEADER)
+      .build
 
 }
