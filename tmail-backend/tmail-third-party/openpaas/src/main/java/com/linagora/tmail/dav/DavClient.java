fe6814090ca1f4b1f8bf4f9eaac050db65e8557d
HoussemNasri <hnasri@linagora.com>
2025-01-20 13:41:13 +0100

    Refactor

--- tmail-backend/tmail-third-party/openpaas/src/main/java/com/linagora/tmail/dav/DavClient.java
+++ tmail-backend/tmail-third-party/openpaas/src/main/java/com/linagora/tmail/dav/DavClient.java
@@ -23,13 +23,11 @@ import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 import java.util.regex.Pattern;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.auth.UsernamePasswordCredentials;
-import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -46,6 +44,7 @@ import com.linagora.tmail.james.jmap.model.CalendarAttendeeParticipationStatus;
 import com.linagora.tmail.james.jmap.model.CalendarEventParsed;
 import com.linagora.tmail.james.jmap.model.CalendarParticipantsField;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpMethod;
@@ -57,188 +56,202 @@ import net.fortuna.ical4j.model.component.VEvent;
 import net.fortuna.ical4j.model.parameter.PartStat;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.netty.ByteBufMono;
 import reactor.netty.http.client.HttpClient;
+import reactor.netty.http.client.HttpClientResponse;
 import scala.compat.java8.OptionConverters;
 
 public class DavClient {
     private static final Logger LOGGER = LoggerFactory.getLogger(DavClient.class);
 
-    private static final Duration RESPONSE_TIMEOUT_DEFAULT = Duration.ofSeconds(10);
+    private static final Duration DEFAULT_RESPONSE_TIMEOUT = Duration.ofSeconds(10);
     private static final String COLLECTED_ADDRESS_BOOK_PATH = "/addressbooks/%s/collected/%s.vcf";
+    private static final String CALENDARS_BASE_PATH = "/calendars/%s";
+    private static final Pattern CALENDAR_URI_PATTERN = Pattern.compile("/calendars/[^/]+/[^/]+/");
+    private static final String ACCEPT_VCARD_JSON = "application/vcard+json";
+    private static final String ACCEPT_XML = "application/xml";
+    private static final String CONTENT_TYPE_VCARD = "application/vcard";
 
     private final HttpClient client;
-    private final DavConfiguration davConfiguration;
-    private final Function<String, UsernamePasswordCredentials>
-        adminCredentialWithDelegatedFunction;
+    private final DavConfiguration config;
 
-    public DavClient(DavConfiguration davConfiguration) {
-        this.davConfiguration = davConfiguration;
-        this.client = createHttpClient(davConfiguration.trustAllSslCerts().orElse(false));
-
-        this.adminCredentialWithDelegatedFunction = openPaasUsername -> new UsernamePasswordCredentials(davConfiguration.adminCredential().getUserName() + "&" + openPaasUsername,
-            davConfiguration.adminCredential().getPassword());
+    public DavClient(DavConfiguration config) {
+        this.config = config;
+        this.client = createHttpClient(config.trustAllSslCerts().orElse(false));
     }
 
     private HttpClient createHttpClient(boolean trustAllSslCerts) {
         if (trustAllSslCerts) {
             return HttpClient.create()
-                .baseUrl(davConfiguration.baseUrl().toString())
-                .responseTimeout(davConfiguration.responseTimeout().orElse(RESPONSE_TIMEOUT_DEFAULT))
+                .baseUrl(config.baseUrl().toString())
+                .responseTimeout(config.responseTimeout().orElse(DEFAULT_RESPONSE_TIMEOUT))
                 .secure(sslContextSpec -> sslContextSpec.sslContext(
                     SslContextBuilder.forClient().trustManager(InsecureTrustManagerFactory.INSTANCE)));
         } else {
             return HttpClient.create()
-                .baseUrl(davConfiguration.baseUrl().toString())
-                .responseTimeout(davConfiguration.responseTimeout().orElse(RESPONSE_TIMEOUT_DEFAULT));
+                .baseUrl(config.baseUrl().toString())
+                .responseTimeout(config.responseTimeout().orElse(DEFAULT_RESPONSE_TIMEOUT));
         }
     }
 
-    public Mono<Boolean> existsCollectedContact(String username, String userId,
-                                                String collectedId) {
+    private UsernamePasswordCredentials createDelegatedCredentials(String username) {
+        return new UsernamePasswordCredentials(
+            config.adminCredential().getUserName() + "&" + username,
+            config.adminCredential().getPassword());
+    }
+
+    public Mono<Boolean> existsCollectedContact(String username, String userId, String collectedId) {
         Preconditions.checkArgument(StringUtils.isNotEmpty(userId), "OpenPaas user id should not be empty");
         Preconditions.checkArgument(StringUtils.isNotEmpty(collectedId), "Collected id should not be empty");
 
         return client
-            .headers(headers -> headers.add(HttpHeaderNames.ACCEPT, "application/vcard+json"))
-            .headers(headers -> headers.add(HttpHeaderNames.AUTHORIZATION, HttpUtils.createBasicAuthenticationToken(adminCredentialWithDelegatedFunction.apply(username))))
+            .headers(headers -> {
+                headers.add(HttpHeaderNames.ACCEPT, ACCEPT_VCARD_JSON);
+                headers.add(HttpHeaderNames.AUTHORIZATION,
+                    HttpUtils.createBasicAuthenticationToken(createDelegatedCredentials(username)));
+            })
             .get()
             .uri(String.format(COLLECTED_ADDRESS_BOOK_PATH, userId, collectedId))
-            .responseSingle((response, byteBufMono) -> switch (response.status().code()) {
-                case 200 -> Mono.just(true);
-                case 404 -> Mono.just(false);
-                default ->
-                    Mono.error(new DavClientException("Unexpected status code: " + response.status().code()
-                                                      + " when checking contact exists for openPaasUserId: " + userId + " and collectedId: " + collectedId));
-            });
+            .responseSingle((response, byteBufMono) -> handleContactExistsResponse(response, userId, collectedId));
     }
 
-    public Mono<Void> createCollectedContact(String username, String userId,
-                                             CardDavCreationObjectRequest creationObjectRequest) {
+    private Mono<Boolean> handleContactExistsResponse(HttpClientResponse response, String userId, String collectedId) {
+        return switch (response.status().code()) {
+            case 200 -> Mono.just(true);
+            case 404 -> Mono.just(false);
+            default -> Mono.error(new DavClientException(
+                String.format("Unexpected status code: %d when checking contact exists for userId: %s and collectedId: %s",
+                    response.status().code(), userId, collectedId)));
+        };
+    }
+
+    public Mono<Void> createCollectedContact(String username, String userId, CardDavCreationObjectRequest request) {
+        Preconditions.checkArgument(StringUtils.isNotEmpty(userId), "OpenPaas user id should not be empty");
+
         return client
-            .headers(headers -> headers.add(HttpHeaderNames.ACCEPT, "application/vcard+json"))
-            .headers(headers -> {
-                headers.add(HttpHeaderNames.CONTENT_TYPE, "text/vcard");
-                headers.add(HttpHeaderNames.AUTHORIZATION, HttpUtils.createBasicAuthenticationToken(adminCredentialWithDelegatedFunction.apply(username)));
-            })
+            .headers(headers ->
+                headers.add(HttpHeaderNames.ACCEPT, ACCEPT_VCARD_JSON)
+                    .add(HttpHeaderNames.CONTENT_TYPE, CONTENT_TYPE_VCARD)
+                    .add(HttpHeaderNames.AUTHORIZATION,
+                        HttpUtils.createBasicAuthenticationToken(createDelegatedCredentials(username))))
             .put()
-            .uri(String.format(COLLECTED_ADDRESS_BOOK_PATH, userId, creationObjectRequest.uid()))
-            .send(Mono.just(Unpooled.wrappedBuffer(creationObjectRequest.toVCard().getBytes())))
-            .responseSingle((response, byteBufMono) -> switch (response.status().code()) {
-                case 201 -> Mono.empty();
-                case 204 -> {
-                    LOGGER.info("Contact for user {} and collected id {} already exists", userId, creationObjectRequest.uid());
-                    yield Mono.empty();
-                }
-                default ->
-                    Mono.error(new DavClientException("Unexpected status code: " + response.status().code()
-                                                      + " when creating contact for user: " + userId + " and collected id: " + creationObjectRequest.uid()));
-            });
+            .uri(String.format(COLLECTED_ADDRESS_BOOK_PATH, userId, request.uid()))
+            .send(Mono.just(Unpooled.wrappedBuffer(request.toVCard().getBytes())))
+            .responseSingle((response, byteBufMono) ->
+                handleContactCreationResponse(response, userId, request));
     }
 
-    public Mono<PartStat> getUserParticipationStatus(String userId, String eventUid,
-                                                     String userEmail) {
+    private Mono<Void> handleContactCreationResponse(HttpClientResponse response, String userId, CardDavCreationObjectRequest request) {
+        return switch (response.status().code()) {
+            case 201 -> Mono.empty();
+            case 204 -> {
+                LOGGER.info("Contact for user {} and collected id {} already exists",
+                    userId, request.uid());
+                yield Mono.empty();
+            }
+            default -> Mono.error(new DavClientException(
+                String.format("Unexpected status code: %d when creating contact for user: %s and collected id: %s",
+                    response.status().code(), userId, request.uid())));
+        };
+    }
+
+    public Mono<PartStat> getUserParticipationStatus(String userId, String eventUid, String userEmail) {
         return findAllUsersCalendars(userId)
-            .flatMap(calendarURI ->
-                getContainingVCalendar(eventUid, calendarURI)
-                    .flatMapMany(vcalendar -> Flux.fromIterable(vcalendar.<VEvent>getComponents("VEVENT"))))
+            .flatMap(calendarURI -> getCalendarEvents(eventUid, calendarURI))
             .collectList()
-            .flatMap(events -> doGetUserParticipationStatus(events, userEmail))
-            .map(CalendarAttendeeParticipationStatus::value)
-            .map(PartStat::new);
-    }
-
-    private Mono<CalendarAttendeeParticipationStatus> doGetUserParticipationStatus(List<VEvent> vEvents, String userEmail) {
-        if (vEvents.size() == 1) {
-            return Mono.just(vEvents.getFirst())
-                .map(CalendarParticipantsField::from)
-                .map(participantsField ->
-                    participantsField.findParticipantByMailTo(userEmail)
-                        .flatMap(CalendarAttendeeField::participationStatus))
-                .map(OptionConverters::toJava)
-                .flatMap(Mono::justOrEmpty)
-                .doOnNext(System.out::println);
-        } else {
-            // TODO: handle recurring events
-            return Mono.empty();
-        }
-    }
-
-    private Mono<Calendar> getContainingVCalendar(String eventUid, URI calendarURI) {
-        return client.headers(headers -> headers.add(HttpHeaderNames.ACCEPT, "application/xml"))
-            .headers(headers -> headers.add("Depth", "1"))
-            .headers(headers -> headers.add(HttpHeaderNames.AUTHORIZATION,
-                HttpUtils.createBasicAuthenticationToken(
-                    davConfiguration.adminCredential().getUserName(),
-                    davConfiguration.adminCredential().getPassword())))
-            .request(HttpMethod.valueOf("REPORT"))
-            .uri(calendarURI.getPath())
-            .send(Mono.just(Unpooled.wrappedBuffer(new GetCalendarByEventIdRequestBody(eventUid).value().getBytes())))
-            .responseSingle((response, byteBufMono) -> {
-                if (response.status() == HttpResponseStatus.MULTI_STATUS) {
-                    return byteBufMono.asString(StandardCharsets.UTF_8)
-                        .flatMap(multiStatusResponse ->
-                            extractVCalendarFromResponse(XMLUtil.parse(multiStatusResponse, DavMultistatus.class)));
-                } else {
-                    return Mono.error(
-                        new DavClientException("Unexpected status code: " + response.status().code()
-                                               + " when finding VCALENDAR object containing event: " + eventUid));
-                }
-            });
-    }
-
-    public Mono<Void> setUserParticipationStatus(String userId, String eventUuid,
-                                                 PartStat partStat) {
-        return Mono.empty();
+            .flatMap(events -> extractUserParticipationStatus(events, userEmail))
+            .map(status -> new PartStat(status.value()));
     }
 
     private Flux<URI> findAllUsersCalendars(String userId) {
-        return client.headers(headers -> headers.add(HttpHeaderNames.ACCEPT, "application/xml"))
-            .headers(headers -> headers.add(HttpHeaderNames.AUTHORIZATION,
-                HttpUtils.createBasicAuthenticationToken(
-                    davConfiguration.adminCredential().getUserName(),
-                    davConfiguration.adminCredential().getPassword())))
+        return client.headers(headers ->
+                headers.add(HttpHeaderNames.ACCEPT, ACCEPT_XML)
+                    .add(HttpHeaderNames.AUTHORIZATION,
+                        HttpUtils.createBasicAuthenticationToken(
+                            config.adminCredential().getUserName(),
+                            config.adminCredential().getPassword()))
+            )
             .request(HttpMethod.valueOf("PROPFIND"))
-            .uri(String.format("/calendars/%s", userId))
+            .uri(String.format(CALENDARS_BASE_PATH, userId))
             .responseSingle((response, byteBufMono) -> {
                 if (response.status() == HttpResponseStatus.MULTI_STATUS) {
                     return byteBufMono.asString(StandardCharsets.UTF_8)
                         .map(multiStatusResponse -> XMLUtil.parse(multiStatusResponse, DavMultistatus.class))
                         .map(this::extractCalendarURIsFromResponse);
                 } else {
-                    return Mono.error(
-                        new DavClientException("Unexpected status code: " + response.status().code()
-                                                      + " when finding user calendars for user: " + userId));
+                    return Mono.error(new DavClientException(
+                        String.format("Unexpected status code: %d when finding user calendars for user: %s",
+                            response.status().code(), userId)));
                 }
-            }).flatMapMany(Flux::fromIterable);
+            })
+            .flatMapMany(Flux::fromIterable);
     }
-
-    private static final Pattern CALENDAR_URI_PATTERN = Pattern.compile("/calendars/[^/]+/[^/]+/");
 
     private List<URI> extractCalendarURIsFromResponse(DavMultistatus multistatus) {
         List<URI> hrefs = new ArrayList<>();
 
         for (DavResponse response : multistatus.getResponses()) {
             response.getHref().getValue().ifPresent(href -> {
-                if (CALENDAR_URI_PATTERN.matcher(href).matches()) {
-                    if (!(href.endsWith("inbox/") || href.endsWith("outbox/"))) {
-                        hrefs.add(URI.create(href));
-                    }
+                if (CALENDAR_URI_PATTERN.matcher(href).matches() &&
+                    !(href.endsWith("inbox/") || href.endsWith("outbox/"))) {
+                    hrefs.add(URI.create(href));
                 }
             });
         }
 
         LOGGER.trace("Found user calendars: {}", hrefs);
-
         return hrefs;
     }
 
-    private static @NotNull Mono<Calendar> extractVCalendarFromResponse(DavMultistatus multistatus) {
+    private Flux<VEvent> getCalendarEvents(String eventUid, URI calendarURI) {
+        return client.headers(headers ->
+                headers.add(HttpHeaderNames.ACCEPT, ACCEPT_XML)
+                    .add("Depth", "1")
+                    .add(HttpHeaderNames.AUTHORIZATION,
+                        HttpUtils.createBasicAuthenticationToken(config.adminCredential().getUserName(), config.adminCredential().getPassword())))
+            .request(HttpMethod.valueOf("REPORT"))
+            .uri(calendarURI.getPath())
+            .send(createCalendarRequestBody(eventUid))
+            .responseSingle((response, byteBufMono) -> handleCalendarResponse(response, byteBufMono, eventUid))
+            .flatMapMany(vcalendar -> Flux.fromIterable(vcalendar.getComponents("VEVENT")));
+    }
+
+    private Mono<CalendarAttendeeParticipationStatus> extractUserParticipationStatus(List<VEvent> events, String userEmail) {
+        if (events.size() != 1) {
+            return Mono.empty(); // TODO: handle recurring events
+        }
+
+        return Mono.just(events.getFirst())
+            .map(CalendarParticipantsField::from)
+            .map(participants ->
+                participants.findParticipantByMailTo(userEmail)
+                    .flatMap(CalendarAttendeeField::participationStatus))
+            .map(OptionConverters::toJava)
+            .flatMap(Mono::justOrEmpty);
+    }
+
+    private Mono<ByteBuf> createCalendarRequestBody(String eventUid) {
+        return Mono.just(Unpooled.wrappedBuffer(
+            new GetCalendarByEventIdRequestBody(eventUid).value().getBytes()));
+    }
+
+    private Mono<Calendar> handleCalendarResponse(HttpClientResponse response, ByteBufMono byteBufMono, String eventUid) {
+        if (response.status() == HttpResponseStatus.MULTI_STATUS) {
+            return byteBufMono.asString(StandardCharsets.UTF_8)
+                .map(content -> XMLUtil.parse(content, DavMultistatus.class))
+                .flatMap(DavClient::extractVCalendarFromResponse);
+        }
+        return Mono.error(new DavClientException(
+            String.format("Unexpected status code: %d when finding VCALENDAR object containing event: %s",
+                response.status().code(), eventUid)));
+    }
+
+    private static Mono<Calendar> extractVCalendarFromResponse(DavMultistatus multistatus) {
         return Flux.fromIterable(multistatus.getResponses())
             .next()
             .map(davResponse -> davResponse.getPropstat().getProp().getCalendarData())
             .flatMap(Mono::justOrEmpty)
-            .map(calendarData ->
-                CalendarEventParsed.parseICal4jCalendar(
-                    IOUtils.toInputStream(calendarData.getValue(), StandardCharsets.UTF_8)));
+            .map(calendarData -> CalendarEventParsed.parseICal4jCalendar(
+                IOUtils.toInputStream(calendarData.getValue(), StandardCharsets.UTF_8)));
     }
 }
