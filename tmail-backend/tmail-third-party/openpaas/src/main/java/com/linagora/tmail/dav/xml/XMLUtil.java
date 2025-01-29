d4c0103fd32d762fbd5ecb13dcfc1193c54e9e6c
HoussemNasri <hnasri@linagora.com>
2025-01-29 12:19:59 +0100

    [ISSUE-1462] Protect against XML external entity attacks

--- tmail-backend/tmail-third-party/openpaas/src/main/java/com/linagora/tmail/dav/xml/XMLUtil.java
+++ tmail-backend/tmail-third-party/openpaas/src/main/java/com/linagora/tmail/dav/xml/XMLUtil.java
@@ -19,30 +19,39 @@
 package com.linagora.tmail.dav.xml;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.StringReader;
+import java.nio.file.Files;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
 
 public class XMLUtil {
 
     public static <T> T parse(File xmlFile, Class<T> clazz) {
         try {
-            JAXBContext jaxbContext = JAXBContext.newInstance(clazz);
-            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-            return clazz.cast(unmarshaller.unmarshal(xmlFile));
-        } catch (JAXBException e) {
-            throw new RuntimeException("Failed to parse the given XML file at: " + xmlFile.getAbsolutePath(), e);
+            return parse(Files.readString(xmlFile.toPath()), clazz);
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to parse XML file", e);
         }
     }
 
     public static <T> T parse(String xml, Class<T> clazz) {
         try {
             JAXBContext jaxbContext = JAXBContext.newInstance(clazz);
+
+            XMLInputFactory xif = XMLInputFactory.newFactory();
+            xif.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, false);
+            xif.setProperty(XMLInputFactory.SUPPORT_DTD, false);
+            XMLStreamReader xsr = xif.createXMLStreamReader(new StringReader(xml));
+
             Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-            return clazz.cast(unmarshaller.unmarshal(new StringReader(xml)));
-        } catch (JAXBException e) {
+            return clazz.cast(unmarshaller.unmarshal(xsr));
+        } catch (JAXBException | XMLStreamException e) {
             throw new RuntimeException("Failed to parse the given XML string", e);
         }
     }
