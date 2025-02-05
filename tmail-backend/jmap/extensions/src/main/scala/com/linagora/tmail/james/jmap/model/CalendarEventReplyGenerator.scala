ba433f4cc108a1267445a6dddba2eaee80ae7b12
TungTV <vttran@linagora.com>
2025-02-05 17:00:34 +0700

    Adapt new version of ical4j 4.0.1

--- tmail-backend/jmap/extensions/src/main/scala/com/linagora/tmail/james/jmap/model/CalendarEventReplyGenerator.scala
+++ tmail-backend/jmap/extensions/src/main/scala/com/linagora/tmail/james/jmap/model/CalendarEventReplyGenerator.scala
@@ -19,17 +19,21 @@
 package com.linagora.tmail.james.jmap.model
 
 import java.net.URI
+import java.time.Instant
+import java.util
 import java.util.function.Predicate
 
 import com.google.common.base.Preconditions
 import net.fortuna.ical4j.model.component.{VEvent, VTimeZone}
 import net.fortuna.ical4j.model.parameter.{Cn, CuType, PartStat, Role}
+import net.fortuna.ical4j.model.property.immutable.ImmutableMethod
 import net.fortuna.ical4j.model.property.{Attendee, DtStamp, LastModified, Method}
-import net.fortuna.ical4j.model.{Calendar, DateTime, Property, PropertyList}
+import net.fortuna.ical4j.model.{Calendar, Component, Property, PropertyList}
 import net.fortuna.ical4j.validate.ValidationResult
 import org.apache.james.core.MailAddress
 
 import scala.jdk.CollectionConverters._
+import scala.jdk.OptionConverters._
 
 case class AttendeeReply(attendee: MailAddress, partStat: PartStat, cn: Option[Cn] = None)
 
@@ -38,32 +42,32 @@ object CalendarEventReplyGenerator {
   val PRODID: String = "-//Linagora//TMail Calendar//EN"
 
   def generate(eventRequest: Calendar, attendeeReply: AttendeeReply): Calendar = {
-    Preconditions.checkArgument(eventRequest.getMethod.equals(Method.REQUEST), "The method of the event request must be REQUEST".asInstanceOf[Object])
-    val requestVEventOpt: Option[VEvent] = eventRequest.getComponents[VEvent]("VEVENT").asScala.headOption
+    Preconditions.checkArgument(Method.VALUE_REQUEST.equalsIgnoreCase(eventRequest.getMethod.getValue), "The method of the event request must be REQUEST".asInstanceOf[Object])
+    val requestVEventOpt: Option[VEvent] = eventRequest.getComponents[VEvent](Component.VEVENT).asScala.headOption
     Preconditions.checkArgument(requestVEventOpt.isDefined, "The event request must be a VEVENT".asInstanceOf[Object])
 
     val requestVEvent: VEvent = requestVEventOpt.get
-    val timeZone: Option[VTimeZone] = requestVEvent.getComponents[VTimeZone]("VTIMEZONE").asScala.headOption
+    val timeZone: Option[VTimeZone] = eventRequest.getComponent[VTimeZone](Component.VTIMEZONE).toScala
 
     def notProperties(properties: String*): Predicate[Property] = property => !properties.contains(property.getName)
 
-    val replyProperty: PropertyList[Property] = Some(new PropertyList[Property])
+    val replyProperty: util.ArrayList[Property] = Some(new util.ArrayList[Property]())
       .map(propertyList => {
-        requestVEvent.getProperties
+        requestVEvent.getProperties[Property]
           .stream()
-          .filter(notProperties("ATTENDEE", "DTSTAMP", "LAST-MODIFIED"))
+          .filter(notProperties(Property.ATTENDEE, Property.DTSTAMP, Property.LAST_MODIFIED))
           .forEach(e => propertyList.add(e))
 
         propertyList.add(buildReplyAttendee(attendeeReply))
-
-        val replyTime: DateTime = new DateTime(true)
+        val replyTime: Instant = Instant.now
         propertyList.add(new DtStamp(replyTime))
         propertyList.add(new LastModified(replyTime))
 
         propertyList
       }).get
 
-    val validationResult: Seq[ValidationResult] = replyProperty.asScala
+    val replyPropertyList = new PropertyList(replyProperty)
+    val validationResult: Seq[ValidationResult] = replyPropertyList.getAll.asScala
       .map(_.validate())
       .filter(_.hasErrors)
       .toSeq
@@ -72,9 +76,9 @@ object CalendarEventReplyGenerator {
 
     val calendar = new Calendar()
       .withDefaults()
-      .withComponent(new VEvent(replyProperty).getFluentTarget)
+      .withComponent(new VEvent(replyPropertyList))
       .withProdId(PRODID)
-      .withProperty(Method.REPLY)
+      .withProperty(ImmutableMethod.REPLY)
 
     Option(eventRequest.getCalendarScale).map(calScale => calendar.withProperty(calScale))
     timeZone.map(calendar.withComponent)
@@ -89,7 +93,7 @@ object CalendarEventReplyGenerator {
       .map(attendee => {
         attendeeReply.cn.map(cn => attendee.withParameter(cn))
         attendee
-      }).get.getFluentTarget
+      }).get.getFluentTarget.asInstanceOf[Attendee]
 
   private def validationResultAsString(input: Seq[ValidationResult]): String =
     input.flatMap(_.getEntries.asScala.toSeq).map(error => s"${error.getContext} : ${error.getMessage}").mkString(";")
