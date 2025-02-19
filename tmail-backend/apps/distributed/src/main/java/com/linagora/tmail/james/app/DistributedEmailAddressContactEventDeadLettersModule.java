1e17509e6f9a6fb444be953adbe322077a2ad406
hung phan <hphan@linagora.com>
2025-02-19 16:27:03 +0700

    Bind CassandraEventDeadLetters for AUTOCOMPLETE EventBus

--- tmail-backend/apps/distributed/src/main/java/com/linagora/tmail/james/app/DistributedEmailAddressContactEventDeadLettersModule.java
+++ tmail-backend/apps/distributed/src/main/java/com/linagora/tmail/james/app/DistributedEmailAddressContactEventDeadLettersModule.java
@@ -18,22 +18,28 @@
 
 package com.linagora.tmail.james.app;
 
+import org.apache.james.events.CassandraEventDeadLetters;
+import org.apache.james.events.CassandraEventDeadLettersDAO;
+import org.apache.james.events.CassandraEventDeadLettersGroupDAO;
 import org.apache.james.events.EventDeadLetters;
 
+import com.datastax.oss.driver.api.core.CqlSession;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
 import com.google.inject.name.Named;
 import com.linagora.tmail.james.jmap.EmailAddressContactInjectKeys;
+import com.linagora.tmail.james.jmap.contact.TmailJmapEventSerializer;
 
 public class DistributedEmailAddressContactEventDeadLettersModule extends AbstractModule {
 
-    // TODO: for pass ci on postgresql branch,
-    // It should be replaced by https://github.com/linagora/tmail-backend/pull/1016  (when rebase master branch)
     @Provides
     @Singleton
     @Named(EmailAddressContactInjectKeys.AUTOCOMPLETE)
-    EventDeadLetters provideEventDeadLetters(EventDeadLetters eventDeadLetters) {
-        return eventDeadLetters;
+    EventDeadLetters provideEventDeadLetters(CassandraEventDeadLettersGroupDAO cassandraEventDeadLettersGroupDAO,
+                                             CqlSession session,
+                                             TmailJmapEventSerializer tmailJmapEventSerializer) {
+        return new CassandraEventDeadLetters(new CassandraEventDeadLettersDAO(session, tmailJmapEventSerializer),
+            cassandraEventDeadLettersGroupDAO);
     }
 }
