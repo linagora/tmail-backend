50194f64f01e79288d9b3cfd55c2a086059a7e33
Quan Tran <hqtran@linagora.com>
2025-02-28 10:07:35 +0700

    ISSUE-1532 [Postgresql] Backport bundle compute JMAP preview into OpenSearch indexing
    backport https://github.com/linagora/tmail-backend/pull/1508

--- tmail-backend/integration-tests/jmap/postgres-jmap-integration-tests/src/test/java/com/linagora/tmail/james/PostgresEmailGetMethodTest.java
+++ tmail-backend/integration-tests/jmap/postgres-jmap-integration-tests/src/test/java/com/linagora/tmail/james/PostgresEmailGetMethodTest.java
@@ -18,23 +18,127 @@
 
 package com.linagora.tmail.james;
 
-import static com.linagora.tmail.james.TmailJmapBase.JAMES_SERVER_EXTENSION_FUNCTION;
 import static com.linagora.tmail.james.TmailJmapBase.MESSAGE_ID_FACTORY;
+import static org.apache.james.MailsShouldBeWellReceived.CALMLY_AWAIT;
+import static org.apache.james.MailsShouldBeWellReceived.DOMAIN;
+import static org.apache.james.MailsShouldBeWellReceived.JAMES_USER;
+import static org.apache.james.MailsShouldBeWellReceived.PASSWORD;
+import static org.apache.james.PostgresJamesConfiguration.EventBusImpl.RABBITMQ;
 
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+
+import org.apache.james.ClockExtension;
+import org.apache.james.GuiceJamesServer;
+import org.apache.james.JamesServerBuilder;
 import org.apache.james.JamesServerExtension;
+import org.apache.james.SearchConfiguration;
+import org.apache.james.backends.postgres.PostgresExtension;
+import org.apache.james.core.Username;
 import org.apache.james.jmap.rfc8621.contract.EmailGetMethodContract;
 import org.apache.james.jmap.rfc8621.contract.probe.DelegationProbeModule;
+import org.apache.james.junit.categories.BasicFeature;
+import org.apache.james.mailbox.DefaultMailboxes;
+import org.apache.james.mailbox.MessageManager;
+import org.apache.james.mailbox.model.MailboxPath;
 import org.apache.james.mailbox.model.MessageId;
+import org.apache.james.mailbox.opensearch.IndexBody;
+import org.apache.james.mailbox.opensearch.OpenSearchMailboxConfiguration;
+import org.apache.james.mime4j.dom.Message;
+import org.apache.james.modules.MailboxProbeImpl;
+import org.apache.james.utils.DataProbeImpl;
+import org.apache.james.utils.GuiceProbe;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
+
+import com.google.inject.multibindings.Multibinder;
+import com.linagora.tmail.blob.guice.BlobStoreConfiguration;
+import com.linagora.tmail.combined.identity.UsersRepositoryClassProbe;
+import com.linagora.tmail.encrypted.MailboxConfiguration;
+import com.linagora.tmail.encrypted.MailboxManagerClassProbe;
+import com.linagora.tmail.james.app.DockerOpenSearchExtension;
+import com.linagora.tmail.james.app.PostgresTmailConfiguration;
+import com.linagora.tmail.james.app.PostgresTmailServer;
+import com.linagora.tmail.james.app.RabbitMQExtension;
+import com.linagora.tmail.james.common.LabelChangesMethodContract;
+import com.linagora.tmail.james.common.probe.JmapGuiceContactAutocompleteProbe;
+import com.linagora.tmail.james.common.probe.JmapSettingsProbe;
+import com.linagora.tmail.james.common.probe.MessageFastViewProjectionProbe;
+import com.linagora.tmail.james.common.probe.MessageIdManagerProbe;
+import com.linagora.tmail.james.jmap.firebase.FirebaseModuleChooserConfiguration;
+import com.linagora.tmail.james.jmap.firebase.FirebasePushClient;
+import com.linagora.tmail.module.LinagoraTestJMAPServerModule;
+import com.linagora.tmail.team.TeamMailboxProbe;
 
 public class PostgresEmailGetMethodTest implements EmailGetMethodContract {
 
     @RegisterExtension
-    static JamesServerExtension testExtension = JAMES_SERVER_EXTENSION_FUNCTION.apply(new DelegationProbeModule())
+    static JamesServerExtension testExtension = new JamesServerBuilder<PostgresTmailConfiguration>(tmpDir ->
+        PostgresTmailConfiguration.builder()
+            .workingDirectory(tmpDir)
+            .configurationFromClasspath()
+            .blobStore(BlobStoreConfiguration.builder()
+                .postgres()
+                .disableCache()
+                .deduplication()
+                .noCryptoConfig()
+                .disableSingleSave())
+            .searchConfiguration(SearchConfiguration.openSearch())
+            .mailbox(new MailboxConfiguration(false))
+            .firebaseModuleChooserConfiguration(FirebaseModuleChooserConfiguration.DISABLED)
+            .eventBusImpl(RABBITMQ)
+            .build())
+        .server(configuration -> PostgresTmailServer.createServer(configuration)
+            .overrideWith(new LinagoraTestJMAPServerModule())
+            .overrideWith(binder -> Multibinder.newSetBinder(binder, GuiceProbe.class).addBinding().to(MailboxManagerClassProbe.class))
+            .overrideWith(binder -> Multibinder.newSetBinder(binder, GuiceProbe.class).addBinding().to(UsersRepositoryClassProbe.class))
+            .overrideWith(binder -> Multibinder.newSetBinder(binder, GuiceProbe.class).addBinding().to(TeamMailboxProbe.class))
+            .overrideWith(binder -> Multibinder.newSetBinder(binder, GuiceProbe.class).addBinding().to(JmapGuiceContactAutocompleteProbe.class))
+            .overrideWith(binder -> Multibinder.newSetBinder(binder, GuiceProbe.class).addBinding().to(JmapSettingsProbe.class))
+            .overrideWith(binder -> binder.bind(FirebasePushClient.class).toInstance(LabelChangesMethodContract.firebasePushClient()))
+            .overrideWith(new DelegationProbeModule())
+            .overrideWith(binder -> binder.bind(OpenSearchMailboxConfiguration.class)
+                .toInstance(OpenSearchMailboxConfiguration.builder()
+                    .indexBody(IndexBody.NO)
+                    .build()))
+            .overrideWith(binder -> Multibinder.newSetBinder(binder, GuiceProbe.class)
+                .addBinding()
+                .to(MessageFastViewProjectionProbe.class))
+            .overrideWith(binder -> Multibinder.newSetBinder(binder, GuiceProbe.class)
+                .addBinding()
+                .to(MessageIdManagerProbe.class)))
+        .extension(PostgresExtension.empty())
+        .extension(new DockerOpenSearchExtension())
+        .extension(new RabbitMQExtension())
+        .extension(new ClockExtension())
         .build();
 
     @Override
     public MessageId randomMessageId() {
         return MESSAGE_ID_FACTORY.generate();
+    }
+
+    @Tag(BasicFeature.TAG)
+    @Test
+    void jmapPreviewShouldBeWellPreComputed(GuiceJamesServer server) throws Exception {
+        server.getProbe(DataProbeImpl.class).fluent()
+            .addDomain(DOMAIN)
+            .addUser(JAMES_USER, PASSWORD);
+
+        MailboxProbeImpl mailboxProbe = server.getProbe(MailboxProbeImpl.class);
+        mailboxProbe.createMailbox("#private", JAMES_USER, DefaultMailboxes.INBOX);
+        MessageId messageId = mailboxProbe.appendMessage(JAMES_USER, MailboxPath.inbox(Username.of(JAMES_USER)),
+                MessageManager.AppendCommand.from(Message.Builder.of()
+                    .setSubject("Subject")
+                    .setBody("This is content of the message", StandardCharsets.UTF_8)
+                    .build()))
+            .getMessageId();
+
+        CALMLY_AWAIT
+            .atMost(Duration.ofSeconds(10))
+            .until(() -> server.getProbe(MessageFastViewProjectionProbe.class)
+                .retrieve(messageId)
+                .isPresent());
     }
 }
