fe0199a111e3086f928a427686ec45f0e69d2c0b
Houssem Nasri <houssemnasri2001@gmail.com>
2025-03-05 21:49:09 +0100

    ISSUE-1545 Fix checkstyle and add license

--- tmail-backend/tmail-third-party/openpaas/src/main/java/com/linagora/tmail/mailet/IsOpenPaasRabbitMqSetup.java
+++ tmail-backend/tmail-third-party/openpaas/src/main/java/com/linagora/tmail/mailet/IsOpenPaasRabbitMqSetup.java
@@ -1,10 +1,25 @@
+/********************************************************************
+ *  As a subpart of Twake Mail, this file is edited by Linagora.    *
+ *                                                                  *
+ *  https://twake-mail.com/                                         *
+ *  https://linagora.com                                            *
+ *                                                                  *
+ *  This file is subject to The Affero Gnu Public License           *
+ *  version 3.                                                      *
+ *                                                                  *
+ *  https://www.gnu.org/licenses/agpl-3.0.en.html                   *
+ *                                                                  *
+ *  This program is distributed in the hope that it will be         *
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied      *
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR         *
+ *  PURPOSE. See the GNU Affero General Public License for          *
+ *  more details.                                                   *
+ ********************************************************************/
+
 package com.linagora.tmail.mailet;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
-
-import static com.linagora.tmail.mailet.OpenPaasAmqpForwardAttribute.*;
 
 import jakarta.inject.Inject;
 import jakarta.mail.MessagingException;
@@ -15,12 +30,15 @@ import org.apache.mailet.base.GenericMatcher;
 
 public class IsOpenPaasRabbitMqSetup extends GenericMatcher {
 
-    private final OpenPaasRabbitMQChannelPoolHolder openPaasRabbitMQChannelPoolHolder;
+    private final OpenPaasAmqpForwardAttribute.OpenPaasRabbitMQChannelPoolHolder
+        openPaasRabbitMQChannelPoolHolder;
 
     @Inject
-    public IsOpenPaasRabbitMqSetup(OpenPaasRabbitMQChannelPoolHolder openPaasRabbitMQChannelPoolHolder) {
+    public IsOpenPaasRabbitMqSetup(
+        OpenPaasAmqpForwardAttribute.OpenPaasRabbitMQChannelPoolHolder openPaasRabbitMQChannelPoolHolder) {
         this.openPaasRabbitMQChannelPoolHolder = openPaasRabbitMQChannelPoolHolder;
     }
+
     @Override
     public Collection<MailAddress> match(Mail mail) throws MessagingException {
         if (openPaasRabbitMQChannelPoolHolder.get().isPresent()) {
