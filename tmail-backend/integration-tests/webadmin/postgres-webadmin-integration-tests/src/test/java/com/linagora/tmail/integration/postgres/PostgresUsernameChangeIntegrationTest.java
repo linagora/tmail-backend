7a16bde7aeed3b4fc3d15a756fab48ee1e675bce
TungTV <vttran@linagora.com>
2025-03-26 12:01:13 +0700

    OSEmailAddressContactSearchEngine - update deprecated method of opensearch java library

--- tmail-backend/integration-tests/webadmin/postgres-webadmin-integration-tests/src/test/java/com/linagora/tmail/integration/postgres/PostgresUsernameChangeIntegrationTest.java
+++ tmail-backend/integration-tests/webadmin/postgres-webadmin-integration-tests/src/test/java/com/linagora/tmail/integration/postgres/PostgresUsernameChangeIntegrationTest.java
@@ -109,7 +109,7 @@ public class PostgresUsernameChangeIntegrationTest extends UsernameChangeIntegra
             .untilAsserted(() -> assertThat(client.search(
                     new SearchRequest.Builder()
                         .index(DEFAULT_CONFIGURATION.getUserContactIndexName().getValue(), DEFAULT_CONFIGURATION.getDomainContactIndexName().getValue())
-                        .query(QueryBuilders.matchAll().build()._toQuery())
+                        .query(QueryBuilders.matchAll().build().toQuery())
                         .build())
                 .block()
                 .hits().total().value()).isEqualTo(documentCount));
