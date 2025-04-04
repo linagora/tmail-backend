2b8dd594a86845f0b6b13d7bb795db44e959aa28
Benoit TELLIER <btellier@linagora.com>
2025-04-04 10:30:06 +0200

    [STORAGE-MONGODB] Provide an OpenPaaS backed DomainList

--- twake-calendar-side-service/app/src/main/java/com/linagora/calendar/app/modules/MemoryUserModule.java
+++ twake-calendar-side-service/app/src/main/java/com/linagora/calendar/app/modules/MemoryUserModule.java
@@ -18,10 +18,7 @@
 
 package com.linagora.calendar.app.modules;
 
-import org.apache.james.dnsservice.api.DNSService;
 import org.apache.james.domainlist.api.DomainList;
-import org.apache.james.domainlist.lib.DomainListConfiguration;
-import org.apache.james.domainlist.memory.MemoryDomainList;
 import org.apache.james.rrt.api.AliasReverseResolver;
 import org.apache.james.rrt.api.CanSendFrom;
 import org.apache.james.rrt.api.RecipientRewriteTable;
@@ -33,21 +30,17 @@ import org.apache.james.user.api.UsersRepository;
 import org.apache.james.user.memory.MemoryDelegationStore;
 import org.apache.james.user.memory.MemoryUsersRepository;
 import org.apache.james.utils.GuiceProbe;
-import org.apache.james.utils.InitializationOperation;
-import org.apache.james.utils.InitilizationOperationBuilder;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Scopes;
 import com.google.inject.Singleton;
 import com.google.inject.multibindings.Multibinder;
-import com.google.inject.multibindings.ProvidesIntoSet;
 
 public class MemoryUserModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(UsersRepository.class).to(MemoryUsersRepository.class);
-        bind(DomainList.class).to(MemoryDomainList.class);
 
         // Needed in order to satisfy domain routes binding
         bind(MemoryRecipientRewriteTable.class).in(Scopes.SINGLETON);
@@ -67,19 +60,5 @@ public class MemoryUserModule extends AbstractModule {
     @Singleton
     MemoryUsersRepository provideRepository(DomainList domainList) {
         return MemoryUsersRepository.withVirtualHosting(domainList);
-    }
-
-    @Provides
-    @Singleton
-    MemoryDomainList provideDomainList(DNSService service) {
-        return new MemoryDomainList(service);
-    }
-
-    @ProvidesIntoSet
-    InitializationOperation configureDomainList(MemoryDomainList memoryDomainList) {
-        DomainListConfiguration domainListConfiguration = DomainListConfiguration.DEFAULT; // TODO load this from conf?
-        return InitilizationOperationBuilder
-            .forClass(MemoryDomainList.class)
-            .init(() -> memoryDomainList.configure(domainListConfiguration));
     }
 }
