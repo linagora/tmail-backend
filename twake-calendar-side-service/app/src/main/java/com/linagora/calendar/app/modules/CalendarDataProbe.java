e4cf28470d57eefba0695b43a22b93b78500e2d5
Benoit TELLIER <btellier@linagora.com>
2025-04-04 15:27:08 +0200

    [STORAGE-MONGODB] Implement profile Avatar REST endpoint

--- twake-calendar-side-service/app/src/main/java/com/linagora/calendar/app/modules/CalendarDataProbe.java
+++ twake-calendar-side-service/app/src/main/java/com/linagora/calendar/app/modules/CalendarDataProbe.java
@@ -26,14 +26,19 @@ import org.apache.james.domainlist.api.DomainList;
 import org.apache.james.user.api.UsersRepository;
 import org.apache.james.utils.GuiceProbe;
 
+import com.linagora.calendar.storage.OpenPaaSId;
+import com.linagora.calendar.storage.OpenPaaSUserDAO;
+
 public class CalendarDataProbe implements GuiceProbe {
     private final UsersRepository usersRepository;
     private final DomainList domainList;
+    private final OpenPaaSUserDAO usersDAO;
 
     @Inject
-    public CalendarDataProbe(UsersRepository usersRepository, DomainList domainList) {
+    public CalendarDataProbe(UsersRepository usersRepository, DomainList domainList, OpenPaaSUserDAO usersDAO) {
         this.usersRepository = usersRepository;
         this.domainList = domainList;
+        this.usersDAO = usersDAO;
     }
 
     public CalendarDataProbe addDomain(Domain domain) {
@@ -45,10 +50,10 @@ public class CalendarDataProbe implements GuiceProbe {
         }
     }
 
-    public CalendarDataProbe addUser(Username username, String password) {
+    public OpenPaaSId addUser(Username username, String password) {
         try {
             usersRepository.addUser(username, password);
-            return this;
+            return usersDAO.add(username).block().id();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
