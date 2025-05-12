4222ee4ef59574fb15795a4173a2985184a5ee6f
Quan Tran <hqtran@linagora.com>
2025-05-12 14:56:41 +0700

    ISSUE-1718 Implement RedisOidcTokenCache

---
+++ tmail-backend/jmap/extensions-redis/src/main/scala/com/linagora/tmail/james/jmap/oidc/RedisConfigurationUtils.scala
@@ -0,0 +1,29 @@
+/********************************************************************
+  *  As a subpart of Twake Mail, this file is edited by Linagora.    *
+  *                                                                  *
+  *  https://twake-mail.com/                                         *
+  *  https://linagora.com                                            *
+  *                                                                  *
+  *  This file is subject to The Affero Gnu Public License           *
+  *  version 3.                                                      *
+  *                                                                  *
+  *  https://www.gnu.org/licenses/agpl-3.0.en.html                   *
+  *                                                                  *
+  *  This program is distributed in the hope that it will be         *
+  *  useful, but WITHOUT ANY WARRANTY; without even the implied      *
+  *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR         *
+  *  PURPOSE. See the GNU Affero General Public License for          *
+  *  more details.                                                   *
+ *******************************************************************/
+
+package com.linagora.tmail.james.jmap.oidc
+
+import io.lettuce.core.RedisURI
+import org.apache.james.backends.redis.RedisUris.RedisUris
+
+import scala.jdk.CollectionConverters._
+
+object RedisConfigurationUtils {
+  def asJavaRedisUris(redisURI: RedisUris): java.util.List[RedisURI] =
+    redisURI.value.asJava
+}
