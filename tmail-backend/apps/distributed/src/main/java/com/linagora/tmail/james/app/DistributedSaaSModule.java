99edd457b30b1007c1ea19349c2c156f3a78908a
Quan Tran <hqtran@linagora.com>
2025-07-31 15:03:54 +0700

    ISSUE-1840 Add saas_plan column to Cassandra user table when SaaS module plugged

---
+++ tmail-backend/apps/distributed/src/main/java/com/linagora/tmail/james/app/DistributedSaaSModule.java
@@ -0,0 +1,47 @@
+/********************************************************************
+ *  As a subpart of Twake Mail, this file is edited by Linagora.    *
+ *                                                                  *
+ *  https://twake-mail.com/                                         *
+ *  https://linagora.com                                            *
+ *                                                                  *
+ *  This file is subject to The Affero Gnu Public License           *
+ *  version 3.                                                      *
+ *                                                                  *
+ *  https://www.gnu.org/licenses/agpl-3.0.en.html                   *
+ *                                                                  *
+ *  This program is distributed in the hope that it will be         *
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied      *
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR         *
+ *  PURPOSE. See the GNU Affero General Public License for          *
+ *  more details.                                                   *
+ ********************************************************************/
+
+package com.linagora.tmail.james.app;
+
+import static com.linagora.tmail.modules.data.TMailCassandraDelegationStoreModule.TMAIL_CASSANDRA_USER;
+
+import java.util.function.Function;
+
+import jakarta.inject.Named;
+
+import com.datastax.oss.driver.api.querybuilder.schema.CreateTable;
+import com.datastax.oss.driver.api.querybuilder.schema.CreateTableStart;
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import com.linagora.tmail.james.jmap.saas.SaaSCapabilitiesModule;
+import com.linagora.tmail.saas.api.cassandra.CassandraSaaSDataDefinition;
+
+public class DistributedSaaSModule extends AbstractModule {
+    @Override
+    protected void configure() {
+        install(new SaaSCapabilitiesModule());
+    }
+
+    @Provides
+    @Singleton
+    @Named(TMAIL_CASSANDRA_USER)
+    public Function<CreateTableStart, CreateTable> overrideCreateUserTableFunction() {
+        return CassandraSaaSDataDefinition.createUserTableWithSaaSSupport();
+    }
+}
---
+++ tmail-backend/saas/saas-cassandra/src/main/java/com/linagora/tmail/saas/api/cassandra/CassandraSaaSDataDefinition.java
@@ -0,0 +1,36 @@
+/********************************************************************
+ *  As a subpart of Twake Mail, this file is edited by Linagora.    *
+ *                                                                  *
+ *  https://twake-mail.com/                                         *
+ *  https://linagora.com                                            *
+ *                                                                  *
+ *  This file is subject to The Affero Gnu Public License           *
+ *  version 3.                                                      *
+ *                                                                  *
+ *  https://www.gnu.org/licenses/agpl-3.0.en.html                   *
+ *                                                                  *
+ *  This program is distributed in the hope that it will be         *
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied      *
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR         *
+ *  PURPOSE. See the GNU Affero General Public License for          *
+ *  more details.                                                   *
+ *******************************************************************/
+
+package com.linagora.tmail.saas.api.cassandra;
+
+import java.util.function.Function;
+
+import com.datastax.oss.driver.api.core.CqlIdentifier;
+import com.datastax.oss.driver.api.core.type.DataTypes;
+import com.datastax.oss.driver.api.querybuilder.schema.CreateTable;
+import com.datastax.oss.driver.api.querybuilder.schema.CreateTableStart;
+import com.linagora.tmail.user.cassandra.TMailCassandraUsersRepositoryDataDefinition;
+
+public interface CassandraSaaSDataDefinition {
+    CqlIdentifier SAAS_PLAN = CqlIdentifier.fromCql("saas_plan");
+
+    static Function<CreateTableStart, CreateTable> createUserTableWithSaaSSupport() {
+        return statement -> TMailCassandraUsersRepositoryDataDefinition.defaultCreateUserTableStatement(statement)
+            .withColumn(SAAS_PLAN, DataTypes.TEXT);
+    }
+}
