5cff0321859e09b87e6290a35c40ad329f28b09c
Quan Tran <hqtran@linagora.com>
2025-08-22 10:36:17 +0700

    ISSUE-1096 Introduce POJO for SaaS subscription AMQP message and its deserializer

---
+++ tmail-backend/saas/saas-rabbitmq/src/main/java/com/linagora/tmail/saas/rabbitmq/subscription/SaaSSubscriptionMessage.java
@@ -0,0 +1,88 @@
+/********************************************************************
+ *  As a subpart of Twake Mail, this file is edited by Linagora.    *
+ *                                                                  *
+ *  https://twake-mail.com/                                         *
+ *  https://linagora.com                                            *
+ *                                                                  *
+ *  This file is subject to The Affero Gnu Public License           *
+ *  version 3.                                                      *
+ *                                                                  *
+ *  https://www.gnu.org/licenses/agpl-3.0.en.html                   *
+ *                                                                  *
+ *  This program is distributed in the hope that it will be         *
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied      *
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR         *
+ *  PURPOSE. See the GNU Affero General Public License for          *
+ *  more details.                                                   *
+ *******************************************************************/
+
+package com.linagora.tmail.saas.rabbitmq.subscription;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Preconditions;
+
+public record SaaSSubscriptionMessage(String username, Boolean isPaying, String planName, MailLimitation mail) {
+    public static class SaaSSubscriptionMessageParseException extends RuntimeException {
+        SaaSSubscriptionMessageParseException(String message, Throwable cause) {
+            super(message, cause);
+        }
+    }
+
+    private static final Deserializer DESERIALIZER = new Deserializer();
+
+    public static class Deserializer {
+        public static SaaSSubscriptionMessage parseAMQPMessage(String messagePayload) {
+            return parseAMQPMessage(messagePayload.getBytes(StandardCharsets.UTF_8));
+        }
+
+        public static SaaSSubscriptionMessage parseAMQPMessage(byte[] messagePayload) {
+            try {
+                return DESERIALIZER.deserialize(messagePayload);
+            } catch (Exception e) {
+                throw new SaaSSubscriptionMessageParseException("Failed to parse SaaS subscription message: " + new String(messagePayload, StandardCharsets.UTF_8), e);
+            }
+        }
+
+        private final ObjectMapper objectMapper;
+
+        public Deserializer() {
+            this.objectMapper = new ObjectMapper();
+            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        }
+
+        public SaaSSubscriptionMessage deserialize(byte[] jsonBytes) throws IOException {
+            return objectMapper.readValue(jsonBytes, SaaSSubscriptionMessage.class);
+        }
+    }
+
+    public record MailLimitation(Long storageQuota) {
+        @JsonCreator
+        public MailLimitation(@JsonProperty("storageQuota") Long storageQuota) {
+            Preconditions.checkNotNull(storageQuota, "storageQuota cannot be null");
+
+            this.storageQuota = storageQuota;
+        }
+    }
+
+    @JsonCreator
+    public SaaSSubscriptionMessage(@JsonProperty("username") String username,
+                                   @JsonProperty("isPaying") Boolean isPaying,
+                                   @JsonProperty("planName") String planName,
+                                   @JsonProperty("mail") MailLimitation mail) {
+        Preconditions.checkNotNull(username, "username cannot be null");
+        Preconditions.checkNotNull(isPaying, "isPaying cannot be null");
+        Preconditions.checkNotNull(planName, "planName cannot be null");
+        Preconditions.checkNotNull(mail, "mail cannot be null");
+
+        this.username = username;
+        this.isPaying = isPaying;
+        this.planName = planName;
+        this.mail = mail;
+    }
+}
