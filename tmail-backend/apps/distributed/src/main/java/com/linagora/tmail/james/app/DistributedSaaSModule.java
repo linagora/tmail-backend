21d7997cdf136d62b97ef98191dcccd82ba3025a
Quan Tran <hqtran@linagora.com>
2025-08-25 16:35:43 +0700

    ISSUE-1096 Guice binding + JMAP Integration tests for SaaSSubscriptionConsumer

--- tmail-backend/apps/distributed/src/main/java/com/linagora/tmail/james/app/DistributedSaaSModule.java
+++ tmail-backend/apps/distributed/src/main/java/com/linagora/tmail/james/app/DistributedSaaSModule.java
@@ -20,9 +20,15 @@ package com.linagora.tmail.james.app;
 
 import static com.linagora.tmail.modules.data.TMailCassandraDelegationStoreModule.TMAIL_CASSANDRA_USER;
 
+import java.io.FileNotFoundException;
 import java.util.function.Function;
 
 import jakarta.inject.Named;
+
+import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.apache.james.utils.InitializationOperation;
+import org.apache.james.utils.InitilizationOperationBuilder;
+import org.apache.james.utils.PropertiesProvider;
 
 import com.datastax.oss.driver.api.querybuilder.schema.CreateTable;
 import com.datastax.oss.driver.api.querybuilder.schema.CreateTableStart;
@@ -30,10 +36,13 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Scopes;
 import com.google.inject.Singleton;
+import com.google.inject.multibindings.ProvidesIntoSet;
 import com.linagora.tmail.james.jmap.saas.SaaSCapabilitiesModule;
 import com.linagora.tmail.saas.api.SaaSAccountRepository;
 import com.linagora.tmail.saas.api.cassandra.CassandraSaaSAccountRepository;
 import com.linagora.tmail.saas.api.cassandra.CassandraSaaSDataDefinition;
+import com.linagora.tmail.saas.rabbitmq.subscription.SaaSSubscriptionConsumer;
+import com.linagora.tmail.saas.rabbitmq.subscription.SaaSSubscriptionRabbitMQConfiguration;
 
 public class DistributedSaaSModule extends AbstractModule {
     @Override
@@ -42,6 +51,7 @@ public class DistributedSaaSModule extends AbstractModule {
 
         bind(SaaSAccountRepository.class).to(CassandraSaaSAccountRepository.class)
             .in(Scopes.SINGLETON);
+        bind(SaaSSubscriptionConsumer.class).in(Scopes.SINGLETON);
     }
 
     @Provides
@@ -49,5 +59,18 @@ public class DistributedSaaSModule extends AbstractModule {
     @Named(TMAIL_CASSANDRA_USER)
     public Function<CreateTableStart, CreateTable> overrideCreateUserTableFunction() {
         return CassandraSaaSDataDefinition.userTableWithSaaSSupport();
+    }
+
+    @ProvidesIntoSet
+    public InitializationOperation initializeSaaSSubscriptionConsumer(SaaSSubscriptionConsumer instance) {
+        return InitilizationOperationBuilder
+            .forClass(SaaSSubscriptionConsumer.class)
+            .init(instance::init);
+    }
+
+    @Provides
+    @Singleton
+    SaaSSubscriptionRabbitMQConfiguration provideSaasSubscriptionRabbitMQConfiguration(PropertiesProvider propertiesProvider) throws ConfigurationException, FileNotFoundException {
+        return SaaSSubscriptionRabbitMQConfiguration.from(propertiesProvider.getConfiguration("rabbitmq"));
     }
 }
