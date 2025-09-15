05f0f96bdffcdcf4cc55e5a8041b5e4b5beb7779
Quang Trung NGUYEN <36415303+Insalien@users.noreply.github.com>
2025-09-15 17:49:13 +0200

    ISSUE 1911 Added start-with filter tests in LinagoraFilterGetMethodContract.scala
    Co-authored-by: Trung NGUYEN <qtnguyen@linagora.com>

--- tmail-backend/integration-tests/jmap/jmap-integration-tests-common/src/main/scala/com/linagora/tmail/james/common/LinagoraFilterGetMethodContract.scala
+++ tmail-backend/integration-tests/jmap/jmap-integration-tests-common/src/main/scala/com/linagora/tmail/james/common/LinagoraFilterGetMethodContract.scala
@@ -750,4 +750,168 @@ trait LinagoraFilterGetMethodContract {
          |}""".stripMargin)
   }
 
+  @Test
+  def filterGetShouldReturnStartwithRule(server: GuiceJamesServer): Unit = {
+    server.getProbe(classOf[JmapGuiceCustomProbe])
+      .setRulesForUser(generateUsername(),
+        Rule.builder
+          .id(Rule.Id.of("1"))
+          .name("My first rule")
+          .conditionGroup(Rule.Condition.of(Rule.Condition.Field.SUBJECT, Rule.Condition.Comparator.START_WITH, "question"))
+          .action(Rule.Action.of(Rule.Action.AppendInMailboxes.withMailboxIds(generateMailboxIdForUser())))
+          .build)
+
+    val request = s"""{
+                     |  "using": ["com:linagora:params:jmap:filter" ],
+                     |  "methodCalls": [
+                     |    [
+                     |      "Filter/get",
+                     |        {
+                     |          "accountId": "$generateAccountIdAsString",
+                     |          "ids": ["singleton"]
+                     |        },
+                     |          "c1"]
+                     |    ]
+                     |}""".stripMargin
+
+    val response = `given`()
+      .header(ACCEPT.toString, ACCEPT_RFC8621_VERSION_HEADER)
+      .body(request)
+    .when()
+      .post()
+    .`then`
+      .log().ifValidationFails()
+      .statusCode(HttpStatus.SC_OK)
+      .contentType(JSON)
+      .extract()
+      .body()
+      .asString()
+
+    assertThatJson(response).isEqualTo(
+      s"""{
+         |  "sessionState": "${SESSION_STATE.value}",
+         |  "methodResponses": [[
+         |    "Filter/get", {
+         |      "accountId": "$generateAccountIdAsString",
+         |      "state": "0",
+         |      "list": [
+         |      {
+         |        "id": "singleton",
+         |        "rules": [
+         |          {
+         |            "name": "My first rule",
+         |            "condition": {
+         |              "field": "subject",
+         |              "comparator": "start-with",
+         |              "value": "question"
+         |            },
+         |            "conditionGroup": {
+         |              "conditionCombiner": "AND",
+         |              "conditions": [
+         |                {
+         |                  "comparator": "start-with",
+         |                  "field": "subject",
+         |                  "value": "question"
+         |                }
+         |              ]
+         |            },
+         |            "action": {
+         |              "appendIn": {
+         |                "mailboxIds":["$generateMailboxIdForUser"]
+         |              },"markAsSeen":false,"markAsImportant":false,"reject":false,"withKeywords":[]
+         |            }
+         |          }
+         |        ]
+         |      }
+         |    ],
+         |    "notFound": []
+         |  }, "c1"]]
+         |}""".stripMargin)
+  }
+
+  @Test
+  def filterGetShouldReturnCombinationOfStartWithAndOthersRules(server: GuiceJamesServer): Unit = {
+    server.getProbe(classOf[JmapGuiceCustomProbe])
+      .setRulesForUser(generateUsername(),
+        Rule.builder
+          .id(Rule.Id.of("1"))
+          .name("My first rule")
+          .conditionGroup(Rule.ConditionGroup.of(Rule.ConditionCombiner.OR, Rule.Condition.of(Rule.Condition.Field.SUBJECT, Rule.Condition.Comparator.CONTAINS, "question"),
+            Rule.Condition.of(Rule.Condition.Field.FROM, Rule.Condition.Comparator.START_WITH, "user")))
+          .action(Rule.Action.of(Rule.Action.AppendInMailboxes.withMailboxIds(generateMailboxIdForUser())))
+          .build)
+
+    val request =
+      s"""{
+         |  "using": ["com:linagora:params:jmap:filter" ],
+         |  "methodCalls": [
+         |    [
+         |      "Filter/get",
+         |        {
+         |          "accountId": "$generateAccountIdAsString",
+         |          "ids": ["singleton"]
+         |        },
+         |          "c1"]
+         |    ]
+         |}""".stripMargin
+
+    val response = `given`()
+      .header(ACCEPT.toString, ACCEPT_RFC8621_VERSION_HEADER)
+      .body(request)
+    .when()
+      .post()
+    .`then`
+      .log().ifValidationFails()
+      .statusCode(HttpStatus.SC_OK)
+      .contentType(JSON)
+      .extract()
+      .body()
+      .asString()
+
+    assertThatJson(response).isEqualTo(
+      s"""{
+         |  "sessionState": "${SESSION_STATE.value}",
+         |  "methodResponses": [[
+         |    "Filter/get", {
+         |      "accountId": "$generateAccountIdAsString",
+         |      "state": "0",
+         |      "list": [
+         |      {
+         |        "id": "singleton",
+         |        "rules": [
+         |          {
+         |            "name": "My first rule",
+         |            "conditionGroup": {
+         |              "conditionCombiner": "OR",
+         |              "conditions": [
+         |                {
+         |                  "comparator": "contains",
+         |                  "field": "subject",
+         |                  "value": "question"
+         |                },
+         |                {
+         |                  "comparator": "start-with",
+         |                  "field": "from",
+         |                  "value": "user"
+         |                }
+         |              ]
+         |            },
+         |            "condition": {
+         |              "field": "subject",
+         |              "comparator": "contains",
+         |              "value": "question"
+         |            },
+         |            "action": {
+         |              "appendIn": {
+         |                "mailboxIds":["$generateMailboxIdForUser"]
+         |              },"markAsSeen":false,"markAsImportant":false,"reject":false,"withKeywords":[]
+         |            }
+         |          }
+         |        ]
+         |      }
+         |    ],
+         |    "notFound": []
+         |  }, "c1"]]
+         |}""".stripMargin)
+  }
 }
