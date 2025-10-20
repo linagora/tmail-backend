83428cbeb2ca4c5b6b30f6058ba7f0b6c486cce8
Quan Tran <hqtran@linagora.com>
2025-10-20 15:12:42 +0700

    ISSUE-1950 Filter capability should return version 2

--- tmail-backend/integration-tests/jmap/jmap-integration-tests-common/src/main/scala/com/linagora/tmail/james/common/LinagoraFilterGetMethodContract.scala
+++ tmail-backend/integration-tests/jmap/jmap-integration-tests-common/src/main/scala/com/linagora/tmail/james/common/LinagoraFilterGetMethodContract.scala
@@ -28,6 +28,7 @@ import io.restassured.http.ContentType.JSON
 import net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson
 import net.javacrumbs.jsonunit.core.Option.IGNORING_ARRAY_ORDER
 import org.apache.http.HttpStatus
+import org.apache.http.HttpStatus.SC_OK
 import org.apache.james.GuiceJamesServer
 import org.apache.james.core.{MailAddress, Username}
 import org.apache.james.jmap.api.filtering.Rule
@@ -36,6 +37,7 @@ import org.apache.james.jmap.http.UserCredential
 import org.apache.james.jmap.rfc8621.contract.Fixture.{ACCEPT_RFC8621_VERSION_HEADER, BOB, BOB_PASSWORD, DOMAIN, authScheme, baseRequestSpecBuilder}
 import org.apache.james.jmap.rfc8621.contract.tags.CategoryTags
 import org.apache.james.utils.DataProbeImpl
+import org.hamcrest.Matchers.{equalTo, hasKey}
 import org.junit.jupiter.api.{BeforeEach, Tag, Test}
 trait LinagoraFilterGetMethodContract {
 
@@ -53,6 +55,19 @@ trait LinagoraFilterGetMethodContract {
     requestSpecification = baseRequestSpecBuilder(server)
       .setAuth(authScheme(UserCredential(BOB, BOB_PASSWORD)))
       .build()
+  }
+
+  @Test
+  def filterCapabilityShouldReturnVersion(): Unit = {
+    `given`()
+    .when()
+      .header(ACCEPT.toString, ACCEPT_RFC8621_VERSION_HEADER)
+      .get("/session")
+    .`then`
+      .statusCode(SC_OK)
+      .contentType(JSON)
+      .body("capabilities.'com:linagora:params:jmap:filter'", hasKey("version"))
+      .body("capabilities.'com:linagora:params:jmap:filter'.version", equalTo(2))
   }
 
   @Test
