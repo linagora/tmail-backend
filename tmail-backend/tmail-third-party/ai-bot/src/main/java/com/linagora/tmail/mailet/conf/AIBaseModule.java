32f22089a071d6956ffe36e38190829d3dfdd6b8
Quan Tran <hqtran@linagora.com>
2026-01-21 15:55:09 +0700

    ISSUE-2101 Guice binding for split Llm listener
    - Provide configuration bean for LlmMailPrioritizationBackendClassifierListener
    - Bind AIAnalysisNeededEventSerializer
    - Bind LlmMailPrioritizationBackendClassifierListener onto the tmail event bus
    - Bind EventSerializersAggregator for the tmail event bus (so it can serialize the AIAnalysisNeeded event)

--- tmail-backend/tmail-third-party/ai-bot/src/main/java/com/linagora/tmail/mailet/conf/AIBaseModule.java
+++ tmail-backend/tmail-third-party/ai-bot/src/main/java/com/linagora/tmail/mailet/conf/AIBaseModule.java
@@ -18,22 +18,36 @@
 
 package com.linagora.tmail.mailet.conf;
 
+import static com.linagora.tmail.event.TmailEventModule.TMAIL_EVENT_BUS_INJECT_NAME;
+import static com.linagora.tmail.listener.rag.LlmMailPrioritizationBackendClassifierListener.LLM_MAIL_CLASSIFIER_CONFIGURATION;
 import static org.apache.james.events.EventDeadLettersHealthCheck.DEAD_LETTERS_IGNORED_GROUPS;
 
 import java.io.FileNotFoundException;
+import java.util.Optional;
 
+import org.apache.commons.configuration2.BaseHierarchicalConfiguration;
 import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.HierarchicalConfiguration;
 import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.apache.commons.configuration2.tree.ImmutableNode;
+import org.apache.james.events.EventListener;
+import org.apache.james.events.EventSerializer;
 import org.apache.james.events.Group;
+import org.apache.james.modules.mailbox.ListenerConfiguration;
+import org.apache.james.modules.mailbox.ListenersConfiguration;
+import org.apache.james.server.core.configuration.ConfigurationProvider;
 import org.apache.james.utils.PropertiesProvider;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
 import com.google.inject.multibindings.Multibinder;
+import com.google.inject.multibindings.ProvidesIntoSet;
 import com.google.inject.name.Named;
 import com.google.inject.name.Names;
+import com.linagora.tmail.listener.rag.LlmMailPrioritizationBackendClassifierListener;
 import com.linagora.tmail.listener.rag.LlmMailPrioritizationClassifierListener;
+import com.linagora.tmail.listener.rag.event.AIAnalysisNeededEventSerializer;
 import com.linagora.tmail.mailet.AIBotConfig;
 import com.linagora.tmail.mailet.AIRedactionalHelper;
 import com.linagora.tmail.mailet.LangchainAIRedactionalHelper;
@@ -53,6 +67,11 @@ public class AIBaseModule extends AbstractModule {
         Multibinder<Group> deadLetterIgnoredGroups = Multibinder.newSetBinder(binder(), Group.class, Names.named(DEAD_LETTERS_IGNORED_GROUPS));
         deadLetterIgnoredGroups.addBinding().toInstance(RagListener.GROUP);
         deadLetterIgnoredGroups.addBinding().toInstance(LlmMailPrioritizationClassifierListener.GROUP);
+        deadLetterIgnoredGroups.addBinding().toInstance(LlmMailPrioritizationBackendClassifierListener.GROUP);
+
+        Multibinder.newSetBinder(binder(), EventListener.ReactiveGroupEventListener.class, Names.named(TMAIL_EVENT_BUS_INJECT_NAME))
+            .addBinding()
+            .to(LlmMailPrioritizationBackendClassifierListener.class);
     }
 
     @Provides
@@ -60,6 +79,27 @@ public class AIBaseModule extends AbstractModule {
     @Named("ai")
     public Configuration getAIBotConfigurations(PropertiesProvider propertiesProvider) throws ConfigurationException, FileNotFoundException {
         return propertiesProvider.getConfiguration("ai");
+    }
+
+    @Provides
+    @Singleton
+    @Named(LLM_MAIL_CLASSIFIER_CONFIGURATION)
+    public HierarchicalConfiguration<ImmutableNode> provideLlmMailPrioritizationListenerConfiguration(ConfigurationProvider configurationProvider) throws ConfigurationException {
+        HierarchicalConfiguration<ImmutableNode> configuration = configurationProvider.getConfiguration("listeners");
+
+        return ListenersConfiguration.from(configuration)
+            .getListenersConfiguration()
+            .stream()
+            .filter(listener -> listener.getClazz().equals(LlmMailPrioritizationClassifierListener.class.getCanonicalName()))
+            .map(ListenerConfiguration::getConfiguration)
+            .flatMap(Optional::stream)
+            .findFirst()
+            .orElseGet(BaseHierarchicalConfiguration::new);
+    }
+
+    @ProvidesIntoSet
+    public EventSerializer provideAIAnalysisNeededEventSerializer(AIAnalysisNeededEventSerializer aiAnalysisNeededEventSerializer) {
+        return aiAnalysisNeededEventSerializer;
     }
 
     @Provides
