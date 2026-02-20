3db3da6efc42ca4cd5d50bab7d00cafee7fd33a8
Benoit TELLIER <btellier@linagora.com>
2026-02-20 14:25:07 +0100

    ISSUE-2175 Be able to list team mailbox folders

--- tmail-backend/webadmin/webadmin-team-mailboxes/src/main/java/com/linagora/tmail/webadmin/TeamMailboxManagementRoutes.java
+++ tmail-backend/webadmin/webadmin-team-mailboxes/src/main/java/com/linagora/tmail/webadmin/TeamMailboxManagementRoutes.java
@@ -25,6 +25,11 @@ import jakarta.inject.Inject;
 import org.apache.james.core.Domain;
 import org.apache.james.core.Username;
 import org.apache.james.domainlist.api.DomainList;
+import org.apache.james.mailbox.MailboxManager;
+import org.apache.james.mailbox.MailboxSession;
+import org.apache.james.mailbox.model.MailboxConstants;
+import org.apache.james.mailbox.model.search.MailboxQuery;
+import org.apache.james.mailbox.model.search.PrefixedWildcard;
 import org.apache.james.webadmin.Constants;
 import org.apache.james.webadmin.Routes;
 import org.apache.james.webadmin.utils.ErrorResponder;
@@ -37,6 +42,7 @@ import com.linagora.tmail.team.TeamMailbox;
 import com.linagora.tmail.team.TeamMailboxMember;
 import com.linagora.tmail.team.TeamMailboxName;
 import com.linagora.tmail.team.TeamMailboxNameConflictException;
+import com.linagora.tmail.team.TeamMailboxNameSpace;
 import com.linagora.tmail.team.TeamMailboxNotFoundException;
 import com.linagora.tmail.team.TeamMailboxRepository;
 import com.linagora.tmail.team.TeamMemberRole;
@@ -50,6 +56,8 @@ import spark.Route;
 import spark.Service;
 
 public class TeamMailboxManagementRoutes implements Routes {
+    public record TeamMailboxFolderResponse(String mailboxName, String mailboxId) {}
+
     public record TeamMailboxResponse(String name, String emailAddress) {
             static TeamMailboxResponse from(TeamMailbox teamMailbox) {
                 Preconditions.checkNotNull(teamMailbox);
@@ -88,17 +96,21 @@ public class TeamMailboxManagementRoutes implements Routes {
     private static final String ROLE_PARAM = "role";
     public static final String BASE_PATH = Constants.SEPARATOR + "domains" + Constants.SEPARATOR + TEAM_MAILBOX_DOMAIN_PARAM + Constants.SEPARATOR + "team-mailboxes";
     public static final String MEMBER_BASE_PATH = BASE_PATH + Constants.SEPARATOR + TEAM_MAILBOX_NAME_PARAM + Constants.SEPARATOR + "members";
+    public static final String MAILBOX_BASE_PATH = BASE_PATH + Constants.SEPARATOR + TEAM_MAILBOX_NAME_PARAM + Constants.SEPARATOR + "mailboxes";
 
     private final TeamMailboxRepository teamMailboxRepository;
     private final DomainList domainList;
+    private final MailboxManager mailboxManager;
     private final JsonTransformer jsonTransformer;
 
     @Inject
     public TeamMailboxManagementRoutes(TeamMailboxRepository teamMailboxRepository,
                                        DomainList domainList,
+                                       MailboxManager mailboxManager,
                                        JsonTransformer jsonTransformer) {
         this.teamMailboxRepository = teamMailboxRepository;
         this.domainList = domainList;
+        this.mailboxManager = mailboxManager;
         this.jsonTransformer = jsonTransformer;
     }
 
@@ -112,6 +124,8 @@ public class TeamMailboxManagementRoutes implements Routes {
         service.get(BASE_PATH, getTeamMailboxesByDomain(), jsonTransformer);
         service.delete(BASE_PATH + Constants.SEPARATOR + TEAM_MAILBOX_NAME_PARAM, deleteTeamMailbox(), jsonTransformer);
         service.put(BASE_PATH + Constants.SEPARATOR + TEAM_MAILBOX_NAME_PARAM, addTeamMailbox(), jsonTransformer);
+
+        service.get(MAILBOX_BASE_PATH, getMailboxFolders(), jsonTransformer);
 
         service.get(MEMBER_BASE_PATH, getMembers(), jsonTransformer);
         service.delete(MEMBER_BASE_PATH + Constants.SEPARATOR + MEMBER_USERNAME_PARAM, deleteMember(), jsonTransformer);
@@ -198,6 +212,36 @@ public class TeamMailboxManagementRoutes implements Routes {
                         .message(e.getMessage())
                         .haltError();
                 })
+                .block();
+        };
+    }
+
+    public Route getMailboxFolders() {
+        return (request, response) -> {
+            TeamMailbox teamMailbox = new TeamMailbox(extractDomain(request), extractName(request));
+
+            boolean exists = Mono.from(teamMailboxRepository.exists(teamMailbox)).map(Boolean.TRUE::equals).block();
+            if (!exists) {
+                throw teamMailboxNotFoundException(teamMailbox, new TeamMailboxNotFoundException(teamMailbox));
+            }
+
+            MailboxSession session = mailboxManager.createSystemSession(teamMailbox.admin());
+            String teamMailboxNameStr = teamMailbox.mailboxName().asString();
+            String folderPrefix = teamMailboxNameStr + MailboxConstants.FOLDER_DELIMITER;
+
+            MailboxQuery query = MailboxQuery.builder()
+                .namespace(TeamMailboxNameSpace.TEAM_MAILBOX_NAMESPACE())
+                .user(teamMailbox.owner())
+                .expression(new PrefixedWildcard(teamMailboxNameStr))
+                .build();
+
+            return mailboxManager.search(query, MailboxManager.MailboxSearchFetchType.Minimal, session)
+                .map(metaData -> {
+                    String fullName = metaData.getPath().getName();
+                    String mailboxName = fullName.startsWith(folderPrefix) ? fullName.substring(folderPrefix.length()) : fullName;
+                    return new TeamMailboxFolderResponse(mailboxName, metaData.getId().serialize());
+                })
+                .collectList()
                 .block();
         };
     }
