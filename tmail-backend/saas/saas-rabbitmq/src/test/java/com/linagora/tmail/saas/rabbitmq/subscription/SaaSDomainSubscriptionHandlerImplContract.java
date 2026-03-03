c127270a624a2ab71d5ed52f9c0992ba3b65ac1d
Rene Cordier <rcordier@linagora.com>
2026-03-03 16:37:02 +0700

    ISSUE-2214 Refactor saas subscription tests into contracts

--- tmail-backend/saas/saas-rabbitmq/src/test/java/com/linagora/tmail/saas/rabbitmq/subscription/SaaSDomainSubscriptionHandlerImplContract.java
+++ tmail-backend/saas/saas-rabbitmq/src/test/java/com/linagora/tmail/saas/rabbitmq/subscription/SaaSDomainSubscriptionHandlerImplContract.java
@@ -1,3 +1,21 @@
+/********************************************************************
+ *  As a subpart of Twake Mail, this file is edited by Linagora.    *
+ *                                                                  *
+ *  https://twake-mail.com/                                         *
+ *  https://linagora.com                                            *
+ *                                                                  *
+ *  This file is subject to The Affero Gnu Public License           *
+ *  version 3.                                                      *
+ *                                                                  *
+ *  https://www.gnu.org/licenses/agpl-3.0.en.html                   *
+ *                                                                  *
+ *  This program is distributed in the hope that it will be         *
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied      *
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR         *
+ *  PURPOSE. See the GNU Affero General Public License for          *
+ *  more details.                                                   *
+ *******************************************************************/
+
 package com.linagora.tmail.saas.rabbitmq.subscription;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,40 +25,26 @@ import java.util.Optional;
 import org.apache.james.core.Domain;
 import org.apache.james.core.quota.QuotaSizeLimit;
 import org.apache.james.domainlist.api.DomainList;
-import org.apache.james.domainlist.api.mock.SimpleDomainList;
-import org.apache.james.mailbox.inmemory.quota.InMemoryPerUserMaxQuotaManager;
 import org.apache.james.mailbox.quota.MaxQuotaManager;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.linagora.tmail.rate.limiter.api.RateLimitingRepository;
-import com.linagora.tmail.rate.limiter.api.memory.MemoryRateLimitingRepository;
 import com.linagora.tmail.rate.limiter.api.model.RateLimitingDefinition;
 
 import reactor.core.publisher.Mono;
 
-public class SaaSDomainSubscriptionHandlerImplTest {
+public interface SaaSDomainSubscriptionHandlerImplContract {
+    boolean DOMAIN_DISABLED = false;
+    boolean MAIL_DNS_CONFIGURATION_VALIDATED = true;
 
-    public static final boolean DOMAIN_DISABLED = false;
-    public static final boolean MAIL_DNS_CONFIGURATION_VALIDATED = true;
+    DomainList domainList();
+    MaxQuotaManager maxQuotaManager();
+    RateLimitingRepository rateLimitingRepository();
 
-    private DomainList domainList;
-    private MaxQuotaManager maxQuotaManager;
-    private RateLimitingRepository rateLimitingRepository;
-
-    private SaaSDomainSubscriptionHandlerImpl handler;
-
-    @BeforeEach
-    void setUp() {
-        domainList = new SimpleDomainList();
-        maxQuotaManager = new InMemoryPerUserMaxQuotaManager();
-        rateLimitingRepository = new MemoryRateLimitingRepository();
-
-        handler = new SaaSDomainSubscriptionHandlerImpl(domainList, maxQuotaManager, rateLimitingRepository);
-    }
+    SaaSDomainSubscriptionHandlerImpl handler();
 
     @Test
-    void shouldAddDomainAndApplySettingsWhenDnsValidatedAndFeaturesPresent() throws Exception {
+    default void shouldAddDomainAndApplySettingsWhenDnsValidatedAndFeaturesPresent() throws Exception {
         String domainName = "example.com";
         Domain domain = Domain.of(domainName);
 
@@ -57,13 +61,13 @@ public class SaaSDomainSubscriptionHandlerImplTest {
         SaaSDomainSubscriptionMessage.SaaSDomainValidSubscriptionMessage message =
             new SaaSDomainSubscriptionMessage.SaaSDomainValidSubscriptionMessage(domainName, Optional.empty(), Optional.of(MAIL_DNS_CONFIGURATION_VALIDATED), Optional.of(features));
 
-        handler.handleMessage(message).block();
+        handler().handleMessage(message).block();
 
         // Domain added
-        assertThat(domainList.containsDomain(domain)).isTrue();
+        assertThat(domainList().containsDomain(domain)).isTrue();
 
         // Storage quota applied
-        assertThat(maxQuotaManager.getDomainMaxStorage(domain)).isEqualTo(Optional.of(QuotaSizeLimit.size(123L)));
+        assertThat(maxQuotaManager().getDomainMaxStorage(domain)).isEqualTo(Optional.of(QuotaSizeLimit.size(123L)));
 
         // Rate limiting applied
         RateLimitingDefinition rateLimitingDefinition = RateLimitingDefinition.builder()
@@ -76,29 +80,29 @@ public class SaaSDomainSubscriptionHandlerImplTest {
             .build();
 
         // fetch from repository via blocking (same approach as consumer tests)
-        RateLimitingDefinition actual = Mono.from(rateLimitingRepository.getRateLimiting(domain)).block();
+        RateLimitingDefinition actual = Mono.from(rateLimitingRepository().getRateLimiting(domain)).block();
         assertThat(actual).isEqualTo(rateLimitingDefinition);
     }
 
     @Test
-    void shouldRemoveDomainWhenDomainIsDisabled() throws Exception {
+    default void shouldRemoveDomainWhenDomainIsDisabled() throws Exception {
         String domainName = "remove.com";
         Domain domain = Domain.of(domainName);
 
         // register domain
-        domainList.addDomain(domain);
-        assertThat(domainList.containsDomain(domain)).isTrue();
+        domainList().addDomain(domain);
+        assertThat(domainList().containsDomain(domain)).isTrue();
 
         SaaSDomainSubscriptionMessage.SaaSDomainCancelSubscriptionMessage message =
             new SaaSDomainSubscriptionMessage.SaaSDomainCancelSubscriptionMessage(domainName, Optional.empty(),DOMAIN_DISABLED);
 
-        handler.handleMessage(message).block();
+        handler().handleMessage(message).block();
 
-        assertThat(domainList.containsDomain(domain)).isFalse();
+        assertThat(domainList().containsDomain(domain)).isFalse();
     }
 
     @Test
-    void addingDomainShouldBeIdempotent() throws Exception {
+    default void addingDomainShouldBeIdempotent() throws Exception {
         String domainName = "dup.com";
         Domain domain = Domain.of(domainName);
 
@@ -106,15 +110,15 @@ public class SaaSDomainSubscriptionHandlerImplTest {
             new SaaSDomainSubscriptionMessage.SaaSDomainValidSubscriptionMessage(domainName, Optional.empty(), Optional.of(MAIL_DNS_CONFIGURATION_VALIDATED), Optional.empty());
 
         // First add
-        handler.handleMessage(message).block();
+        handler().handleMessage(message).block();
         // Duplicate add should not throw and should remain present
-        handler.handleMessage(message).block();
+        handler().handleMessage(message).block();
 
-        assertThat(domainList.containsDomain(domain)).isTrue();
+        assertThat(domainList().containsDomain(domain)).isTrue();
     }
 
     @Test
-    void shouldNotThrowWhenRemovingNonexistentDomain() throws Exception {
+    default void shouldNotThrowWhenRemovingNonexistentDomain() throws Exception {
         String domainName = "nonexistent.com";
         Domain domain = Domain.of(domainName);
 
@@ -122,8 +126,8 @@ public class SaaSDomainSubscriptionHandlerImplTest {
             new SaaSDomainSubscriptionMessage.SaaSDomainCancelSubscriptionMessage(domainName, Optional.empty(), DOMAIN_DISABLED);
 
         // Should not throw
-        handler.handleMessage(message).block();
+        handler().handleMessage(message).block();
 
-        assertThat(domainList.containsDomain(domain)).isFalse();
+        assertThat(domainList().containsDomain(domain)).isFalse();
     }
 }
