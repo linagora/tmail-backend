29f7a2cf3c34a515c75435ddeed0fbb54b191683
Benoit TELLIER <btellier@linagora.com>
2026-03-13 23:08:55 +0100

    ISSUE-2255 Define TMail label events

---
+++ tmail-backend/jmap/extensions-api/src/main/scala/com/linagora/tmail/james/jmap/label/LabelEvents.scala
@@ -0,0 +1,44 @@
+/********************************************************************
+ *  As a subpart of Twake Mail, this file is edited by Linagora.    *
+ *                                                                  *
+ *  https://twake-mail.com/                                         *
+ *  https://linagora.com                                            *
+ *                                                                  *
+ *  This file is subject to The Affero Gnu Public License           *
+ *  version 3.                                                      *
+ *                                                                  *
+ *  https://www.gnu.org/licenses/agpl-3.0.en.html                   *
+ *                                                                  *
+ *  This program is distributed in the hope that it will be         *
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied      *
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR         *
+ *  PURPOSE. See the GNU Affero General Public License for          *
+ *  more details.                                                   *
+ ********************************************************************/
+
+package com.linagora.tmail.james.jmap.label
+
+import com.linagora.tmail.james.jmap.contact.TmailEvent
+import com.linagora.tmail.james.jmap.model.{Label, LabelId}
+import org.apache.james.core.Username
+import org.apache.james.events.Event.EventId
+
+trait TmailLabelEvent extends TmailEvent
+
+case class LabelCreated(eventId: EventId, username: Username, label: Label) extends TmailLabelEvent {
+  override def getUsername: Username = username
+  override def isNoop: Boolean = false
+  override def getEventId: EventId = eventId
+}
+
+case class LabelUpdated(eventId: EventId, username: Username, updatedLabel: Label) extends TmailLabelEvent {
+  override def getUsername: Username = username
+  override def isNoop: Boolean = false
+  override def getEventId: EventId = eventId
+}
+
+case class LabelDestroyed(eventId: EventId, username: Username, labelId: LabelId) extends TmailLabelEvent {
+  override def getUsername: Username = username
+  override def isNoop: Boolean = false
+  override def getEventId: EventId = eventId
+}
