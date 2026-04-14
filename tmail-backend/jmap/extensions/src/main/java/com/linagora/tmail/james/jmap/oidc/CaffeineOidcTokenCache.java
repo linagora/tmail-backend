929c92b1243ea3d0a6cda2ad65bb8337e0d5c001
Benoit TELLIER <btellier@linagora.com>
2026-04-14 21:34:52 +0200

    [FIX] Run Caffeine synchronous SLO on bounded elastic

--- tmail-backend/jmap/extensions/src/main/java/com/linagora/tmail/james/jmap/oidc/CaffeineOidcTokenCache.java
+++ tmail-backend/jmap/extensions/src/main/java/com/linagora/tmail/james/jmap/oidc/CaffeineOidcTokenCache.java
@@ -73,7 +73,7 @@ public class CaffeineOidcTokenCache implements OidcTokenCache {
     public Mono<Void> invalidate(Sid sid) {
         return Flux.fromIterable(sidToTokens.get(sid))
             .collectList()
-            .flatMap(tokenList -> Mono.fromRunnable(() -> cacheToken.synchronous().invalidateAll(tokenList)))
+            .flatMap(tokenList -> Mono.fromRunnable(() -> cacheToken.synchronous().invalidateAll(tokenList)).subscribeOn(Schedulers.boundedElastic()))
             .then(Mono.fromRunnable(() -> sidToTokens.removeAll(sid)))
             .then();
     }
