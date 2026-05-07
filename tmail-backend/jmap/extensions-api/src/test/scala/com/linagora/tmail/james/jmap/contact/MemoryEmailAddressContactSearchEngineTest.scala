d351036ca230f50eae86b4296944b246d077805e
Quan Tran <hqtran@linagora.com>
2026-05-07 16:02:35 +0700

    ISSUE-2322 Add a test reproduces shouldRemoveAllEmailsFromSearchEngineWhenCollectedContactWithMultipleEmailsIsDeleted's ConcurrentModificationException

--- tmail-backend/jmap/extensions-api/src/test/scala/com/linagora/tmail/james/jmap/contact/MemoryEmailAddressContactSearchEngineTest.scala
+++ tmail-backend/jmap/extensions-api/src/test/scala/com/linagora/tmail/james/jmap/contact/MemoryEmailAddressContactSearchEngineTest.scala
@@ -18,9 +18,16 @@
 
 package com.linagora.tmail.james.jmap.contact
 
+import org.apache.james.core.{MailAddress, Username}
+import org.apache.james.jmap.api.model.AccountId
+import org.assertj.core.api.Assertions.assertThatCode
 import org.junit.jupiter.api.{BeforeEach, Disabled, Test}
+import reactor.core.scala.publisher.{SFlux, SMono}
+import reactor.core.scheduler.Schedulers
 
 class MemoryEmailAddressContactSearchEngineTest extends EmailAddressContactSearchEngineContract {
+  private val accountId: AccountId = AccountId.fromUsername(Username.of("bob@linagora.com"))
+
   var inMemoryEmailAddressContactSearchEngine: InMemoryEmailAddressContactSearchEngine = _
 
   override def testee(): EmailAddressContactSearchEngine = inMemoryEmailAddressContactSearchEngine
@@ -37,4 +44,29 @@ class MemoryEmailAddressContactSearchEngineTest extends EmailAddressContactSearc
   @Disabled("Memory does not need to support language special characters normalization")
   override def searchASCIICharactersShouldReturnMatchedFrenchName(): Unit = {
   }
+
+  @Test
+  def autoCompleteShouldNotThrowConcurrentModificationExceptionWhenAutoCompleteAndIndexingConcurrently(): Unit = {
+    val initialContactCount = 10
+
+    SFlux.range(0, initialContactCount)
+      .flatMap(index => SMono(testee().index(accountId, contact(index))))
+      .blockLast()
+
+    val autoCompleteContactsConcurrently: SFlux[Seq[EmailAddressContact]] = SFlux.range(0, initialContactCount)
+      .flatMap(_ => SFlux(testee().autoComplete(accountId, "contact", 10_000))
+        .collectSeq()
+        .subscribeOn(Schedulers.parallel()))
+
+    val indexContactsConcurrently: SFlux[EmailAddressContact] = SFlux.range(initialContactCount, 10)
+      .flatMap(index => SMono(testee().index(accountId, contact(index)))
+        .subscribeOn(Schedulers.parallel()))
+
+    assertThatCode(() => SFlux.merge(Seq(autoCompleteContactsConcurrently, indexContactsConcurrently))
+      .blockLast())
+      .doesNotThrowAnyException()
+  }
+
+  private def contact(index: Int): ContactFields =
+    ContactFields(new MailAddress(s"contact-$index@linagora.com"), firstname = s"Contact $index", surname = "User")
 }
