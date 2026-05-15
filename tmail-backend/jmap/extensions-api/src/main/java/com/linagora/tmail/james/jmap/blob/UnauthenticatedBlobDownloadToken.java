2e87343d140267f703b732774bf3c914f2960086
Quan Tran <hqtran@linagora.com>
2026-05-15 16:08:34 +0700

    ISSUE-2346 UnauthenticatedBlobDownloadTokenRepository + memory implementation + contract tests

---
+++ tmail-backend/jmap/extensions-api/src/main/java/com/linagora/tmail/james/jmap/blob/UnauthenticatedBlobDownloadToken.java
@@ -0,0 +1,29 @@
+/********************************************************************
+ *  As a subpart of Twake Mail, this file is edited by Linagora.    *
+ *                                                                  *
+ *  https://twake-mail.com/                                         *
+ *  https://linagora.com                                            *
+ *                                                                  *
+ *  This file is subject to The Affero Gnu Public License           *
+ *  version 3.                                                      *
+ *                                                                  *
+ *  https://www.gnu.org/licenses/agpl-3.0.en.html                   *
+ *                                                                  *
+ *  This program is distributed in the hope that it will be         *
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied      *
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR         *
+ *  PURPOSE. See the GNU Affero General Public License for          *
+ *  more details.                                                   *
+ ********************************************************************/
+
+package com.linagora.tmail.james.jmap.blob;
+
+import java.util.UUID;
+
+import com.google.common.base.Preconditions;
+
+public record UnauthenticatedBlobDownloadToken(UUID value) {
+    public UnauthenticatedBlobDownloadToken {
+        Preconditions.checkNotNull(value);
+    }
+}
