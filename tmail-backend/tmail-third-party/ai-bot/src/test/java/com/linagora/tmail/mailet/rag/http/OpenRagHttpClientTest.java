e6b6fc388a6203896e52a547d78f55f5adf3426e
amghirbi <amghirbi@linagora.com>
2026-07-22 16:03:36 +0200

    [TMail] Basing OpenRagHttpClient on AiHttpClient

--- tmail-backend/tmail-third-party/ai-bot/src/test/java/com/linagora/tmail/mailet/rag/http/OpenRagHttpClientTest.java
+++ tmail-backend/tmail-third-party/ai-bot/src/test/java/com/linagora/tmail/mailet/rag/http/OpenRagHttpClientTest.java
@@ -18,27 +18,19 @@
 
 package com.linagora.tmail.mailet.rag.http;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.containing;
 import static com.github.tomakehurst.wiremock.client.WireMock.deleteRequestedFor;
-import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
-import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
-import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
 import static com.github.tomakehurst.wiremock.client.WireMock.verify;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.nio.charset.StandardCharsets;
-
 import org.apache.commons.configuration2.PropertiesConfiguration;
 import org.apache.james.mailbox.model.TestMessageId;
-import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 import com.linagora.tmail.extension.WireMockRagServerExtension;
 import com.linagora.tmail.mailet.rag.RagConfig;
-import com.linagora.tmail.mailet.rag.httpclient.ChatCompletionResult;
 import com.linagora.tmail.mailet.rag.httpclient.DocumentId;
 import com.linagora.tmail.mailet.rag.httpclient.OpenRagHttpClient;
 import com.linagora.tmail.mailet.rag.httpclient.OpenRagUnexpectedException;
@@ -61,76 +53,6 @@ public class OpenRagHttpClientTest {
         configuration.addProperty("openrag.partition.pattern", "{localPart}.twake.{domainName}");
         RagConfig ragConfig = RagConfig.from(configuration);
         client = new OpenRagHttpClient(ragConfig);
-    }
-
-    @Test
-    void proxyChatCompletionsShouldForwardPayloadAndHeadersAndReturnExactResponse() {
-        String resultBody = """
-            {
-              "id": "1234",
-              "choices": [
-                {
-                  "finish_reason": "stop",
-                  "index": 0,
-                  "message": {
-                    "content": "The glass green worm around Anvers"
-                  }
-                }
-              ]
-            }
-            """;
-
-        wireMockRagServerExtension.setChatCompletionResponse(200, resultBody);
-
-        String incomingPayload = """
-            {
-               "messages" : [
-                 {"role": "system", "content": "You are a translator."},
-                 {"role": "user", "content": "Translate this text:"},
-                 {"role": "user", "content": "Le ver vert en verre vers Anvers"}
-               ],
-                "metadata": {
-                 "action": "translate"
-               },
-               "stream": false
-            }""";
-
-        ChatCompletionResult result = client.proxyChatCompletions(incomingPayload.getBytes(StandardCharsets.UTF_8)).block();
-
-        verify(1, postRequestedFor(urlMatching(CHAT_COMPLETIONS_ENDPOINT)));
-
-        verify(postRequestedFor(urlMatching(CHAT_COMPLETIONS_ENDPOINT))
-            .withHeader("Authorization", equalTo("Bearer dummy-token"))
-            .withHeader("Content-Type", containing("application/json"))
-            .withRequestBody(equalToJson(incomingPayload)));
-
-        String body = new String(result.body(), StandardCharsets.UTF_8);
-        SoftAssertions.assertSoftly(solftly -> {
-            solftly.assertThat(result.status()).isEqualTo(200);
-            solftly.assertThat(result.headers().get("Content-Type")).isEqualTo("application/json");
-            solftly.assertThat(body).isEqualTo(resultBody);
-        });
-    }
-
-    @Test
-    void proxyChatCompletionsShouldReturnExactErrorResponses() {
-        String resultBody = """
-            {
-              "details": "Internal server error..."
-            }
-            """;
-
-        String incomingPayload = "{bad_payload}";
-
-        wireMockRagServerExtension.setChatCompletionResponse(500, resultBody);
-
-        ChatCompletionResult result = client.proxyChatCompletions(incomingPayload.getBytes(StandardCharsets.UTF_8)).block();
-        String body = new String(result.body(), StandardCharsets.UTF_8);
-        SoftAssertions.assertSoftly(solftly -> {
-            solftly.assertThat(result.status()).isEqualTo(500);
-            solftly.assertThat(result.headers().get("Content-Type")).isEqualTo("application/json");
-            solftly.assertThat(body).isEqualTo(resultBody);
-        });
     }
 
     @Test
