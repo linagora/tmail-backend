85d1787500fa528021c44dc651aca9c8e9f2d093
amghirbi <amghirbi@linagora.com>
2026-07-23 15:35:08 +0200

    [TMail] refactor: making ScribeConfiguration and AiHttpConfiguration two records

--- tmail-backend/tmail-third-party/ai-bot/src/main/java/com/linagora/tmail/mailet/conf/AiHttpClientConfiguration.java
+++ tmail-backend/tmail-third-party/ai-bot/src/main/java/com/linagora/tmail/mailet/conf/AiHttpClientConfiguration.java
@@ -23,17 +23,9 @@ import java.util.Optional;
 import com.linagora.tmail.mailet.rag.RagConfig;
 import com.linagora.tmail.scribe.ScribeConfiguration;
 
-public class AiHttpClientConfiguration {
-
-    private String authorizationToken;
-    private Optional<URL> baseUrl;
-    private boolean trustAllCertificates;
-
-    private AiHttpClientConfiguration(String authorizationToken, Optional<URL> baseUrl, boolean trustAllCertificates) {
-        this.authorizationToken = authorizationToken;
-        this.baseUrl = baseUrl;
-        this.trustAllCertificates = trustAllCertificates;
-    }
+public record AiHttpClientConfiguration(String authorizationToken,
+                                        Optional<URL> baseURLOpt,
+                                        boolean trustAllCertificates) {
 
     public static AiHttpClientConfiguration from(RagConfig ragConfig) {
         return new AiHttpClientConfiguration(ragConfig.getAuthorizationToken(),
@@ -42,21 +34,8 @@ public class AiHttpClientConfiguration {
     }
 
     public static AiHttpClientConfiguration from(ScribeConfiguration scribeConfig) {
-        return new AiHttpClientConfiguration(scribeConfig.getAuthorizationToken(),
-            scribeConfig.getBaseURLOpt(),
-            scribeConfig.getTrustAllCertificates());
-    }
-
-
-    public String getAuthorizationToken() {
-        return authorizationToken;
-    }
-
-    public Optional<URL> getBaseURLOpt() {
-        return baseUrl;
-    }
-
-    public boolean getTrustAllCertificates() {
-        return trustAllCertificates;
+        return new AiHttpClientConfiguration(scribeConfig.authorizationToken(),
+            scribeConfig.baseURLOpt(),
+            scribeConfig.trustAllCertificates());
     }
 }
