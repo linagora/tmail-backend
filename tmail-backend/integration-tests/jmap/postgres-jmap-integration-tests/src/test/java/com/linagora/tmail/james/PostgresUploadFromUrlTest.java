31dd46d9f00e074459cadec8834aa6ec77496744
Quan Tran <hqtran@linagora.com>
2026-08-19 17:06:47 +0700

    ISSUE-2556 Add JMAP upload from URL contract tests

---
+++ tmail-backend/integration-tests/jmap/postgres-jmap-integration-tests/src/test/java/com/linagora/tmail/james/PostgresUploadFromUrlTest.java
@@ -0,0 +1,88 @@
+/********************************************************************
+  *  As a subpart of Twake Mail, this file is edited by Linagora.    *
+  *                                                                  *
+  *  https://twake-mail.com/                                         *
+  *  https://linagora.com                                            *
+  *                                                                  *
+  *  This file is subject to The Affero Gnu Public License           *
+  *  version 3.                                                      *
+  *                                                                  *
+  *  https://www.gnu.org/licenses/agpl-3.0.en.html                   *
+  *                                                                  *
+  *  This program is distributed in the hope that it will be         *
+  *  useful, but WITHOUT ANY WARRANTY; without even the implied      *
+  *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR         *
+  *  PURPOSE. See the GNU Affero General Public License for          *
+  *  more details.                                                   *
+ *******************************************************************/
+
+/********************************************************************
+ *  As a subpart of Twake Mail, this file is edited by Linagora.    *
+ *                                                                  *
+ *  https://twake-mail.com/                                         *
+ *  https://linagora.com                                            *
+ *                                                                  *
+ *  This file is subject to The Affero Gnu Public License           *
+ *  version 3.                                                      *
+ *                                                                  *
+ *  https://www.gnu.org/licenses/agpl-3.0.en.html                   *
+ *                                                                  *
+ *  This program is distributed in the hope that it will be         *
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied      *
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR         *
+ *  PURPOSE. See the GNU Affero General Public License for          *
+ *  more details.                                                   *
+ ********************************************************************/
+
+package com.linagora.tmail.james;
+
+import static com.linagora.tmail.james.app.PostgresTmailConfiguration.EventBusImpl.IN_MEMORY;
+
+import org.apache.james.JamesServerBuilder;
+import org.apache.james.JamesServerExtension;
+import org.apache.james.SearchConfiguration;
+import org.apache.james.backends.postgres.PostgresExtension;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import com.linagora.tmail.blob.guice.BlobStoreConfiguration;
+import com.linagora.tmail.james.app.PostgresTmailConfiguration;
+import com.linagora.tmail.james.app.PostgresTmailServer;
+import com.linagora.tmail.james.common.UploadFromUrlContract;
+import com.linagora.tmail.james.common.extension.RemoteFileServerExtension;
+import com.linagora.tmail.james.jmap.firebase.FirebaseModuleChooserConfiguration;
+import com.linagora.tmail.module.LinagoraTestJMAPServerModule;
+
+public class PostgresUploadFromUrlTest implements UploadFromUrlContract {
+    @Order(1)
+    @RegisterExtension
+    static RemoteFileServerExtension remoteFileServer = new RemoteFileServerExtension();
+
+    @Order(2)
+    @RegisterExtension
+    static JamesServerExtension jamesServerExtension = new JamesServerBuilder<PostgresTmailConfiguration>(tmpDir ->
+        PostgresTmailConfiguration.builder()
+            .workingDirectory(tmpDir)
+            .configurationFromClasspath()
+            .blobStore(BlobStoreConfiguration.builder()
+                .postgres()
+                .disableCache()
+                .deduplication()
+                .noCryptoConfig()
+                .disableSingleSave())
+            .searchConfiguration(SearchConfiguration.scanning())
+            .firebaseModuleChooserConfiguration(FirebaseModuleChooserConfiguration.DISABLED)
+            .eventBusImpl(IN_MEMORY)
+            .uploadFromUrlConfiguration(remoteFileServer.uploadFromUrlConfiguration())
+            .build())
+        .server(configuration -> PostgresTmailServer.createServer(configuration)
+            .overrideWith(new LinagoraTestJMAPServerModule()))
+        .extension(PostgresExtension.empty())
+        .lifeCycle(JamesServerExtension.Lifecycle.PER_CLASS)
+        .build();
+
+    @Override
+    public RemoteFileServerExtension remoteFileServer() {
+        return remoteFileServer;
+    }
+}
